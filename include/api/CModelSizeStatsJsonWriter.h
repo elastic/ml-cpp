af650fd64a2ea5f711643c6424ffdae3b8346334
Dimitris Athanasiou <dimitris@elastic.co>
2018-03-14 12:21:04 +0000

    [ML] Refactor model snapshot writing to use concurrent writer (#12)
    This refactors the writing of the model snapshot document and
    model size stats document out of CJsonOutputWriter.
    
    It achieves the following improvements:
    
    - model snapshot documents do not have to be queued up. They
    can be written directly leveraging the safe concurrent writer
    that was implemented for forecasts. This minimizes the delay
    of a model snapshot being written which in failure cases may
    result into having a more recent snapshot available.
    - better separation of concerns and a smaller CJsonOutputWriter class.
    - simplifies adding new fields in the model snapshot document as
    needed for writing the minimum compatible version.

---
+++ include/api/CModelSizeStatsJsonWriter.h
@@ -0,0 +1,46 @@
+/*
+ * ELASTICSEARCH CONFIDENTIAL
+ *
+ * Copyright (c) 2018 Elasticsearch BV. All Rights Reserved.
+ *
+ * Notice: this software, and all information contained
+ * therein, is the exclusive property of Elasticsearch BV
+ * and its licensors, if any, and is protected under applicable
+ * domestic and foreign law, and international treaties.
+ *
+ * Reproduction, republication or distribution without the
+ * express written consent of Elasticsearch BV is
+ * strictly prohibited.
+ */
+#ifndef INCLUDED_ml_api_CModelSizeStatsJsonWriter_h
+#define INCLUDED_ml_api_CModelSizeStatsJsonWriter_h
+
+#include <core/CNonInstantiatable.h>
+#include <core/CRapidJsonConcurrentLineWriter.h>
+
+#include <model/CResourceMonitor.h>
+
+#include <api/ImportExport.h>
+
+#include <string>
+
+namespace ml
+{
+namespace api
+{
+
+//! \brief
+//! A static utility for writing the model_size_stats document in JSON.
+class API_EXPORT CModelSizeStatsJsonWriter : private core::CNonInstantiatable
+{
+    public:
+        //! Writes the model size stats in the \p results in JSON format.
+        static void write(const std::string &jobId,
+                          const model::CResourceMonitor::SResults &results,
+                          core::CRapidJsonConcurrentLineWriter &writer);
+};
+
+}
+}
+
+#endif // INCLUDED_ml_api_CModelSizeStatsJsonWriter_h
