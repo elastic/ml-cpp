61f1ea0fe4121579827acdb9852960e189f98192
Hendrik Muhs <hendrik.muhs@elastic.co>
2018-03-26 14:07:50 +0200

    refactor restore macro for enums

--- include/core/RestoreMacros.h
+++ include/core/RestoreMacros.h
@@ -56,7 +56,7 @@ namespace core
             continue;                                                                 \
         }
 
-#define RESTORE_ENUM(tag, target, enumtype, restoreSuccess)                           \
+#define RESTORE_ENUM(tag, target, enumtype)                                           \
         if (name == tag)                                                              \
         {                                                                             \
             int value;                                                                \
@@ -66,8 +66,14 @@ namespace core
                 return false;                                                         \
             }                                                                         \
             target = enumtype(value);                                                 \
-            restoreSuccess = true;													  \
             continue;                                                                 \
+        }
+
+#define RESTORE_ENUM_CHECKED(tag, target, enumtype, restoreSuccess)                   \
+        if (name == tag)                                                              \
+        {                                                                             \
+            restoreSuccess = true;                                                    \
+            RESTORE_ENUM(tag, target, enumtype)                                       \
         }
 
 #define RESTORE_SETUP_TEARDOWN(tag, setup, restore, teardown)                       \
