959070453e4f20d7303dd333e3e33df75f7e6735
David Roberts <dave.roberts@elastic.co>
2018-04-05 14:20:09 +0100

    [ML] Add a dev-tools/ci script to automate the Linux/macOS portion of the CI (#29)
    To facilitate migration from ML team CI to Infra CI we need to make the
    steps triggered by Jenkins as simple as possible.
    
    This change adds a ci script that can be used for Linux/macOS.

--- dev-tools/docker_test.sh
+++ dev-tools/docker_test.sh
@@ -5,18 +5,18 @@
 # you may not use this file except in compliance with the Elastic License.
 #
 
-# Builds the machine learning C++ code for Linux or Mac OS X in a Docker
-# container.
+# Builds the machine learning C++ code for Linux in a Docker container,
+# then runs the unit tests.
 #
 # The output .zip files are then copied out of the container to the
 # location in the current repository that they'd be in had they been
 # built outside of Docker.
 #
-# Finally, the Docker container used for the build is deleted.
+# Finally, the Docker container used for the build/test is deleted.
 
 usage()
 {
-    echo "Usage: $0 linux|linux-musl|macosx ..."
+    echo "Usage: $0 linux ..."
     exit 1
 }
 
@@ -26,7 +26,7 @@ while [ -n "$1" ]
 do
 
     case "$1" in
-        linux|linux-musl|macosx)
+        linux)
             PLATFORMS="$1 $PLATFORMS"
             ;;
         *)
@@ -60,17 +60,19 @@ cd "$TOOLS_DIR/.."
 for PLATFORM in `echo $PLATFORMS | tr ' ' '\n' | sort -u`
 do
 
-    # This Dockerfile is for the temporary image that is used to do the build.
-    # It is based on a pre-built build image stored on Docker Hub, but will have
-    # the local repository contents copied into it before the entrypoint script
-    # is run.  This temporary image is discarded after the build is complete.
-    DOCKERFILE="$TOOLS_DIR/docker/${PLATFORM}_builder/Dockerfile"
+    # This Dockerfile is for the temporary image that is used to do the build
+    # and unit tests.  It is based on a pre-built test image stored on Docker
+    # Hub, but will have the local repository contents copied into it before
+    # the entrypoint script is run.  This temporary image is discarded after
+    # the build and unit tests are complete.
+    DOCKERFILE="$TOOLS_DIR/docker/${PLATFORM}_tester/Dockerfile"
     TEMP_TAG=`git rev-parse --short=14 HEAD`-$PLATFORM-$$
 
     docker build -t $TEMP_TAG --build-arg SNAPSHOT=$SNAPSHOT -f "$DOCKERFILE" .
-    # Using tar to copy the build artifacts out of the container seems more reliable
-    # than docker cp, and also means the files end up with the correct uid/gid
-    docker run --workdir=/ml-cpp $TEMP_TAG tar cf - build/distributions | tar xvf -
+    # Using tar to copy the build and test artifacts out of the container seems
+    # more reliable than docker cp, and also means the files end up with the
+    # correct uid/gid
+    docker run --workdir=/ml-cpp $TEMP_TAG bash -c 'find . -name cppunit_results.xml | xargs tar cf - build/distributions' | tar xvf -
     docker rmi --force $TEMP_TAG
 
 done
