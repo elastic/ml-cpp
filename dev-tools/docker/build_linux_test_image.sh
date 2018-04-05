66c8ef7de276b6ebe1dad27a36bf63a8919ceeba
David Roberts <dave.roberts@elastic.co>
2018-04-05 14:20:09 +0100

    [ML] Add a dev-tools/ci script to automate the Linux/macOS portion of the CI (#29)
    To facilitate migration from ML team CI to Infra CI we need to make the
    steps triggered by Jenkins as simple as possible.
    
    This change adds a ci script that can be used for Linux/macOS.

---
+++ dev-tools/docker/build_linux_test_image.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# ELASTICSEARCH CONFIDENTIAL
+#
+# Copyright (c) 2018 Elasticsearch BV. All Rights Reserved.
+#
+# Notice: this software, and all information contained
+# therein, is the exclusive property of Elasticsearch BV
+# and its licensors, if any, and is protected under applicable
+# domestic and foreign law, and international treaties.
+#
+# Reproduction, republication or distribution without the
+# express written consent of Elasticsearch BV is
+# strictly prohibited.
+#
+
+# Builds the Docker image that can be used to unit test the machine learning
+# C++ code for Linux.
+#
+# This script is not intended to be run regularly.  When changing the tools
+# or 3rd party components required to build the machine learning C++ code
+# increment the version, change the Dockerfile and build a new image to be
+# used for subsequent builds on this branch.  Then update the version to be
+# used for builds in docker/linux_builder/Dockerfile.
+
+ACCOUNT=droberts195
+REPOSITORY=ml-linux-test
+VERSION=1
+
+set -e
+
+cd `dirname $0`
+
+docker build --no-cache -t $ACCOUNT/$REPOSITORY:$VERSION linux_test_image
+docker login
+docker push $ACCOUNT/$REPOSITORY:$VERSION
+
