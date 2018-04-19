5d92a2d7b966b0d240e9aad3d3c669a9a5763a58
David Roberts <dave.roberts@elastic.co>
2018-04-19 11:15:45 +0100

    [ML] Add a check style option to check all files
    By default check-style.sh only checks changed files.  This is good
    for interactive development, but to enforce style in CI we need an
    option to check every file conforms to the required style.  This
    change adds a --all option to check-style.sh to achieve this.

--- dev-tools/check-style.sh
+++ dev-tools/check-style.sh
@@ -14,24 +14,55 @@
 # strictly prohibited.
 #
 
-infiles=`git diff --name-only --diff-filter=ACMRT | grep -v "3rd_party" | grep -E "\.(cc|h)$"`
+CPP_SRC_HOME=${CPP_SRC_HOME:-`git rev-parse --show-toplevel`}
 
-files=()
-
-for file in ${infiles}; do
-  fqfile=`git rev-parse --show-toplevel`/${file}
-  echo "Checking: ${file}"
-  if ! cmp -s ${fqfile} <(clang-format ${fqfile}); then
-    files+=("${file}")
-  fi
-done
-
-rc=0
-
-if [ -n "${files}" ]; then
-echo "A Format error has been detected within the following files:"
-printf "%s\n" "${files[@]}"
-rc=1
+# Ensure clang-format is available
+which clang-format > /dev/null 2>&1
+if [ $? != 0 ] ; then
+    echo "ERROR: The clang-format code formatter is not available. Exiting."
+    exit 1
 fi
 
-exit ${rc}
+REQUIRED_CLANG_FORMAT_VERSION=5.0.1
+FOUND_CLANG_FORMAT_VERSION=$(expr "`clang-format --version`" : ".* \([0-9].[0-9].[0-9]\)")
+
+if [ -z ${FOUND_CLANG_FORMAT_VERSION} ] ; then
+    echo "ERROR: Required clang-format major version ${REQUIRED_CLANG_FORMAT_VERSION} not found."
+    echo "       Could not determine clang-format version."
+    exit 2
+fi
+
+if [ "${REQUIRED_CLANG_FORMAT_VERSION}" != "${FOUND_CLANG_FORMAT_VERSION}" ] ; then
+    echo "ERROR: Required clang-format major version ${REQUIRED_CLANG_FORMAT_VERSION} not found."
+    echo "       Detected clang-format version ${FOUND_CLANG_FORMAT_VERSION}"
+    exit 3
+fi
+
+FILES=()
+
+if [ "x$1" = "x--all" ] ; then
+    # Batch mode - check everything and only display errors
+    INFILES=`find $CPP_SRC_HOME \( -name 3rd_party -o -name build-setup \) -prune -o \( -name \*.cc -o -name \*.h \) -print`
+    for FILE in ${INFILES}; do
+        if ! cmp -s ${FILE} <(clang-format ${FILE}); then
+            FILES+=("${FILE}")
+        fi
+    done
+else
+    # Local mode - check changed files only and report which files are checked
+    INFILES=`git diff --name-only --diff-filter=ACMRT | grep -v 3rd_party | grep -E "\.(cc|h)$"`
+    for FILE in ${INFILES}; do
+        FQFILE=${CPP_SRC_HOME}/${FILE}
+        echo "Checking: ${FILE}"
+        if ! cmp -s ${FQFILE} <(clang-format ${FQFILE}); then
+            FILES+=("${FILE}")
+        fi
+    done
+fi
+
+if [ -n "${FILES}" ] ; then
+    echo "A format error has been detected within the following files:"
+    printf "%s\n" "${FILES[@]}"
+    exit 4
+fi
+
