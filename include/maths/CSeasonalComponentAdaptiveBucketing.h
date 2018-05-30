1f6ce15244d7e1747a11220b4dbfec297ab843fa
Tom Veasey <tveasey@elastic.co>
2018-05-30 14:52:33 +0100

    Another VC work around

--- include/maths/CSeasonalComponentAdaptiveBucketing.h
+++ include/maths/CSeasonalComponentAdaptiveBucketing.h
@@ -12,6 +12,7 @@
 #include <maths/CAdaptiveBucketing.h>
 #include <maths/CBasicStatistics.h>
 #include <maths/CRegression.h>
+#include <maths/CSeasonalTime.h>
 #include <maths/ImportExport.h>
 
 #include <cstddef>
@@ -26,7 +27,6 @@ class CStatePersistInserter;
 class CStateRestoreTraverser;
 }
 namespace maths {
-class CSeasonalTime;
 
 //! \brief An adaptive bucketing of the value of a periodic function.
 //!
