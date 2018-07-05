88a453b071be6935eb5349e746f6e4617e7065f8
Tom Veasey <tveasey@elastic.co>
2018-07-05 11:11:42 +0100

    Merge master

---
+++ include/maths/CRandomizedPeriodicityTest.h
@@ -0,0 +1,159 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+#ifndef INCLUDED_ml_maths_CRandomizedPeriodicityTest_h
+#define INCLUDED_ml_maths_CRandomizedPeriodicityTest_h
+
+#include <core/CMutex.h>
+#include <core/CoreTypes.h>
+
+#include <maths/CBasicStatistics.h>
+#include <maths/CLinearAlgebra.h>
+#include <maths/CPRNG.h>
+#include <maths/ImportExport.h>
+
+#include <boost/random/mersenne_twister.hpp>
+
+#include <atomic>
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+class CRandomizedPeriodicityTestTest;
+
+namespace ml {
+namespace core {
+class CStatePersistInserter;
+class CStateRestoreTraverser;
+}
+namespace maths {
+class CSeasonalTime;
+
+//! \brief A low memory footprint randomized test for probability.
+//!
+//! This is based on the idea of random projection in a Hilbert
+//! space.
+//!
+//! If we choose a direction uniformly at random, \f$r\f$,
+//! verses in the subspace of periodic functions, \f$p\$, we
+//! expect, with probability 1, that the inner product with
+//! a periodic function with matched period, \$f\f$, will be
+//! larger. In particular, if we imagine taking the expectation
+//! over a each family of functions then with probability 1:
+//! <pre class="fragment">
+//!     \f$E\left[\frac{\|r^t f\|}{\|r\|}\right] < E\left[\frac{\|p^t f\|}{\|p\|}\right]\f$
+//! </pre>
+//!
+//! Therefore, if we sample independently many such random
+//! projections of the function and the function is periodic
+//! then we can test the for periodicity by comparing means.
+//! The variance of the means will tend to zero as the number
+//! of samples grows so the significance for rejecting the
+//! null hypothesis (that the function is a-periodic) will
+//! shrink to zero.
+class MATHS_EXPORT CRandomizedPeriodicityTest {
+public:
+    //! The size of the projection sample coefficients
+    static const std::size_t N = 5;
+
+public:
+    CRandomizedPeriodicityTest();
+
+    //! \name Persistence
+    //@{
+    //! Restore the static members by reading state from \p traverser.
+    static bool staticsAcceptRestoreTraverser(core::CStateRestoreTraverser& traverser);
+
+    //! Persist the static members by passing information to \p inserter.
+    static void staticsAcceptPersistInserter(core::CStatePersistInserter& inserter);
+
+    //! Initialize by reading state from \p traverser.
+    bool acceptRestoreTraverser(core::CStateRestoreTraverser& traverser);
+
+    //! Persist state by passing information to \p inserter.
+    void acceptPersistInserter(core::CStatePersistInserter& inserter) const;
+    //@}
+
+    //! Add a new value \p value at \p time.
+    void add(core_t::TTime time, double value);
+
+    //! Test whether there is a periodic trend.
+    bool test() const;
+
+    //! Reset the test static random vectors.
+    //!
+    //! \note For unit testing only.
+    static void reset();
+
+    //! Get a checksum for this object.
+    std::uint64_t checksum(std::uint64_t seed = 0) const;
+
+private:
+    using TDoubleVec = std::vector<double>;
+    using TVector2 = CVectorNx1<CFloatStorage, 2>;
+    using TVector2MeanAccumulator = CBasicStatistics::SSampleMean<TVector2>::TAccumulator;
+    using TVector2N = CVectorNx1<CFloatStorage, 2 * N>;
+    using TVector2NMeanAccumulator = CBasicStatistics::SSampleMean<TVector2N>::TAccumulator;
+    using TAtomicTime = std::atomic<core_t::TTime>;
+
+private:
+    //! The length over which the periodic random projection decoheres.
+    static const core_t::TTime SAMPLE_INTERVAL;
+    //! The time between day resample events.
+    static const core_t::TTime DAY_RESAMPLE_INTERVAL;
+    //! The time between week resample events.
+    static const core_t::TTime WEEK_RESAMPLE_INTERVAL;
+    //! The random number generator.
+    static boost::random::mt19937_64 ms_Rng;
+    //! The permutations daily projections.
+    static TDoubleVec ms_DayRandomProjections[N];
+    //! The daily periodic projections.
+    static TDoubleVec ms_DayPeriodicProjections[N];
+    //! The time at which we re-sampled day projections.
+    static TAtomicTime ms_DayResampled;
+    //! The permutations weekly projections.
+    static TDoubleVec ms_WeekRandomProjections[N];
+    //! The weekly periodic projections.
+    static TDoubleVec ms_WeekPeriodicProjections[N];
+    //! The time at which we re-sampled week projections.
+    static TAtomicTime ms_WeekResampled;
+    //! The mutex for protecting state update.
+    static core::CMutex ms_Lock;
+
+private:
+    //! Refresh \p projections and update \p statistics.
+    static void updateStatistics(TVector2NMeanAccumulator& projections,
+                                 TVector2MeanAccumulator& statistics);
+
+    //! Re-sample the projections.
+    static void resample(core_t::TTime time);
+
+    //! Re-sample the specified projections.
+    static void resample(core_t::TTime period,
+                         core_t::TTime resampleInterval,
+                         TDoubleVec (&periodicProjections)[N],
+                         TDoubleVec (&randomProjections)[N]);
+
+private:
+    //! The day projections.
+    TVector2NMeanAccumulator m_DayProjections;
+    //! The sample mean of the square day projections.
+    TVector2MeanAccumulator m_DayStatistics;
+    //! The last time the day projections were updated.
+    core_t::TTime m_DayRefreshedProjections;
+    //! The week projections.
+    TVector2NMeanAccumulator m_WeekProjections;
+    //! The sample mean of the square week projections.
+    TVector2MeanAccumulator m_WeekStatistics;
+    //! The last time the day projections were updated.
+    core_t::TTime m_WeekRefreshedProjections;
+
+    friend class ::CRandomizedPeriodicityTestTest;
+};
+}
+}
+
+#endif // INCLUDED_ml_maths_CRandomizedPeriodicityTest_h
---
+++ lib/maths/CRandomizedPeriodicityTest.cc
@@ -0,0 +1,373 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+#include <maths/CRandomizedPeriodicityTest.h>
+
+#include <core/CContainerPrinter.h>
+#include <core/CLogger.h>
+#include <core/CScopedLock.h>
+#include <core/CStatePersistInserter.h>
+#include <core/CStateRestoreTraverser.h>
+#include <core/Constants.h>
+#include <core/RestoreMacros.h>
+
+#include <maths/CBasicStatistics.h>
+#include <maths/CBasicStatisticsPersist.h>
+#include <maths/CChecksum.h>
+#include <maths/CIntegerTools.h>
+#include <maths/CLinearAlgebra.h>
+#include <maths/CLinearAlgebraPersist.h>
+#include <maths/CLinearAlgebraTools.h>
+#include <maths/CSampling.h>
+#include <maths/CStatisticalTests.h>
+
+#include <boost/bind.hpp>
+#include <boost/numeric/conversion/bounds.hpp>
+#include <boost/random/uniform_real_distribution.hpp>
+#include <boost/range.hpp>
+#include <boost/ref.hpp>
+
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace ml {
+namespace maths {
+namespace {
+
+using TDoubleVec = std::vector<double>;
+using TMeanAccumulator = CBasicStatistics::SSampleMean<double>::TAccumulator;
+using TTimeVec = std::vector<core_t::TTime>;
+
+//! Generate \p n samples uniformly in the interval [\p a, \p b].
+template<typename ITR>
+void generateUniformSamples(boost::random::mt19937_64& rng, double a, double b, std::size_t n, ITR samples) {
+    boost::random::uniform_real_distribution<> uniform(a, b);
+    std::generate_n(samples, n, boost::bind(uniform, boost::ref(rng)));
+}
+
+//! Force the sample mean to zero.
+void zeroMean(TDoubleVec& samples) {
+    TMeanAccumulator mean;
+    for (auto sample : samples) {
+        mean.add(sample);
+    }
+    for (auto& sample : samples) {
+        sample -= CBasicStatistics::mean(mean);
+    }
+}
+
+// CRandomizedPeriodicityTest
+// statics
+const std::string RNG_TAG("a");
+const std::string DAY_RANDOM_PROJECTIONS_TAG("b");
+const std::string DAY_PERIODIC_PROJECTIONS_TAG("c");
+const std::string DAY_RESAMPLED_TAG("d");
+const std::string WEEK_RANDOM_PROJECTIONS_TAG("e");
+const std::string WEEK_PERIODIC_PROJECTIONS_TAG("f");
+const std::string WEEK_RESAMPLED_TAG("g");
+const std::string ARRAY_INDEX_TAG("h");
+// non-statics
+const std::string DAY_PROJECTIONS_TAG("a");
+const std::string DAY_STATISTICS_TAG("b");
+const std::string DAY_REFRESHED_PROJECTIONS_TAG("c");
+const std::string WEEK_PROJECTIONS_TAG("d");
+const std::string WEEK_STATISTICS_TAG("e");
+const std::string WEEK_REFRESHED_PROJECTIONS_TAG("f");
+}
+
+//////// CRandomizedPeriodicitytest ////////
+
+CRandomizedPeriodicityTest::CRandomizedPeriodicityTest()
+    : m_DayRefreshedProjections(-DAY_RESAMPLE_INTERVAL),
+      m_WeekRefreshedProjections(-DAY_RESAMPLE_INTERVAL) {
+    resample(0);
+}
+
+bool CRandomizedPeriodicityTest::staticsAcceptRestoreTraverser(core::CStateRestoreTraverser& traverser) {
+    // Note we require that we only ever do one persistence per process.
+
+    std::size_t index = 0;
+    reset();
+
+    core::CScopedLock lock(ms_Lock);
+
+    do {
+        const std::string& name = traverser.name();
+
+        if (name == RNG_TAG) {
+            // Replace '_' with space
+            std::string value(traverser.value());
+            std::replace(value.begin(), value.end(), '_', ' ');
+            std::stringstream ss;
+            ss << value;
+            ss >> ms_Rng;
+            continue;
+        }
+        RESTORE_SETUP_TEARDOWN(DAY_RESAMPLED_TAG, core_t::TTime resampled,
+                               core::CStringUtils::stringToType(traverser.value(), resampled),
+                               ms_DayResampled.store(resampled))
+        RESTORE_SETUP_TEARDOWN(WEEK_RESAMPLED_TAG, core_t::TTime resampled,
+                               core::CStringUtils::stringToType(traverser.value(), resampled),
+                               ms_WeekResampled.store(resampled))
+        RESTORE_BUILT_IN(ARRAY_INDEX_TAG, index)
+        RESTORE_SETUP_TEARDOWN(DAY_RANDOM_PROJECTIONS_TAG, double d,
+                               core::CStringUtils::stringToType(traverser.value(), d),
+                               ms_DayRandomProjections[index].push_back(d))
+        RESTORE_SETUP_TEARDOWN(DAY_PERIODIC_PROJECTIONS_TAG, double d,
+                               core::CStringUtils::stringToType(traverser.value(), d),
+                               ms_DayPeriodicProjections[index].push_back(d))
+        RESTORE_SETUP_TEARDOWN(WEEK_RANDOM_PROJECTIONS_TAG, double d,
+                               core::CStringUtils::stringToType(traverser.value(), d),
+                               ms_WeekRandomProjections[index].push_back(d))
+        RESTORE_SETUP_TEARDOWN(WEEK_PERIODIC_PROJECTIONS_TAG, double d,
+                               core::CStringUtils::stringToType(traverser.value(), d),
+                               ms_WeekPeriodicProjections[index].push_back(d))
+    } while (traverser.next());
+
+    return true;
+}
+
+void CRandomizedPeriodicityTest::staticsAcceptPersistInserter(core::CStatePersistInserter& inserter) {
+    // Note we require that we only ever do one persistence per process.
+
+    core::CScopedLock lock(ms_Lock);
+
+    std::ostringstream ss;
+    ss << ms_Rng;
+    std::string rng(ss.str());
+    // Replace spaces else JSON parsers get confused
+    std::replace(rng.begin(), rng.end(), ' ', '_');
+    inserter.insertValue(RNG_TAG, rng);
+    inserter.insertValue(DAY_RESAMPLED_TAG, ms_DayResampled.load());
+    inserter.insertValue(WEEK_RESAMPLED_TAG, ms_WeekResampled.load());
+    for (std::size_t i = 0; i < N; ++i) {
+        inserter.insertValue(ARRAY_INDEX_TAG, i);
+        for (auto rand : ms_DayRandomProjections[i]) {
+            inserter.insertValue(DAY_RANDOM_PROJECTIONS_TAG, rand);
+        }
+        for (auto rand : ms_DayPeriodicProjections[i]) {
+            inserter.insertValue(DAY_PERIODIC_PROJECTIONS_TAG, rand);
+        }
+        for (auto rand : ms_WeekRandomProjections[i]) {
+            inserter.insertValue(WEEK_RANDOM_PROJECTIONS_TAG, rand);
+        }
+        for (auto rand : ms_WeekPeriodicProjections[i]) {
+            inserter.insertValue(WEEK_PERIODIC_PROJECTIONS_TAG, rand);
+        }
+    }
+}
+
+bool CRandomizedPeriodicityTest::acceptRestoreTraverser(core::CStateRestoreTraverser& traverser) {
+    do {
+        const std::string& name = traverser.name();
+
+        RESTORE(DAY_PROJECTIONS_TAG, m_DayProjections.fromDelimited(traverser.value()))
+        RESTORE(DAY_STATISTICS_TAG, m_DayStatistics.fromDelimited(traverser.value()))
+        RESTORE(DAY_REFRESHED_PROJECTIONS_TAG,
+                core::CStringUtils::stringToType(traverser.value(), m_DayRefreshedProjections))
+        RESTORE(WEEK_PROJECTIONS_TAG, m_WeekProjections.fromDelimited(traverser.value()))
+        RESTORE(WEEK_STATISTICS_TAG, m_WeekStatistics.fromDelimited(traverser.value()))
+        RESTORE(DAY_STATISTICS_TAG, m_DayStatistics.fromDelimited(traverser.value()))
+        RESTORE(WEEK_REFRESHED_PROJECTIONS_TAG,
+                core::CStringUtils::stringToType(traverser.value(), m_WeekRefreshedProjections))
+    } while (traverser.next());
+
+    return true;
+}
+
+void CRandomizedPeriodicityTest::acceptPersistInserter(core::CStatePersistInserter& inserter) const {
+    inserter.insertValue(DAY_PROJECTIONS_TAG, m_DayProjections.toDelimited());
+    inserter.insertValue(DAY_STATISTICS_TAG, m_DayStatistics.toDelimited());
+    inserter.insertValue(DAY_REFRESHED_PROJECTIONS_TAG, m_DayRefreshedProjections);
+    inserter.insertValue(WEEK_PROJECTIONS_TAG, m_WeekProjections.toDelimited());
+    inserter.insertValue(WEEK_STATISTICS_TAG, m_WeekStatistics.toDelimited());
+    inserter.insertValue(WEEK_REFRESHED_PROJECTIONS_TAG, m_WeekRefreshedProjections);
+}
+
+void CRandomizedPeriodicityTest::add(core_t::TTime time, double value) {
+    resample(time);
+
+    if (time >= m_DayRefreshedProjections + DAY_RESAMPLE_INTERVAL) {
+        LOG_TRACE(<< "Updating day statistics");
+        updateStatistics(m_DayProjections, m_DayStatistics);
+        m_DayRefreshedProjections = CIntegerTools::floor(time, DAY_RESAMPLE_INTERVAL);
+    }
+    if (time >= m_WeekRefreshedProjections + WEEK_RESAMPLE_INTERVAL) {
+        LOG_TRACE(<< "Updating week statistics");
+        updateStatistics(m_WeekProjections, m_WeekStatistics);
+        m_WeekRefreshedProjections = CIntegerTools::floor(time, WEEK_RESAMPLE_INTERVAL);
+    }
+
+    TVector2N daySample;
+    TVector2N weekSample;
+    std::size_t td = static_cast<std::size_t>((time % DAY_RESAMPLE_INTERVAL) / SAMPLE_INTERVAL);
+    std::size_t d = static_cast<std::size_t>((time % core::constants::DAY) / SAMPLE_INTERVAL);
+    std::size_t tw = static_cast<std::size_t>((time % WEEK_RESAMPLE_INTERVAL) / SAMPLE_INTERVAL);
+    std::size_t w = static_cast<std::size_t>((time % core::constants::WEEK) / SAMPLE_INTERVAL);
+
+    for (std::size_t i = 0u; i < N; ++i) {
+        daySample(2 * i + 0) = ms_DayRandomProjections[i][td] * value;
+        daySample(2 * i + 1) = ms_DayPeriodicProjections[i][d] * value;
+        weekSample(2 * i + 0) = ms_WeekRandomProjections[i][tw] * value;
+        weekSample(2 * i + 1) = ms_WeekPeriodicProjections[i][w] * value;
+    }
+
+    m_DayProjections.add(daySample);
+    m_WeekProjections.add(weekSample);
+}
+
+bool CRandomizedPeriodicityTest::test() const {
+    static const double SIGNIFICANCE = 1e-3;
+
+    try {
+        double nd = CBasicStatistics::count(m_DayStatistics);
+        if (nd >= 1.0) {
+            TVector2 S = CBasicStatistics::mean(m_DayStatistics);
+            LOG_TRACE(<< "Day test statistic, S = " << S << ", n = " << nd);
+            double ratio = S(0) == S(1)
+                               ? 1.0
+                               : (S(0) == 0.0 ? boost::numeric::bounds<double>::highest()
+                                              : static_cast<double>(S(1) / S(0)));
+            double significance = CStatisticalTests::rightTailFTest(ratio, nd, nd);
+            LOG_TRACE(<< "Daily significance = " << significance);
+            if (significance < SIGNIFICANCE) {
+                return true;
+            }
+        }
+
+        double nw = CBasicStatistics::count(m_WeekStatistics);
+        if (nw >= 1.0) {
+            TVector2 S = CBasicStatistics::mean(m_WeekStatistics);
+            LOG_TRACE(<< "Week test statistic, S = " << S);
+            double ratio = S(0) == S(1)
+                               ? 1.0
+                               : (S(0) == 0.0 ? boost::numeric::bounds<double>::highest()
+                                              : static_cast<double>(S(1) / S(0)));
+            double significance = CStatisticalTests::rightTailFTest(ratio, nw, nw);
+            LOG_TRACE(<< "Weekly significance = " << significance);
+            if (significance < SIGNIFICANCE) {
+                return true;
+            }
+        }
+    } catch (const std::exception& e) {
+        LOG_ERROR(<< "Failed to test for periodicity: " << e.what());
+    }
+
+    return false;
+}
+
+void CRandomizedPeriodicityTest::reset() {
+    core::CScopedLock lock(ms_Lock);
+
+    ms_Rng = boost::random::mt19937_64();
+    for (std::size_t i = 0u; i < N; ++i) {
+        ms_DayRandomProjections[i].clear();
+        ms_DayPeriodicProjections[i].clear();
+        ms_WeekRandomProjections[i].clear();
+        ms_WeekPeriodicProjections[i].clear();
+    }
+    ms_DayResampled = -DAY_RESAMPLE_INTERVAL;
+    ms_WeekResampled = -WEEK_RESAMPLE_INTERVAL;
+}
+
+std::uint64_t CRandomizedPeriodicityTest::checksum(std::uint64_t seed) const {
+    // This checksum is problematic until we switch to using our
+    // own rng for each test.
+    seed = CChecksum::calculate(seed, m_DayProjections);
+    seed = CChecksum::calculate(seed, m_DayStatistics);
+    seed = CChecksum::calculate(seed, m_DayRefreshedProjections);
+    seed = CChecksum::calculate(seed, m_WeekProjections);
+    seed = CChecksum::calculate(seed, m_WeekStatistics);
+    return CChecksum::calculate(seed, m_WeekRefreshedProjections);
+}
+
+void CRandomizedPeriodicityTest::updateStatistics(TVector2NMeanAccumulator& projections,
+                                                  TVector2MeanAccumulator& statistics) {
+    static const double ALPHA = 0.1;
+
+    if (CBasicStatistics::count(projections) > 0.0) {
+        const TVector2N& mean = CBasicStatistics::mean(projections);
+        LOG_TRACE(<< "mean = " << mean);
+
+        TVector2MeanAccumulator statistic;
+        for (std::size_t i = 0u; i < N; ++i) {
+            TVector2 s;
+            s(0) = mean(2 * i + 0) * mean(2 * i + 0);
+            s(1) = mean(2 * i + 1) * mean(2 * i + 1);
+            statistic.add(s);
+        }
+        statistics += statistic;
+        statistics.age(1.0 - ALPHA);
+        LOG_TRACE(<< "statistics = " << statistics);
+    }
+
+    projections = TVector2NMeanAccumulator();
+}
+
+void CRandomizedPeriodicityTest::resample(core_t::TTime time) {
+    if (time >= ms_DayResampled.load(std::memory_order_acquire) + DAY_RESAMPLE_INTERVAL) {
+        core::CScopedLock lock(ms_Lock);
+
+        LOG_TRACE(<< "Updating daily random projections at " << time);
+        if (time >= ms_DayResampled.load(std::memory_order_relaxed) + DAY_RESAMPLE_INTERVAL) {
+            resample(core::constants::DAY, DAY_RESAMPLE_INTERVAL,
+                     ms_DayPeriodicProjections, ms_DayRandomProjections);
+            ms_DayResampled.store(CIntegerTools::floor(time, DAY_RESAMPLE_INTERVAL),
+                                  std::memory_order_release);
+        }
+    }
+
+    if (time >= ms_WeekResampled.load(std::memory_order_acquire) + WEEK_RESAMPLE_INTERVAL) {
+        core::CScopedLock lock(ms_Lock);
+
+        LOG_TRACE(<< "Updating weekly random projections at " << time);
+        if (time >= ms_WeekResampled.load(std::memory_order_relaxed) + WEEK_RESAMPLE_INTERVAL) {
+            resample(core::constants::WEEK, WEEK_RESAMPLE_INTERVAL,
+                     ms_WeekPeriodicProjections, ms_WeekRandomProjections);
+            ms_WeekResampled.store(CIntegerTools::floor(time, WEEK_RESAMPLE_INTERVAL),
+                                   std::memory_order_release);
+        }
+    }
+}
+
+void CRandomizedPeriodicityTest::resample(core_t::TTime period,
+                                          core_t::TTime resampleInterval,
+                                          TDoubleVec (&periodicProjections)[N],
+                                          TDoubleVec (&randomProjections)[N]) {
+    std::size_t n = static_cast<std::size_t>(period / SAMPLE_INTERVAL);
+    std::size_t t = static_cast<std::size_t>(resampleInterval / SAMPLE_INTERVAL);
+    std::size_t p = static_cast<std::size_t>(resampleInterval / period);
+    for (std::size_t i = 0u; i < N; ++i) {
+        periodicProjections[i].resize(n);
+        generateUniformSamples(ms_Rng, -1.0, 1.0, n, periodicProjections[i].begin());
+        zeroMean(periodicProjections[i]);
+        randomProjections[i].resize(t);
+        for (std::size_t j = 0u; j < p; ++j) {
+            std::copy(periodicProjections[i].begin(), periodicProjections[i].end(),
+                      randomProjections[i].begin() + j * n);
+            CSampling::random_shuffle(ms_Rng, randomProjections[i].begin() + j * n,
+                                      randomProjections[i].begin() + (j + 1) * n);
+        }
+    }
+}
+
+const core_t::TTime CRandomizedPeriodicityTest::SAMPLE_INTERVAL(3600);
+const core_t::TTime CRandomizedPeriodicityTest::DAY_RESAMPLE_INTERVAL(1209600);
+const core_t::TTime CRandomizedPeriodicityTest::WEEK_RESAMPLE_INTERVAL(2419200);
+boost::random::mt19937_64 CRandomizedPeriodicityTest::ms_Rng = boost::random::mt19937_64();
+TDoubleVec CRandomizedPeriodicityTest::ms_DayRandomProjections[N] = {};
+TDoubleVec CRandomizedPeriodicityTest::ms_DayPeriodicProjections[N] = {};
+std::atomic<core_t::TTime> CRandomizedPeriodicityTest::ms_DayResampled(-DAY_RESAMPLE_INTERVAL);
+TDoubleVec CRandomizedPeriodicityTest::ms_WeekRandomProjections[N] = {};
+TDoubleVec CRandomizedPeriodicityTest::ms_WeekPeriodicProjections[N] = {};
+std::atomic<core_t::TTime> CRandomizedPeriodicityTest::ms_WeekResampled(-WEEK_RESAMPLE_INTERVAL);
+core::CMutex CRandomizedPeriodicityTest::ms_Lock;
+}
+}
