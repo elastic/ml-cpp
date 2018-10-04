99b7b832d909d84ec4b0ab83f6a5d339555e2f8f
David Roberts <dave.roberts@elastic.co>
2018-10-04 14:34:20 +0100

    [ML] Add support for PR CI builds (#233)
    The CI scripts will now behave differently if triggered from
    a PR build:
    
    1. The code style will be checked first and style violations
       will cause the PR check to fail
    2. The artifacts will only be uploaded to S3 for non-PR builds
    
    PR builds are detected by checking whether a PR_AUTHOR environment
    variable exists.

--- dev-tools/ci.bat
+++ dev-tools/ci.bat
@@ -5,11 +5,11 @@ rem or more contributor license agreements. Licensed under the Elastic License;
 rem you may not use this file except in compliance with the Elastic License.
 rem
 
-rem The Windows part of ML C++ CI:
+rem The Windows part of ML C++ CI does the following:
 rem
 rem 1. Build and unit test the Windows version of the C++
-rem 2. Upload the build to the artifacts directory on S3 that
-rem    subsequent Java builds will download the C++ components from
+rem 2. If this is not a PR build, upload the build to the artifacts directory on
+rem    S3 that subsequent Java builds will download the C++ components from
 
 setlocal enableextensions
 
@@ -24,8 +24,8 @@ rem Run the build and unit tests
 set ML_KEEP_GOING=1
 call .\gradlew.bat --info clean buildZip buildZipSymbols check || exit /b %ERRORLEVEL%
 
-rem Upload the artifacts to S3
-call .\gradlew.bat --info -b upload.gradle upload || exit /b %ERRORLEVEL%
+rem If this isn't a PR build then upload the artifacts
+if not defined PR_AUTHOR call .\gradlew.bat --info -b upload.gradle upload || exit /b %ERRORLEVEL%
 
 endlocal
 
