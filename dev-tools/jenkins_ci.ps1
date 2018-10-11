2c9b564f9be9391ff6da24f7ffe7be036f4f4a5f
David Roberts <dave.roberts@elastic.co>
2018-10-11 16:14:11 +0100

    [ML] Consolidate CI scripts (#250)
    This change combines the `ci` and `jenkins-ci` scripts, as there's
    no good reason for splitting the functionality.
    
    The `ci.bat` script is converted to Powershell, and the exit code
    of every invocation of external programs is checked so that
    failures can immediately fail the overall CI run.
    
    Additionally there are some style changes to the Powershell syntax
    to match standard practice.

--- dev-tools/jenkins_ci.ps1
+++ dev-tools/jenkins_ci.ps1
@@ -1,35 +1,62 @@
-function Exec {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        [scriptblock]$cmd,
-        [string]$errorMessage = ($msgs.error_bad_command -f $cmd)
-    )
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
 
-    try {
-        $global:lastexitcode = 0
-        & $cmd
-        if ($lastexitcode -ne 0) {
-            throw $errorMessage
-        }
+# The Windows part of ML C++ CI does the following:
+#
+# 1. If this is not a PR build, obtain credentials from Vault for the accessing
+#    S3
+# 2. Build and unit test the Windows version of the C++
+# 3. If this is not a PR build, upload the build to the artifacts directory on
+#    S3 that subsequent Java builds will download the C++ components from
+
+$ErrorActionPreference="Stop"
+
+# If this isn't a PR build then obtain credentials from Vault
+if (!(Test-Path Env:PR_AUTHOR)) {
+    # Generate a Vault token
+    $Env:VAULT_TOKEN=& vault write -field=token auth/approle/login "role_id=$Env:VAULT_ROLE_ID" "secret_id=$Env:VAULT_SECRET_ID"
+    if ($LastExitCode -ne 0) {
+        Exit $LastExitCode
     }
-    catch [Exception] {
-        throw $_
+
+    $AwsCreds=& vault read -format=json -field=data aws-dev/creds/prelertartifacts
+    if ($LastExitCode -ne 0) {
+        Exit $LastExitCode
+    }
+    $Env:ML_AWS_ACCESS_KEY=(echo $AwsCreds | jq -r ".access_key")
+    $Env:ML_AWS_SECRET_KEY=(echo $AwsCreds | jq -r ".secret_key")
+
+    # Remove VAULT_* environment variables
+    Remove-Item Env:VAULT_TOKEN
+    Remove-Item Env:VAULT_ROLE_ID
+    Remove-Item Env:VAULT_SECRET_ID
+}
+
+# Change directory to the top level of the repo
+Set-Location -Path "$PSScriptRoot\.."
+
+# Ensure 3rd party dependencies are installed
+& "dev-tools\download_windows_deps.ps1"
+
+# Default to a snapshot build
+if (!(Test-Path Env:BUILD_SNAPSHOT)) {
+    $Env:BUILD_SNAPSHOT="true"
+}
+
+# Run the build and unit tests
+& ".\gradlew.bat" --info "-Dbuild.snapshot=$Env:BUILD_SNAPSHOT" clean buildZip buildZipSymbols check
+if ($LastExitCode -ne 0) {
+    Exit $LastExitCode
+}
+
+# If this isn't a PR build then upload the artifacts
+if (!(Test-Path Env:PR_AUTHOR)) {
+    & ".\gradlew.bat" --info -b "upload.gradle" "-Dbuild.snapshot=$Env:BUILD_SNAPSHOT" upload
+    if ($LastExitCode -ne 0) {
+        Exit $LastExitCode
     }
 }
 
-# Generate a Vault token
-$env:VAULT_TOKEN = & vault write -field=token auth/approle/login role_id="$env:VAULT_ROLE_ID" secret_id="$env:VAULT_SECRET_ID"
-
-# Load aws-creds from vault
-$aws_creds = & vault read -format=json -field=data aws-dev/creds/prelertartifacts
-$env:ML_AWS_ACCESS_KEY=(echo $aws_creds | jq -r ".access_key")
-$env:ML_AWS_SECRET_KEY=(echo $aws_creds | jq -r ".secret_key")
-
-# Remove VAULT_* values
-Remove-Item env:VAULT_TOKEN
-Remove-Item env:VAULT_ROLE_ID
-Remove-Item env:VAULT_SECRET_ID
-
-# Call dev-tools\ci.bat
-exec { cmd /c "dev-tools\ci.bat" } "CI Test FAILURE"
