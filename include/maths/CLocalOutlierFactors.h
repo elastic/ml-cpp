64f2378004aa43cd47cea10a47293d0b58edf5f6
Tom Veasey <tveasey@elastic.co>
2018-11-02 14:57:29 +0000

    Linux and Windows build fixes

--- include/maths/CLocalOutlierFactors.h
+++ include/maths/CLocalOutlierFactors.h
@@ -193,7 +193,7 @@ protected:
                               !points.empty() ? las::dimension(points[0]) : 1)),
                           4.0)};
         double n{static_cast<double>(points.size())};
-        double result;
+        double result{};
         switch (algorithm) {
         case E_Lof:
             result = std::max(std::min(5.0 * d, 20.0), std::pow(n, 1.0 / 3.0));
@@ -586,55 +586,12 @@ protected:
     //!
     //! This simply fits a Gaussian to a collection of scores for an
     //! outlier method and computes the right tail probability.
-    static void normalize(TDoubleVec& scores) {
-        using TMaxAccumulator =
-            CBasicStatistics::COrderStatisticsHeap<double, std::greater<double>>;
-
-        TMaxAccumulator max(std::max(scores.size() / 10, std::size_t(1)));
-        max.add(scores);
-        double cutoff{max.biggest()};
-
-        TMeanVarAccumulator moments;
-        for (auto score : scores) {
-            moments.add(std::min(score, cutoff));
-        }
-
-        try {
-            double mean{CBasicStatistics::mean(moments)};
-            double variance{CBasicStatistics::variance(moments)};
-            if (variance > 0.0) {
-                boost::math::normal normal(mean, std::sqrt(variance));
-                for (auto& score : scores) {
-                    score = cdfComplementToScore(CTools::safeCdfComplement(normal, score));
-                }
-            } else {
-                scores.assign(scores.size(), 0.0);
-            }
-        } catch (const std::exception& e) {
-            LOG_ERROR(<< "Failed to normalise scores: " << e.what());
-        }
-    }
+    static void normalize(TDoubleVec& scores);
 
     //! Convert the c.d.f. complement of an outlier factor into a
     //! score in the range [0,100] with the higher the score the
     //! greater the outlier.
-    static double cdfComplementToScore(double cdfComplement) {
-        auto logInterpolate = [](double x, double xa, double fa, double xb, double fb) {
-            x = CTools::truncate(x, std::min(xa, xb), std::max(xa, xb));
-            double alpha{(CTools::fastLog(x) - CTools::fastLog(xa)) /
-                         (CTools::fastLog(xb) - CTools::fastLog(xa))};
-            double beta{1.0 - alpha};
-            return fb * alpha + fa * beta;
-        };
-        if (cdfComplement > 1e-4) {
-            return 1e-4 / cdfComplement;
-        }
-        if (cdfComplement > 1e-20) {
-            return logInterpolate(cdfComplement, 1e-4, 1.0, 1e-20, 50.0);
-        }
-        return logInterpolate(cdfComplement, 1e-20, 50.0,
-                              std::numeric_limits<double>::min(), 100.0);
-    }
+    static double cdfComplementToScore(double cdfComplement);
 };
 }
 }
