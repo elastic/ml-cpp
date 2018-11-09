83e821eed6e8758cf624795f7a1ed40cf920112b
Tom Veasey <tveasey@elastic.co>
2018-11-09 13:51:45 +0000

    Unit test relies on the constructor not being explicit. I don't want to refactor any code around this at present since it is a tangent.

--- include/maths/CLassoLogisticRegression.h
+++ include/maths/CLassoLogisticRegression.h
@@ -37,7 +37,7 @@ public:
 
 public:
     CLrDenseMatrix();
-    explicit CLrDenseMatrix(TDoubleVecVec& elements);
+    CLrDenseMatrix(TDoubleVecVec& elements);
 
     //! Efficiently swap the contents of two matrices.
     void swap(CLrDenseMatrix& other);
