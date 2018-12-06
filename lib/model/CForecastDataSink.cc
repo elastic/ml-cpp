80c23ba9fd84b3433071c1b94c0f94dca0ccfe4b
David Roberts <dave.roberts@elastic.co>
2018-12-06 11:32:38 +0000

    Revert "[ML] Correct query times for model plot and forecast (#329)"
    This reverts commit c53a6f0c402913297298e8eb38412881648f9b23.

--- lib/model/CForecastDataSink.cc
+++ lib/model/CForecastDataSink.cc
@@ -9,8 +9,6 @@
 #include <core/CLogger.h>
 #include <core/CScopedRapidJsonPoolAllocator.h>
 
-#include <boost/bind.hpp>
-
 #include <vector>
 
 namespace ml {
@@ -56,34 +54,16 @@ const std::string CForecastDataSink::STATUS("forecast_status");
 
 using TScopedAllocator = core::CScopedRapidJsonPoolAllocator<core::CRapidJsonConcurrentLineWriter>;
 
-CForecastDataSink::CForecastModelWrapper::CForecastModelWrapper(model_t::EFeature feature,
+CForecastDataSink::SForecastModelWrapper::SForecastModelWrapper(model_t::EFeature feature,
                                                                 TMathsModelPtr&& forecastModel,
                                                                 const std::string& byFieldValue)
-    : m_Feature(feature), m_ForecastModel(std::move(forecastModel)),
-      m_ByFieldValue(byFieldValue) {
+    : s_Feature(feature), s_ForecastModel(std::move(forecastModel)),
+      s_ByFieldValue(byFieldValue) {
 }
 
-CForecastDataSink::CForecastModelWrapper::CForecastModelWrapper(CForecastModelWrapper&& other)
-    : m_Feature(other.m_Feature), m_ForecastModel(std::move(other.m_ForecastModel)),
-      m_ByFieldValue(std::move(other.m_ByFieldValue)) {
-}
-
-bool CForecastDataSink::CForecastModelWrapper::forecast(const SForecastResultSeries& series,
-                                                        core_t::TTime startTime,
-                                                        core_t::TTime endTime,
-                                                        double boundsPercentile,
-                                                        CForecastDataSink& sink,
-                                                        std::string& message) const {
-    core_t::TTime bucketLength{m_ForecastModel->params().bucketLength()};
-    startTime = model_t::sampleTime(m_Feature, startTime, bucketLength);
-    endTime = model_t::sampleTime(m_Feature, endTime, bucketLength);
-    model_t::TDouble1VecDouble1VecPr support{model_t::support(m_Feature)};
-    return m_ForecastModel->forecast(
-        startTime, endTime, boundsPercentile, support.first, support.second,
-        boost::bind(&model::CForecastDataSink::push, &sink, _1, model_t::print(m_Feature),
-                    series.s_PartitionFieldName, series.s_PartitionFieldValue,
-                    series.s_ByFieldName, m_ByFieldValue, series.s_DetectorIndex),
-        message);
+CForecastDataSink::SForecastModelWrapper::SForecastModelWrapper(SForecastModelWrapper&& other)
+    : s_Feature(other.s_Feature), s_ForecastModel(std::move(other.s_ForecastModel)),
+      s_ByFieldValue(std::move(other.s_ByFieldValue)) {
 }
 
 CForecastDataSink::SForecastResultSeries::SForecastResultSeries(const SModelParams& modelParams)
