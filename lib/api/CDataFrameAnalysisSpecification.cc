c3b32ad37cd2726ca9adacfe9a3796cdcb43bfe0
Tom Veasey <tveasey@elastic.co>
2019-01-15 11:16:05 +0000

    [FEATURE][ML] Avoid accidental copy

--- lib/api/CDataFrameAnalysisSpecification.cc
+++ lib/api/CDataFrameAnalysisSpecification.cc
@@ -203,7 +203,7 @@ void CDataFrameAnalysisSpecification::initializeRunner(const char* name,
         if (std::strcmp(factory->name(), name) == 0) {
             m_Runner = analysis.HasMember(PARAMETERS)
                            ? factory->make(*this, analysis[PARAMETERS])
-                           : factory->make((*this));
+                           : factory->make(*this);
             return;
         }
     }
