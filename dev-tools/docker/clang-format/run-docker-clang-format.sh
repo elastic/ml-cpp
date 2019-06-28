19910f1209e2160496bd7b19f8c8cd629898e6a9
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2019-06-28 10:41:45 +0200

    Adding copyrights and --no-cache

--- dev-tools/docker/clang-format/run-docker-clang-format.sh
+++ dev-tools/docker/clang-format/run-docker-clang-format.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
 docker run --rm -v $CPP_SRC_HOME:/ml-cpp -u $(id -u):$(id -g) alpine:clang-format
