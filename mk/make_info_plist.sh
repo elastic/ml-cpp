e494b78e53bfc051486a8ab8c8fb3d6f8cc7c414
David Roberts <dave.roberts@elastic.co>
2019-08-29 10:36:40 +0100

    Add CFBundlePackageType = APPL to Info.plist
    Also make sure the identifier only uses valid characters

--- mk/make_info_plist.sh
+++ mk/make_info_plist.sh
@@ -3,14 +3,17 @@
 ML_VERSION_NUM=`cat $CPP_SRC_HOME/gradle.properties | grep '^elasticsearchVersion' | awk -F= '{ print $2 }' | xargs echo | sed 's/-.*//'`
 ML_TARGET=$1
 ML_SHORTENED_TARGET=`echo $ML_TARGET | cut -c1-15`
+ML_URL_TARGET=`echo $ML_TARGET | tr _ - | tr -d -C A-Za-z0-9- | cut -c1-63`
 
 cat <<EOF
 <plist version="1.0">
 <dict>
     <key>CFBundleDevelopmentRegion</key>
     <string>English</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
     <key>CFBundleIdentifier</key>
-    <string>co.elastic.ml-cpp.$ML_TARGET</string>
+    <string>co.elastic.ml-cpp.$ML_URL_TARGET</string>
     <key>CFBundleInfoDictionaryVersion</key>
     <string>6.0</string>
     <key>CFBundleName</key>
