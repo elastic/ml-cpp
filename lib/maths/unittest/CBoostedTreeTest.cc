94e4c4e0a7d35fe78a54fd104589b49e33b21be7
Tom Veasey <tveasey@elastic.co>
2019-09-10 18:34:29 +0100

    Test threshold

--- lib/maths/unittest/CBoostedTreeTest.cc
+++ lib/maths/unittest/CBoostedTreeTest.cc
@@ -259,7 +259,7 @@ void CBoostedTreeTest::testLinear() {
         meanModelRSquared.add(modelRSquared[i][0]);
     }
     LOG_DEBUG(<< "mean R^2 = " << maths::CBasicStatistics::mean(meanModelRSquared));
-    CPPUNIT_ASSERT(maths::CBasicStatistics::mean(meanModelRSquared) > 0.97);
+    CPPUNIT_ASSERT(maths::CBasicStatistics::mean(meanModelRSquared) > 0.96);
 }
 
 void CBoostedTreeTest::testNonLinear() {
