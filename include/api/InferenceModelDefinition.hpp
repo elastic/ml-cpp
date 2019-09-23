55a4819d5fc25c64e64999efa58d607298162c1e
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2019-09-23 15:46:41 +0200

    extract tree node

--- include/api/InferenceModelDefinition.hpp
+++ include/api/InferenceModelDefinition.hpp
@@ -9,51 +9,48 @@
 
 #pragma once
 
-#include <json/json.hpp>
+#include "json/json.hpp"
 
 #ifndef NLOHMANN_OPT_HELPER
 #define NLOHMANN_OPT_HELPER
 namespace nlohmann {
 template <typename T>
 struct adl_serializer<std::shared_ptr<T>> {
-    static void to_json(json & j, const std::shared_ptr<T> & opt) {
-        if (!opt) j = nullptr; else j = *opt;
-    }
+static void to_json(json & j, const std::shared_ptr<T> & opt) {
+    if (!opt) j = nullptr; else j = *opt;
+}
 
-    static std::shared_ptr<T> from_json(const json & j) {
-        if (j.is_null()) return std::unique_ptr<T>(); else return std::unique_ptr<T>(new T(j.get<T>()));
-    }
+static std::shared_ptr<T> from_json(const json & j) {
+    if (j.is_null()) return std::unique_ptr<T>(); else return std::unique_ptr<T>(new T(j.get<T>()));
+}
 };
 }
 #endif
 
-namespace ml {
-namespace api {
 namespace inference_model {
-
 using nlohmann::json;
 
-inline json get_untyped(const json &j, const char *property) {
+inline json get_untyped(const json & j, const char * property) {
     if (j.find(property) != j.end()) {
         return j.at(property).get<json>();
     }
     return json();
 }
 
-inline json get_untyped(const json &j, std::string property) {
+inline json get_untyped(const json & j, std::string property) {
     return get_untyped(j, property.data());
 }
 
-template<typename T>
-inline std::shared_ptr<T> get_optional(const json &j, const char *property) {
+template <typename T>
+inline std::shared_ptr<T> get_optional(const json & j, const char * property) {
     if (j.find(property) != j.end()) {
         return j.at(property).get<std::shared_ptr<T>>();
     }
     return std::shared_ptr<T>();
 }
 
-template<typename T>
-inline std::shared_ptr<T> get_optional(const json &j, std::string property) {
+template <typename T>
+inline std::shared_ptr<T> get_optional(const json & j, std::string property) {
     return get_optional<T>(j, property.data());
 }
 
@@ -76,15 +73,14 @@ struct AggregateOutput {
     std::shared_ptr<WeightedMode> weightedMode;
 };
 
-enum class NumericRelationship : int {
-    Empty
-};
+enum class NumericRelationship : int { Empty };
 
 struct TreeNode {
     NumericRelationship decisionType;
     bool defaultLeft;
-    std::shared_ptr<TreeNode> leftChild;
-    std::shared_ptr<TreeNode> rightChild;
+    std::shared_ptr<int64_t> leftChild;
+    double nodeValue;
+    std::shared_ptr<int64_t> rightChild;
     int64_t splitFeature;
     std::shared_ptr<double> splitGain;
     int64_t splitIndex;
@@ -93,7 +89,7 @@ struct TreeNode {
 
 struct Tree {
     std::shared_ptr<std::vector<std::string>> featureNames;
-    TreeNode treeStructure;
+    std::vector<TreeNode> treeStructure;
 };
 
 /**
@@ -101,7 +97,7 @@ struct Tree {
  */
 struct Evaluation {
     std::shared_ptr<std::vector<std::string>> featureNames;
-    std::shared_ptr<TreeNode> treeStructure;
+    std::shared_ptr<std::vector<TreeNode>> treeStructure;
     std::shared_ptr<AggregateOutput> aggregateOutput;
     std::shared_ptr<std::vector<Tree>> models;
 };
@@ -156,6 +152,10 @@ struct OneHotEncoding {
  */
 struct TargetMeanEncoding {
     /**
+     * Value for categories that have not been seen before
+     */
+    double defaultValue;
+    /**
      * Feature name after pre-processing
      */
     std::string featureName;
@@ -208,94 +208,94 @@ struct InferenceModelDefinition {
     std::shared_ptr<std::vector<Preprocessing>> preprocessing;
 };
 }
-}
-}
 
 namespace nlohmann {
-void from_json(const json & j, ml::api::inference_model::WeightedMode & x);
-void to_json(json & j, const ml::api::inference_model::WeightedMode & x);
+void from_json(const json & j, inference_model::WeightedMode & x);
+void to_json(json & j, const inference_model::WeightedMode & x);
 
-void from_json(const json & j, ml::api::inference_model::WeightedSum & x);
-void to_json(json & j, const ml::api::inference_model::WeightedSum & x);
+void from_json(const json & j, inference_model::WeightedSum & x);
+void to_json(json & j, const inference_model::WeightedSum & x);
 
-void from_json(const json & j, ml::api::inference_model::AggregateOutput & x);
-void to_json(json & j, const ml::api::inference_model::AggregateOutput & x);
+void from_json(const json & j, inference_model::AggregateOutput & x);
+void to_json(json & j, const inference_model::AggregateOutput & x);
 
-void from_json(const json & j, ml::api::inference_model::TreeNode & x);
-void to_json(json & j, const ml::api::inference_model::TreeNode & x);
+void from_json(const json & j, inference_model::TreeNode & x);
+void to_json(json & j, const inference_model::TreeNode & x);
 
-void from_json(const json & j, ml::api::inference_model::Tree & x);
-void to_json(json & j, const ml::api::inference_model::Tree & x);
+void from_json(const json & j, inference_model::Tree & x);
+void to_json(json & j, const inference_model::Tree & x);
 
-void from_json(const json & j, ml::api::inference_model::Evaluation & x);
-void to_json(json & j, const ml::api::inference_model::Evaluation & x);
+void from_json(const json & j, inference_model::Evaluation & x);
+void to_json(json & j, const inference_model::Evaluation & x);
 
-void from_json(const json & j, ml::api::inference_model::Input & x);
-void to_json(json & j, const ml::api::inference_model::Input & x);
+void from_json(const json & j, inference_model::Input & x);
+void to_json(json & j, const inference_model::Input & x);
 
-void from_json(const json & j, ml::api::inference_model::FrequencyEncoding & x);
-void to_json(json & j, const ml::api::inference_model::FrequencyEncoding & x);
+void from_json(const json & j, inference_model::FrequencyEncoding & x);
+void to_json(json & j, const inference_model::FrequencyEncoding & x);
 
-void from_json(const json & j, ml::api::inference_model::OneHotEncoding & x);
-void to_json(json & j, const ml::api::inference_model::OneHotEncoding & x);
+void from_json(const json & j, inference_model::OneHotEncoding & x);
+void to_json(json & j, const inference_model::OneHotEncoding & x);
 
-void from_json(const json & j, ml::api::inference_model::TargetMeanEncoding & x);
-void to_json(json & j, const ml::api::inference_model::TargetMeanEncoding & x);
+void from_json(const json & j, inference_model::TargetMeanEncoding & x);
+void to_json(json & j, const inference_model::TargetMeanEncoding & x);
 
-void from_json(const json & j, ml::api::inference_model::Preprocessing & x);
-void to_json(json & j, const ml::api::inference_model::Preprocessing & x);
+void from_json(const json & j, inference_model::Preprocessing & x);
+void to_json(json & j, const inference_model::Preprocessing & x);
 
-void from_json(const json & j, ml::api::inference_model::InferenceModelDefinition & x);
-void to_json(json & j, const ml::api::inference_model::InferenceModelDefinition & x);
+void from_json(const json & j, inference_model::InferenceModelDefinition & x);
+void to_json(json & j, const inference_model::InferenceModelDefinition & x);
 
-void from_json(const json & j, ml::api::inference_model::NumericRelationship & x);
-void to_json(json & j, const ml::api::inference_model::NumericRelationship & x);
+void from_json(const json & j, inference_model::NumericRelationship & x);
+void to_json(json & j, const inference_model::NumericRelationship & x);
 
-inline void from_json(const json & j, ml::api::inference_model::WeightedMode& x) {
+inline void from_json(const json & j, inference_model::WeightedMode& x) {
     x.weights = j.at("weights").get<std::vector<double>>();
 }
 
-inline void to_json(json & j, const ml::api::inference_model::WeightedMode & x) {
+inline void to_json(json & j, const inference_model::WeightedMode & x) {
     j = json::object();
     j["weights"] = x.weights;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::WeightedSum& x) {
+inline void from_json(const json & j, inference_model::WeightedSum& x) {
     x.weights = j.at("weights").get<std::vector<double>>();
 }
 
-inline void to_json(json & j, const ml::api::inference_model::WeightedSum & x) {
+inline void to_json(json & j, const inference_model::WeightedSum & x) {
     j = json::object();
     j["weights"] = x.weights;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::AggregateOutput& x) {
-    x.weightedSum = ml::api::inference_model::get_optional<ml::api::inference_model::WeightedSum>(j, "weighted_sum");
-    x.weightedMode = ml::api::inference_model::get_optional<ml::api::inference_model::WeightedMode>(j, "weighted_mode");
+inline void from_json(const json & j, inference_model::AggregateOutput& x) {
+    x.weightedSum = inference_model::get_optional<inference_model::WeightedSum>(j, "weighted_sum");
+    x.weightedMode = inference_model::get_optional<inference_model::WeightedMode>(j, "weighted_mode");
 }
 
-inline void to_json(json & j, const ml::api::inference_model::AggregateOutput & x) {
+inline void to_json(json & j, const inference_model::AggregateOutput & x) {
     j = json::object();
     j["weighted_sum"] = x.weightedSum;
     j["weighted_mode"] = x.weightedMode;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::TreeNode& x) {
-    x.decisionType = j.at("decision_type").get<ml::api::inference_model::NumericRelationship>();
+inline void from_json(const json & j, inference_model::TreeNode& x) {
+    x.decisionType = j.at("decision_type").get<inference_model::NumericRelationship>();
     x.defaultLeft = j.at("default_left").get<bool>();
-    x.leftChild = ml::api::inference_model::get_optional<ml::api::inference_model::TreeNode>(j, "left_child");
-    x.rightChild = ml::api::inference_model::get_optional<ml::api::inference_model::TreeNode>(j, "right_child");
+    x.leftChild = inference_model::get_optional<int64_t>(j, "left_child");
+    x.nodeValue = j.at("node_value").get<double>();
+    x.rightChild = inference_model::get_optional<int64_t>(j, "right_child");
     x.splitFeature = j.at("split_feature").get<int64_t>();
-    x.splitGain = ml::api::inference_model::get_optional<double>(j, "split_gain");
+    x.splitGain = inference_model::get_optional<double>(j, "split_gain");
     x.splitIndex = j.at("split_index").get<int64_t>();
     x.threshold = j.at("threshold").get<double>();
 }
 
-inline void to_json(json & j, const ml::api::inference_model::TreeNode & x) {
+inline void to_json(json & j, const inference_model::TreeNode & x) {
     j = json::object();
     j["decision_type"] = x.decisionType;
     j["default_left"] = x.defaultLeft;
     j["left_child"] = x.leftChild;
+    j["node_value"] = x.nodeValue;
     j["right_child"] = x.rightChild;
     j["split_feature"] = x.splitFeature;
     j["split_gain"] = x.splitGain;
@@ -303,25 +303,25 @@ inline void to_json(json & j, const ml::api::inference_model::TreeNode & x) {
     j["threshold"] = x.threshold;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::Tree& x) {
-    x.featureNames = ml::api::inference_model::get_optional<std::vector<std::string>>(j, "feature_names");
-    x.treeStructure = j.at("tree_structure").get<ml::api::inference_model::TreeNode>();
+inline void from_json(const json & j, inference_model::Tree& x) {
+    x.featureNames = inference_model::get_optional<std::vector<std::string>>(j, "feature_names");
+    x.treeStructure = j.at("tree_structure").get<std::vector<inference_model::TreeNode>>();
 }
 
-inline void to_json(json & j, const ml::api::inference_model::Tree & x) {
+inline void to_json(json & j, const inference_model::Tree & x) {
     j = json::object();
     j["feature_names"] = x.featureNames;
     j["tree_structure"] = x.treeStructure;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::Evaluation& x) {
-    x.featureNames = ml::api::inference_model::get_optional<std::vector<std::string>>(j, "feature_names");
-    x.treeStructure = ml::api::inference_model::get_optional<ml::api::inference_model::TreeNode>(j, "tree_structure");
-    x.aggregateOutput = ml::api::inference_model::get_optional<ml::api::inference_model::AggregateOutput>(j, "aggregate_output");
-    x.models = ml::api::inference_model::get_optional<std::vector<ml::api::inference_model::Tree>>(j, "models");
+inline void from_json(const json & j, inference_model::Evaluation& x) {
+    x.featureNames = inference_model::get_optional<std::vector<std::string>>(j, "feature_names");
+    x.treeStructure = inference_model::get_optional<std::vector<inference_model::TreeNode>>(j, "tree_structure");
+    x.aggregateOutput = inference_model::get_optional<inference_model::AggregateOutput>(j, "aggregate_output");
+    x.models = inference_model::get_optional<std::vector<inference_model::Tree>>(j, "models");
 }
 
-inline void to_json(json & j, const ml::api::inference_model::Evaluation & x) {
+inline void to_json(json & j, const inference_model::Evaluation & x) {
     j = json::object();
     j["feature_names"] = x.featureNames;
     j["tree_structure"] = x.treeStructure;
@@ -329,86 +329,88 @@ inline void to_json(json & j, const ml::api::inference_model::Evaluation & x) {
     j["models"] = x.models;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::Input& x) {
-    x.columns = ml::api::inference_model::get_optional<std::vector<std::string>>(j, "columns");
+inline void from_json(const json & j, inference_model::Input& x) {
+    x.columns = inference_model::get_optional<std::vector<std::string>>(j, "columns");
 }
 
-inline void to_json(json & j, const ml::api::inference_model::Input & x) {
+inline void to_json(json & j, const inference_model::Input & x) {
     j = json::object();
     j["columns"] = x.columns;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::FrequencyEncoding& x) {
+inline void from_json(const json & j, inference_model::FrequencyEncoding& x) {
     x.featureName = j.at("feature_name").get<std::string>();
     x.field = j.at("field").get<std::string>();
     x.frequencyMap = j.at("frequency_map").get<std::map<std::string, double>>();
 }
 
-inline void to_json(json & j, const ml::api::inference_model::FrequencyEncoding & x) {
+inline void to_json(json & j, const inference_model::FrequencyEncoding & x) {
     j = json::object();
     j["feature_name"] = x.featureName;
     j["field"] = x.field;
     j["frequency_map"] = x.frequencyMap;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::OneHotEncoding& x) {
+inline void from_json(const json & j, inference_model::OneHotEncoding& x) {
     x.field = j.at("field").get<std::string>();
     x.hotMap = j.at("hot_map").get<std::map<std::string, std::string>>();
 }
 
-inline void to_json(json & j, const ml::api::inference_model::OneHotEncoding & x) {
+inline void to_json(json & j, const inference_model::OneHotEncoding & x) {
     j = json::object();
     j["field"] = x.field;
     j["hot_map"] = x.hotMap;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::TargetMeanEncoding& x) {
+inline void from_json(const json & j, inference_model::TargetMeanEncoding& x) {
+    x.defaultValue = j.at("default_value").get<double>();
     x.featureName = j.at("feature_name").get<std::string>();
     x.field = j.at("field").get<std::string>();
     x.targetMap = j.at("target_map").get<std::map<std::string, double>>();
 }
 
-inline void to_json(json & j, const ml::api::inference_model::TargetMeanEncoding & x) {
+inline void to_json(json & j, const inference_model::TargetMeanEncoding & x) {
     j = json::object();
+    j["default_value"] = x.defaultValue;
     j["feature_name"] = x.featureName;
     j["field"] = x.field;
     j["target_map"] = x.targetMap;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::Preprocessing& x) {
-    x.oneHotEncoding = ml::api::inference_model::get_optional<ml::api::inference_model::OneHotEncoding>(j, "one_hot_encoding");
-    x.targetMeanEncoding = ml::api::inference_model::get_optional<ml::api::inference_model::TargetMeanEncoding>(j, "target_mean_encoding");
-    x.frequencyEncoding = ml::api::inference_model::get_optional<ml::api::inference_model::FrequencyEncoding>(j, "frequency_encoding");
+inline void from_json(const json & j, inference_model::Preprocessing& x) {
+    x.oneHotEncoding = inference_model::get_optional<inference_model::OneHotEncoding>(j, "one_hot_encoding");
+    x.targetMeanEncoding = inference_model::get_optional<inference_model::TargetMeanEncoding>(j, "target_mean_encoding");
+    x.frequencyEncoding = inference_model::get_optional<inference_model::FrequencyEncoding>(j, "frequency_encoding");
 }
 
-inline void to_json(json & j, const ml::api::inference_model::Preprocessing & x) {
+inline void to_json(json & j, const inference_model::Preprocessing & x) {
     j = json::object();
     j["one_hot_encoding"] = x.oneHotEncoding;
     j["target_mean_encoding"] = x.targetMeanEncoding;
     j["frequency_encoding"] = x.frequencyEncoding;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::InferenceModelDefinition& x) {
-    x.evaluation = j.at("evaluation").get<ml::api::inference_model::Evaluation>();
-    x.input = j.at("input").get<ml::api::inference_model::Input>();
-    x.preprocessing = ml::api::inference_model::get_optional<std::vector<ml::api::inference_model::Preprocessing>>(j, "preprocessing");
+inline void from_json(const json & j, inference_model::InferenceModelDefinition& x) {
+    x.evaluation = j.at("evaluation").get<inference_model::Evaluation>();
+    x.input = j.at("input").get<inference_model::Input>();
+    x.preprocessing = inference_model::get_optional<std::vector<inference_model::Preprocessing>>(j, "preprocessing");
 }
 
-inline void to_json(json & j, const ml::api::inference_model::InferenceModelDefinition & x) {
+inline void to_json(json & j, const inference_model::InferenceModelDefinition & x) {
     j = json::object();
     j["evaluation"] = x.evaluation;
     j["input"] = x.input;
     j["preprocessing"] = x.preprocessing;
 }
 
-inline void from_json(const json & j, ml::api::inference_model::NumericRelationship & x) {
-    if (j == "<=") x = ml::api::inference_model::NumericRelationship::Empty;
+inline void from_json(const json & j, inference_model::NumericRelationship & x) {
+    if (j == "<=") x = inference_model::NumericRelationship::Empty;
     else throw "Input JSON does not conform to schema";
 }
 
-inline void to_json(json & j, const ml::api::inference_model::NumericRelationship & x) {
+inline void to_json(json & j, const inference_model::NumericRelationship & x) {
     switch (x) {
-        case ml::api::inference_model::NumericRelationship::Empty: j = "<="; break;
+        case inference_model::NumericRelationship::Empty: j = "<="; break;
         default: throw "This should not happen";
     }
 }
--- lib/api/CInferenceModelFormatter.cc
+++ lib/api/CInferenceModelFormatter.cc
@@ -3,6 +3,8 @@
 #include <core/CLogger.h>
 #include <core/LogMacros.h>
 
+#include <api/InferenceModelDefinition.hpp>
+
 #include <rapidjson/writer.h>
 #include <rapidjson/fwd.h>
 
@@ -16,16 +18,46 @@ ml::api::CInferenceModelFormatter::CInferenceModelFormatter(const rapidjson::Doc
         for (auto forestIterator = bestForest.MemberBegin();
              forestIterator != bestForest.MemberEnd(); ++forestIterator) {
             std::string objectName{forestIterator->name.GetString()};
-            if ( objectName == "a") {
+            if (objectName == "a") {
 
                 for (auto treeIterator = forestIterator->value.MemberBegin();
                      treeIterator != forestIterator->value.MemberEnd(); ++treeIterator) {
                     std::string objectName2{treeIterator->name.GetString()};
                     if (objectName2 == "a") {
+                        inference_model::TreeNode treeNode;
                         for (auto treeElementIteartor = treeIterator->value.MemberBegin();
                              treeElementIteartor != treeIterator->value.MemberEnd(); ++treeElementIteartor) {
-                        LOG_DEBUG(<< treeElementIteartor->name.GetString());
-                    }
+                            std::string name{treeElementIteartor->name.GetString()};
+                            std::string value{treeElementIteartor->name.GetString()};
+                            if (name == "split_feature") {
+                                treeNode.splitFeature = std::stoul(value);
+                            }
+                            else if (name == "assign_missing_to_left") {
+                                treeNode.defaultLeft = (value == "true");
+                            }
+                            else if (name == "node_value") {
+                                treeNode.nodeValue = std::stod(value);
+                            }
+                            else if (name == "split_index") {
+                                treeNode.splitIndex = std::stoul(value);
+                            }
+                            else if (name == "split_value") {
+                                treeNode.threshold = std::stod(value);
+                            }
+                            else if (name == "left_child") {
+                                if (std::stoi(value) != -1) {
+                                    treeNode.leftChild =std::make_shared<std::int64_t >(std::stoi(value));
+                                }
+                            }
+                            else if (name == "right_child") {
+                                if (std::stoi(value) != -1) {
+                                    treeNode.rightChild = std::make_shared<std::int64_t >(std::stoi(value));
+                                }
+                            }
+
+
+                            LOG_DEBUG(<< treeElementIteartor->name.GetString());
+                        }
                     }
                 }
             }
