elastic/ml-cpp

commit b831cf6974602a3d0224a0fedcb0a2c0b063d57c

[ML] Make the ML native code more ".app"-like on macOS (#593)
Successful notarization and acceptance by Gatekeeper on macOS
Catalina requires that the ML programs and dynamic libraries be
arranged into the directory structure used by macOS apps.

This functionality may be required for notarization on
macOS Catalina.

* Avoid clashing use of .plist files for static analysis output

Static analysis files are now named *.xml (after all .plist
files really are .xml files)

* Add CFBundlePackageType = APPL to Info.plist

Also make sure the identifier only uses valid characters

* Change macOS directory structure

* Switch CPP_CROSS_COMPILE back to macosx

* Fix resource dir and include top level Info.plist with XML header

* Oops

* Fix problem with ordering of static initialization

* Now we're shipping an app, use the app name in all Info.plist entries

When this PR started out we thought we'd be shipping a number
of standalone "command line tools".  But that didn't work with
notarization, so we had to make a more radical change, to become
an app.

(I also removed some dead code related to detecting Linux MUSL.)

lib/core/CResourceLocator.cc
@@ -8,28 +8,36 @@
 #include <core/COsFileFuncs.h>
 #include <core/CProgName.h>
 
-#include <stdlib.h>
+#include <cstdlib>
 
 namespace {
-const char* const CPP_SRC_HOME("CPP_SRC_HOME");
+// Important: all file scope in this file must be simple types that don't
+// require construction, as they could be accessed before the static
+// constructors are run.
+const char* const CPP_SRC_HOME{"CPP_SRC_HOME"};
+
+#ifdef MacOSX
+const char* const RESOURCE_RELATIVE_DIR{"../Resources"};
+#else
+const char* const RESOURCE_RELATIVE_DIR{"../resources"};
+#endif
 }
 
 namespace ml {
 namespace core {
 
 std::string CResourceLocator::resourceDir() {
-    // Look relative to the program that's running, assuming this directory layout:
-    // $ES_HOME/plugin/<plugin name>/resources
-    // $ES_HOME/plugin/<plugin name>/platform/<platform name>/bin
 
-    std::string productionDir(CProgName::progDir() + "/../../../resources");
+    // Look relative to the program that's running, assuming that the resource
+    // directory is located relative to the directory the current program is in.
+    std::string productionDir(CProgName::progDir() + RESOURCE_RELATIVE_DIR);
 
     // If the production directory doesn't exist, return the dev directory if
     // that does, but if neither exist return the production directory so the
     // error message is nicer for the end user.
     COsFileFuncs::TStat buf;
     if (COsFileFuncs::stat(productionDir.c_str(), &buf) != 0) {
-        const char* cppSrcHome(::getenv(CPP_SRC_HOME));
+        const char* cppSrcHome(std::getenv(CPP_SRC_HOME));
         if (cppSrcHome != nullptr) {
             std::string devDir(cppSrcHome);
             devDir += "/lib/core";
@@ -42,24 +50,8 @@ std::string CResourceLocator::resourceDir() {
     return productionDir;
 }
 
-std::string CResourceLocator::logDir() {
-    // Look relative to the program that's running, assuming this directory layout:
-    // $ES_HOME/logs
-    // $ES_HOME/plugin/<plugin name>/platform/<platform name>/bin
-
-    std::string productionDir(CProgName::progDir() + "/../../../../../logs");
-
-    COsFileFuncs::TStat buf;
-    if (COsFileFuncs::stat(productionDir.c_str(), &buf) != 0) {
-        // Assume we're running as a unit test
-        return ".";
-    }
-
-    return productionDir;
-}
-
 std::string CResourceLocator::cppRootDir() {
-    const char* cppSrcHome(::getenv(CPP_SRC_HOME));
+    const char* cppSrcHome(std::getenv(CPP_SRC_HOME));
     if (cppSrcHome == nullptr) {
         // Assume we're in a unittest directory
         return "../../..";

3rd_party/3rd_party.sh
@@ -59,21 +59,19 @@ case `uname` in
             STL_PREFIX=libstdc++
             STL_EXTENSION=.so.6
             ZLIB_LOCATION=
+        elif [ "$CPP_CROSS_COMPILE" = macosx ] ; then
+            SYSROOT=/usr/local/sysroot-x86_64-apple-macosx10.11
+            BOOST_LOCATION=$SYSROOT/usr/local/lib
+            BOOST_COMPILER=clang
+            BOOST_EXTENSION=mt-x64-1_71.dylib
+            BOOST_LIBRARIES='atomic chrono date_time filesystem iostreams log log_setup program_options regex system thread'
+            XML_LOCATION=
+            GCC_RT_LOCATION=
+            STL_LOCATION=
+            ZLIB_LOCATION=
         else
-            if [ "$CPP_CROSS_COMPILE" = macosx ] ; then
-                SYSROOT=/usr/local/sysroot-x86_64-apple-macosx10.11
-                BOOST_LOCATION=$SYSROOT/usr/local/lib
-                BOOST_COMPILER=clang
-                BOOST_EXTENSION=mt-x64-1_71.dylib
-                BOOST_LIBRARIES='atomic chrono date_time filesystem iostreams log log_setup program_options regex system thread'
-                XML_LOCATION=
-                GCC_RT_LOCATION=
-                STL_LOCATION=
-                ZLIB_LOCATION=
-            else
-                echo "Cannot cross compile to $CPP_CROSS_COMPILE"
-                exit 3
-            fi
+            echo "Cannot cross compile to $CPP_CROSS_COMPILE"
+            exit 3
         fi
         ;;
 

bin/autoconfig/Makefile
@@ -7,7 +7,7 @@ include $(CPP_SRC_HOME)/mk/defines.mk
 
 TARGET=$(OBJS_DIR)/autoconfig$(EXE_EXT)
 
-INSTALL_DIR=$(CPP_PLATFORM_HOME)/bin
+INSTALL_DIR=$(CPP_PLATFORM_HOME)/$(EXE_DIR)
 
 ML_LIBS=$(LIB_ML_CORE) $(LIB_ML_MATHS) $(LIB_ML_MODEL) $(LIB_ML_API) $(LIB_ML_CONFIG)
 

bin/autodetect/Makefile
@@ -7,7 +7,7 @@ include $(CPP_SRC_HOME)/mk/defines.mk
 
 TARGET=$(OBJS_DIR)/autodetect$(EXE_EXT)
 
-INSTALL_DIR=$(CPP_PLATFORM_HOME)/bin
+INSTALL_DIR=$(CPP_PLATFORM_HOME)/$(EXE_DIR)
 
 ML_LIBS=$(LIB_ML_CORE) $(LIB_ML_MATHS) $(LIB_ML_MODEL) $(LIB_ML_API)
 

bin/categorize/Makefile
@@ -7,7 +7,7 @@ include $(CPP_SRC_HOME)/mk/defines.mk
 
 TARGET=$(OBJS_DIR)/categorize$(EXE_EXT)
 
-INSTALL_DIR=$(CPP_PLATFORM_HOME)/bin
+INSTALL_DIR=$(CPP_PLATFORM_HOME)/$(EXE_DIR)
 
 ML_LIBS=$(LIB_ML_CORE) $(LIB_ML_MATHS) $(LIB_ML_MODEL) $(LIB_ML_API)
 

bin/controller/Makefile
@@ -7,7 +7,7 @@ include $(CPP_SRC_HOME)/mk/defines.mk
 
 TARGET=$(OBJS_DIR)/controller$(EXE_EXT)
 
-INSTALL_DIR=$(CPP_PLATFORM_HOME)/bin
+INSTALL_DIR=$(CPP_PLATFORM_HOME)/$(EXE_DIR)
 
 ML_LIBS=$(LIB_ML_CORE)
 
@@ -16,7 +16,7 @@ USE_BOOST_PROGRAMOPTIONS_LIBS=1
 
 LIBS=$(ML_LIBS)
 
-all: build
+all:: build
 
 SRCS= \
     Main.cc \
@@ -24,6 +24,11 @@ SRCS= \
     CCmdLineParser.cc \
     CCommandProcessor.cc \
 
+ifneq ($(PLIST_FILE),)
+all:: $(CPP_SRC_HOME)/gradle.properties $(CPP_SRC_HOME)/mk/make_info_plist.sh
+	$(MKDIR) $(INSTALL_DIR)
+	$(CPP_SRC_HOME)/mk/make_info_plist.sh "$(ML_APP_NAME)" true > $(INSTALL_DIR)/../$(notdir $(PLIST_FILE))
+endif
 
 include $(CPP_SRC_HOME)/mk/stdapp.mk
 

bin/data_frame_analyzer/Makefile
@@ -7,7 +7,7 @@ include $(CPP_SRC_HOME)/mk/defines.mk
 
 TARGET=$(OBJS_DIR)/data_frame_analyzer$(EXE_EXT)
 
-INSTALL_DIR=$(CPP_PLATFORM_HOME)/bin
+INSTALL_DIR=$(CPP_PLATFORM_HOME)/$(EXE_DIR)
 
 ML_LIBS=$(LIB_ML_CORE) $(LIB_ML_MATHS) $(LIB_ML_MODEL) $(LIB_ML_API)
 

bin/normalize/Makefile
@@ -7,7 +7,7 @@ include $(CPP_SRC_HOME)/mk/defines.mk
 
 TARGET=$(OBJS_DIR)/normalize$(EXE_EXT)
 
-INSTALL_DIR=$(CPP_PLATFORM_HOME)/bin
+INSTALL_DIR=$(CPP_PLATFORM_HOME)/$(EXE_DIR)
 
 ML_LIBS=$(LIB_ML_CORE) $(LIB_ML_MATHS) $(LIB_ML_MODEL) $(LIB_ML_API)
 

commit 6c586dc1bc6665eef5061a3459c12adc16d1b40a

[ML] Fix relative location to load resources from
Fixes a bug introduced in #593

lib/core/CResourceLocator.cc
@@ -30,7 +30,7 @@ std::string CResourceLocator::resourceDir() {
 
     // Look relative to the program that's running, assuming that the resource
     // directory is located relative to the directory the current program is in.
-    std::string productionDir(CProgName::progDir() + RESOURCE_RELATIVE_DIR);
+    std::string productionDir(CProgName::progDir() + '/' + RESOURCE_RELATIVE_DIR);
 
     // If the production directory doesn't exist, return the dev directory if
     // that does, but if neither exist return the production directory so the