a09929d5b43a293c2be21eed344364a9882d4a66
Tom Veasey <tveasey@elastic.co>
2019-10-09 13:33:51 +0100

    Further comment tweak

--- lib/maths/CBoostedTree.cc
+++ lib/maths/CBoostedTree.cc
@@ -67,7 +67,7 @@ double CArgMinMseImpl::value() const {
     //
     //    x^* = argmin_x{ sum_i{(a_i - (p_i + x))^2} + lambda * x^2 }
     //
-    // This is convex so there is one minimum where derivative w.r.t. x is zero
+    // This is convex so there is one minimum where the derivative w.r.t. x is zero
     // and x^* = 1 / (n + lambda) sum_i{ a_i - p_i }. Denoting the mean prediction
     // error m = 1/n sum_i{ a_i - p_i } we have x^* = n / (n + lambda) m.
 
