b8e04198fb6077484f8482c44469cb9c281caf63
Tom Veasey <tveasey@elastic.co>
2019-10-14 15:10:44 +0100

    Snapshot after final train and re-enable test

--- lib/api/unittest/CDataFrameAnalyzerTest.cc
+++ lib/api/unittest/CDataFrameAnalyzerTest.cc
@@ -972,14 +972,13 @@ void CDataFrameAnalyzerTest::testRunBoostedTreeRegressionTrainingWithStateRecove
 
     std::size_t numberRoundsPerHyperparameter{3};
 
-    TSizeVec intermediateIterations;
+    TSizeVec intermediateIterations{0, 0, 0};
     std::size_t finalIteration{0};
 
     test::CRandomNumbers rng;
 
-    // TODO re-enable case that all parameters are set.
     for (const auto& params :
-         {/*SHyperparameters{},*/ SHyperparameters{-1.0},
+         {SHyperparameters{}, SHyperparameters{-1.0},
           SHyperparameters{-1.0, -1.0}, SHyperparameters{-1.0, -1.0, -1.0}}) {
 
         LOG_DEBUG(<< "Number parameters to search = " << params.numberUnset());
@@ -995,8 +994,11 @@ void CDataFrameAnalyzerTest::testRunBoostedTreeRegressionTrainingWithStateRecove
                                   params.s_FeatureBagFraction, &persisterSupplier);
         };
 
-        finalIteration = params.numberUnset() * numberRoundsPerHyperparameter - 1;
-        rng.generateUniformSamples(0, finalIteration - 1, 3, intermediateIterations);
+        finalIteration = params.numberUnset() * numberRoundsPerHyperparameter;
+        finalIteration = std::max(finalIteration, std::size_t{1}) - 1;
+        if (finalIteration > 1) {
+            rng.generateUniformSamples(0, finalIteration - 1, 3, intermediateIterations);
+        }
 
         for (auto intermediateIteration : intermediateIterations) {
             LOG_DEBUG(<< "restart from " << intermediateIteration);
