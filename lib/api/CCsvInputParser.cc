c45ee0fbc6b1fcaa6967a550d051794466e84b1b
David Roberts <dave.roberts@elastic.co>
2019-11-06 13:06:23 -0500

    [ML] Move CSV line parser to core library (#808)
    The CCsvLineParser class used to be a nested class in
    api::CCsvInputParser.  However, it was public and used
    by other classes in the api library.  For #724 there is
    a need to move some of these classes that use the
    CCsvLineParser to the model library, so they can take
    advantage of the CResourceMonitor functionality.  This
    PR moves the CCsvLineParser class to the core library.
    
    Backport of #807

--- lib/api/CCsvInputParser.cc
+++ lib/api/CCsvInputParser.cc
@@ -17,8 +17,6 @@ namespace ml {
 namespace api {
 
 // Initialise statics
-const char CCsvInputParser::COMMA(',');
-const char CCsvInputParser::QUOTE('"');
 const char CCsvInputParser::RECORD_END('\n');
 const char CCsvInputParser::STRIP_BEFORE_END('\r');
 const size_t CCsvInputParser::WORK_BUFFER_SIZE(131072); // 128kB
@@ -188,7 +186,7 @@ bool CCsvInputParser::parseCsvRecordFromStream() {
             }
         }
 
-        quoteCount += std::count(m_WorkBufferPtr, endPtr, QUOTE);
+        quoteCount += std::count(m_WorkBufferPtr, endPtr, core::CCsvLineParser::QUOTE);
         if (delimPtr != nullptr) {
             m_WorkBufferPtr = delimPtr + 1;
 
@@ -268,115 +266,6 @@ bool CCsvInputParser::parseDataRecord(STR_VEC& values) {
     }
 
     this->gotData(true);
-
-    return true;
-}
-
-CCsvInputParser::CCsvLineParser::CCsvLineParser(char separator)
-    : m_Separator(separator), m_SeparatorAfterLastField(false), m_Line(nullptr),
-      m_LineCurrent(nullptr), m_LineEnd(nullptr), m_WorkFieldEnd(nullptr),
-      m_WorkFieldCapacity(0) {
-}
-
-void CCsvInputParser::CCsvLineParser::reset(const std::string& line) {
-    m_SeparatorAfterLastField = false;
-
-    m_Line = &line;
-    m_LineCurrent = line.data();
-    m_LineEnd = line.data() + line.length();
-
-    // Ensure that m_WorkField is big enough to hold the entire record, even if
-    // it turns out to be a single field - this avoids the need to check if it's
-    // big enough when it's populated (unlike std::vector or std::string)
-    size_t minCapacity(line.length() + 1);
-    if (m_WorkFieldCapacity < minCapacity) {
-        m_WorkFieldCapacity = minCapacity;
-        m_WorkField.reset(new char[minCapacity]);
-    }
-    m_WorkFieldEnd = m_WorkField.get();
-}
-
-bool CCsvInputParser::CCsvLineParser::parseNext(std::string& value) {
-    if (m_Line == nullptr) {
-        return false;
-    }
-    if (this->parseNextToken(m_LineEnd, m_LineCurrent) == false) {
-        return false;
-    }
-    value.assign(m_WorkField.get(), m_WorkFieldEnd - m_WorkField.get());
-    return true;
-}
-
-bool CCsvInputParser::CCsvLineParser::atEnd() const {
-    return m_LineCurrent == m_LineEnd;
-}
-
-bool CCsvInputParser::CCsvLineParser::parseNextToken(const char* end, const char*& current) {
-    m_WorkFieldEnd = m_WorkField.get();
-
-    if (current == end) {
-        // Allow one empty token at the end of a line
-        if (!m_SeparatorAfterLastField) {
-            LOG_ERROR(<< "Trying to read too many fields from record:" << core_t::LINE_ENDING
-                      << *m_Line);
-            return false;
-        }
-        m_SeparatorAfterLastField = false;
-        return true;
-    }
-
-    bool insideQuotes(false);
-    do {
-        if (insideQuotes) {
-            if (*current == QUOTE) {
-                // We need to look at the character after the quote
-                ++current;
-                if (current == end) {
-                    m_SeparatorAfterLastField = false;
-                    return true;
-                }
-
-                // The quoting state needs to be reversed UNLESS there are two
-                // adjacent quotes
-                if (*current != QUOTE) {
-                    insideQuotes = false;
-
-                    // Cater for the case where the character after the quote is
-                    // the separator
-                    if (*current == m_Separator) {
-                        ++current;
-                        m_SeparatorAfterLastField = true;
-                        return true;
-                    }
-                }
-            }
-
-            *(m_WorkFieldEnd++) = *current;
-        } else {
-            if (*current == m_Separator) {
-                ++current;
-                m_SeparatorAfterLastField = true;
-                return true;
-            }
-
-            if (*current == QUOTE) {
-                // We're not currently inside quotes so a quote puts us inside
-                // quotes regardless of the next character, and we never want to
-                // include this quote in the field value
-                insideQuotes = true;
-            } else {
-                *(m_WorkFieldEnd++) = *current;
-            }
-        }
-    } while (++current != end);
-
-    m_SeparatorAfterLastField = false;
-
-    // Inconsistency if the last character of the string is an unmatched quote
-    if (insideQuotes) {
-        LOG_ERROR(<< "Unmatched final quote in record:" << core_t::LINE_ENDING << *m_Line);
-        return false;
-    }
 
     return true;
 }
