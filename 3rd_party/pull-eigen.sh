848ea7804c3fa2fff24493587d3f369894c02dde
Ed Savage <ed.savage@elastic.co>
2019-11-15 11:36:28 +0000

    Ensuring we pull the Eigen 3.3.7 tag

--- 3rd_party/pull-eigen.sh
+++ 3rd_party/pull-eigen.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-git subtree pull --prefix 3rd_party/eigen git@github.com:eigenteam/eigen-git-mirror.git branches/3.3 --squash
+git subtree pull -P 3.3.7 --prefix 3rd_party/eigen git@github.com:eigenteam/eigen-git-mirror.git branches/3.3 --squash
 
 echo "Please check whether the licences are uptodate."
