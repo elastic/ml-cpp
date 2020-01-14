7bc74f8a82c5b6a3e8fb48b8b27ca175a78ef1d5
Dimitris Athanasiou <dimitris@elastic.co>
2020-01-14 10:49:03 +0200

    [7.x][ML] Rename to num_top_feature_importance_values (#932)
    Renames boosted tree param `top_feature_importance_values` to
    `num_top_feature_importance_values`.
    
    Backport of #932

--- lib/test/CDataFrameAnalysisSpecificationFactory.cc
+++ lib/test/CDataFrameAnalysisSpecificationFactory.cc
@@ -80,7 +80,7 @@ CDataFrameAnalysisSpecificationFactory::predictionSpec(
     double eta,
     std::size_t maximumNumberTrees,
     double featureBagFraction,
-    size_t topFeatureImportanceValues,
+    size_t numTopFeatureImportanceValues,
     TPersisterSupplier* persisterSupplier,
     TRestoreSearcherSupplier* restoreSearcherSupplier) {
 
@@ -131,9 +131,9 @@ CDataFrameAnalysisSpecificationFactory::predictionSpec(
         writer.Key(api::CDataFrameTrainBoostedTreeRunner::BAYESIAN_OPTIMISATION_RESTARTS);
         writer.Uint64(bayesianOptimisationRestarts);
     }
-    if (topFeatureImportanceValues > 0) {
-        writer.Key(api::CDataFrameTrainBoostedTreeRunner::TOP_FEATURE_IMPORTANCE_VALUES);
-        writer.Uint64(topFeatureImportanceValues);
+    if (numTopFeatureImportanceValues > 0) {
+        writer.Key(api::CDataFrameTrainBoostedTreeRunner::NUM_TOP_FEATURE_IMPORTANCE_VALUES);
+        writer.Uint64(numTopFeatureImportanceValues);
     }
     writer.EndObject();
 
