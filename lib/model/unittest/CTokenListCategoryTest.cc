52449763a64f358a4c4c6ff363c9d855ca0b78f2
David Roberts <dave.roberts@elastic.co>
2020-01-29 14:58:46 +0000

    [ML] Use largest ordered subset of categorization tokens for regex
    Previously, when reducing the subset of common tokens that
    were in the same order for all messages in a category the
    subset matching from the start of the original category
    tokens would be used.  This was highly suboptimal in the
    case of a message being added to the category that
    contained the first token in the original category tokens,
    but near the end of the new message rather than near the
    beginning.
    
    This PR changes categorization such that the largest
    ordered subset is chosen, even if this involves excluding
    tokens that occurred near the start of the original category
    tokens.
    
    In practice this means that if a category contains messages
    of the form "a b c d e" and a message "b c d e a" is
    considered close enough to match then the regex chosen that
    matches all messages in the category will now be
    ".*?b.+?c.+?d.+?e.*" when previously it would have been
    ".*?a.*".
    
    This change alters the categorization state format, by adding
    the begin index for the ordered subset to complement the end
    index.  Upgrade is seamless, because effectively the begin
    index always used to be 0, so 0 can be assumed in the case
    of an older version state being restored.  Downgrade (if the
    job relocates to an older node in a mixed version cluster)
    means that the begin index will get reset to 0.  This will
    not cause a crash, but may mean that extra tokens are
    erroneously reported in a regex generated after downgrade.
    However, the problem will self-correct when a message that
    doesn't match the erroneous regex is seen after the job
    eventually relocates to a newer node again.
    
    Backport of #970

--- lib/model/unittest/CTokenListCategoryTest.cc
+++ lib/model/unittest/CTokenListCategoryTest.cc
@@ -19,19 +19,20 @@ BOOST_AUTO_TEST_CASE(testCommonTokensSameOrder) {
 
     std::string baseString{"she sells seashells on the seashore"};
     ml::model::CTokenListCategory::TSizeSizePrVec baseTokenIds{
-        {0 /* she */, 1}, {1 /* sells */, 1}, {2 /* seashells */, 1},
-        {3 /* on */, 1},  {4 /* the */, 1},   {5 /* seashore */, 1}};
+        {0 /* she */, 2}, {1 /* sells */, 2}, {2 /* seashells */, 2},
+        {3 /* on */, 2},  {4 /* the */, 2},   {5 /* seashore */, 2}};
     ml::model::CTokenListCategory::TSizeSizeMap baseUniqueTokenIds(
         baseTokenIds.begin(), baseTokenIds.end());
 
-    ml::model::CTokenListCategory category(false, baseString, baseString.length(), baseTokenIds,
-                                           baseTokenIds.size(), baseUniqueTokenIds);
+    ml::model::CTokenListCategory category(false, baseString, baseString.length(),
+                                           baseTokenIds, baseTokenIds.size() * 2,
+                                           baseUniqueTokenIds);
 
     std::string newString{"she sells ice cream on the seashore"};
     ml::model::CTokenListCategory::TSizeSizePrVec newTokenIds{
-        {0 /* she */, 1},     {1 /* sells */, 1}, {6 /* ice */, 1},
-        {7 /* cream */, 1},   {3 /* on */, 1},    {4 /* the */, 1},
-        {5 /* seashore */, 1}};
+        {0 /* she */, 2},     {1 /* sells */, 2}, {6 /* ice */, 2},
+        {7 /* cream */, 2},   {3 /* on */, 2},    {4 /* the */, 2},
+        {5 /* seashore */, 2}};
     ml::model::CTokenListCategory::TSizeSizeMap newUniqueTokenIds(
         newTokenIds.begin(), newTokenIds.end());
 
@@ -41,15 +42,15 @@ BOOST_AUTO_TEST_CASE(testCommonTokensSameOrder) {
     BOOST_REQUIRE_EQUAL(baseString, category.baseString());
     BOOST_REQUIRE_EQUAL(ml::core::CContainerPrinter::print(baseTokenIds),
                         ml::core::CContainerPrinter::print(category.baseTokenIds()));
-    BOOST_REQUIRE_EQUAL(baseTokenIds.size(), category.baseWeight());
+    BOOST_REQUIRE_EQUAL(baseTokenIds.size() * 2, category.baseWeight());
     ml::model::CTokenListCategory::TSizeSizeMap expectedCommonUniqueTokenIds{
-        {0 /* she */, 1}, {1 /* sells */, 1}, {3 /* on */, 1}, {4 /* the */, 1}, {5 /* seashore */, 1}};
+        {0 /* she */, 2}, {1 /* sells */, 2}, {3 /* on */, 2}, {4 /* the */, 2}, {5 /* seashore */, 2}};
     BOOST_REQUIRE_EQUAL(
         ml::core::CContainerPrinter::print(expectedCommonUniqueTokenIds),
         ml::core::CContainerPrinter::print(category.commonUniqueTokenIds()));
-    BOOST_REQUIRE_EQUAL(expectedCommonUniqueTokenIds.size(),
+    BOOST_REQUIRE_EQUAL(expectedCommonUniqueTokenIds.size() * 2,
                         category.commonUniqueTokenWeight());
-    BOOST_REQUIRE_EQUAL(baseUniqueTokenIds.size(), category.origUniqueTokenWeight());
+    BOOST_REQUIRE_EQUAL(baseUniqueTokenIds.size() * 2, category.origUniqueTokenWeight());
     BOOST_REQUIRE_EQUAL(std::max(baseString.length(), newString.length()),
                         category.maxStringLen());
     ml::model::CTokenListCategory::TSizeSizePr expectedOrderedCommonTokenBounds{0, 6};
@@ -62,37 +63,102 @@ BOOST_AUTO_TEST_CASE(testCommonTokensDifferentOrder) {
 
     std::string baseString{"she sells seashells on the seashore"};
     ml::model::CTokenListCategory::TSizeSizePrVec baseTokenIds{
-        {0 /* she */, 1}, {1 /* sells */, 1}, {2 /* seashells */, 1},
-        {3 /* on */, 1},  {4 /* the */, 1},   {5 /* seashore */, 1}};
+        {0 /* she */, 2}, {1 /* sells */, 2}, {2 /* seashells */, 2},
+        {3 /* on */, 2},  {4 /* the */, 2},   {5 /* seashore */, 2}};
     ml::model::CTokenListCategory::TSizeSizeMap baseUniqueTokenIds(
         baseTokenIds.begin(), baseTokenIds.end());
 
-    ml::model::CTokenListCategory category(false, baseString, baseString.length(), baseTokenIds,
-                                           baseTokenIds.size(), baseUniqueTokenIds);
+    ml::model::CTokenListCategory category(false, baseString, baseString.length(),
+                                           baseTokenIds, baseTokenIds.size() * 2,
+                                           baseUniqueTokenIds);
 
-    std::string newString{"sells seashells on the seashore, she does"};
-    ml::model::CTokenListCategory::TSizeSizePrVec newTokenIds{
-        {1 /* sells */, 1}, {2 /* seashells */, 1}, {3 /* on */, 1},
-        {4 /* the */, 1},   {5 /* seashore */, 1},  {0 /* she */, 1},
-        {6 /* does */, 1}};
-    ml::model::CTokenListCategory::TSizeSizeMap newUniqueTokenIds(
-        newTokenIds.begin(), newTokenIds.end());
+    std::string newString1{"sells seashells on the seashore, she does"};
+    ml::model::CTokenListCategory::TSizeSizePrVec newTokenIds1{
+        {1 /* sells */, 2}, {2 /* seashells */, 2}, {3 /* on */, 2},
+        {4 /* the */, 2},   {5 /* seashore */, 2},  {0 /* she */, 2},
+        {6 /* does */, 2}};
+    ml::model::CTokenListCategory::TSizeSizeMap newUniqueTokenIds1(
+        newTokenIds1.begin(), newTokenIds1.end());
 
-    BOOST_TEST_REQUIRE(category.addString(false, newString, newString.length(),
-                                          newTokenIds, newUniqueTokenIds));
+    BOOST_TEST_REQUIRE(category.addString(false, newString1, newString1.length(),
+                                          newTokenIds1, newUniqueTokenIds1));
 
     BOOST_REQUIRE_EQUAL(baseString, category.baseString());
     BOOST_REQUIRE_EQUAL(ml::core::CContainerPrinter::print(baseTokenIds),
                         ml::core::CContainerPrinter::print(category.baseTokenIds()));
-    BOOST_REQUIRE_EQUAL(baseTokenIds.size(), category.baseWeight());
+    BOOST_REQUIRE_EQUAL(baseTokenIds.size() * 2, category.baseWeight());
     BOOST_REQUIRE_EQUAL(ml::core::CContainerPrinter::print(baseUniqueTokenIds),
                         ml::core::CContainerPrinter::print(category.commonUniqueTokenIds()));
-    BOOST_REQUIRE_EQUAL(baseUniqueTokenIds.size(), category.commonUniqueTokenWeight());
-    BOOST_REQUIRE_EQUAL(baseUniqueTokenIds.size(), category.origUniqueTokenWeight());
-    BOOST_REQUIRE_EQUAL(std::max(baseString.length(), newString.length()),
+    BOOST_REQUIRE_EQUAL(baseUniqueTokenIds.size() * 2, category.commonUniqueTokenWeight());
+    BOOST_REQUIRE_EQUAL(baseUniqueTokenIds.size() * 2, category.origUniqueTokenWeight());
+    BOOST_REQUIRE_EQUAL(std::max(baseString.length(), newString1.length()),
                         category.maxStringLen());
-    // FIXME: {0, 1} is clearly sub-optimal here; {1, 6} would be much better
-    ml::model::CTokenListCategory::TSizeSizePr expectedOrderedCommonTokenBounds{0, 1};
+    ml::model::CTokenListCategory::TSizeSizePr expectedOrderedCommonTokenBounds{1, 6};
+    BOOST_REQUIRE_EQUAL(
+        ml::core::CContainerPrinter::print(expectedOrderedCommonTokenBounds),
+        ml::core::CContainerPrinter::print(category.orderedCommonTokenBounds()));
+
+    std::string newString2{"nice seashells can be found near the seashore"};
+    ml::model::CTokenListCategory::TSizeSizePrVec newTokenIds2{
+        {7 /* nice */, 2}, {2 /* seashells */, 2}, {8 /* can */, 2},
+        {9 /* be */, 2},   {10 /* found */, 2},    {11 /* near */, 2},
+        {4 /* the */, 2},  {5 /* seashore */, 2}};
+    ml::model::CTokenListCategory::TSizeSizeMap newUniqueTokenIds2(
+        newTokenIds2.begin(), newTokenIds2.end());
+
+    BOOST_TEST_REQUIRE(category.addString(false, newString2, newString2.length(),
+                                          newTokenIds2, newUniqueTokenIds2));
+
+    BOOST_REQUIRE_EQUAL(baseString, category.baseString());
+    BOOST_REQUIRE_EQUAL(ml::core::CContainerPrinter::print(baseTokenIds),
+                        ml::core::CContainerPrinter::print(category.baseTokenIds()));
+    BOOST_REQUIRE_EQUAL(baseTokenIds.size() * 2, category.baseWeight());
+    ml::model::CTokenListCategory::TSizeSizeMap expectedCommonUniqueTokenIds{
+        {2 /* seashells */, 2}, {4 /* the */, 2}, {5 /* seashore */, 2}};
+    BOOST_REQUIRE_EQUAL(
+        ml::core::CContainerPrinter::print(expectedCommonUniqueTokenIds),
+        ml::core::CContainerPrinter::print(category.commonUniqueTokenIds()));
+    BOOST_REQUIRE_EQUAL(expectedCommonUniqueTokenIds.size() * 2,
+                        category.commonUniqueTokenWeight());
+    BOOST_REQUIRE_EQUAL(baseUniqueTokenIds.size() * 2, category.origUniqueTokenWeight());
+    BOOST_REQUIRE_EQUAL(std::max(newString1.length(), newString2.length()),
+                        category.maxStringLen());
+    // The bounds go from {1, 6} to {2, 6} even though there are now only 3
+    // common tokens, because the bounds reference the base token indices,
+    // and the range needs to be filtered to exclude tokens that are not common.
+    // (When the real reverse search is created tokens may also be filtered if
+    // their cost is too high for the available budget, so this doesn't create
+    // too much complexity outside of the unit test.)
+    expectedOrderedCommonTokenBounds = {2, 6};
+    BOOST_REQUIRE_EQUAL(
+        ml::core::CContainerPrinter::print(expectedOrderedCommonTokenBounds),
+        ml::core::CContainerPrinter::print(category.orderedCommonTokenBounds()));
+
+    std::string newString3{"the rock"};
+    ml::model::CTokenListCategory::TSizeSizePrVec newTokenIds3{{4 /* the */, 2},
+                                                               {12 /* rock */, 2}};
+    ml::model::CTokenListCategory::TSizeSizeMap newUniqueTokenIds3(
+        newTokenIds3.begin(), newTokenIds3.end());
+
+    BOOST_TEST_REQUIRE(category.addString(false, newString3, newString3.length(),
+                                          newTokenIds3, newUniqueTokenIds3));
+
+    BOOST_REQUIRE_EQUAL(baseString, category.baseString());
+    BOOST_REQUIRE_EQUAL(ml::core::CContainerPrinter::print(baseTokenIds),
+                        ml::core::CContainerPrinter::print(category.baseTokenIds()));
+    BOOST_REQUIRE_EQUAL(baseTokenIds.size() * 2, category.baseWeight());
+    expectedCommonUniqueTokenIds = {{4 /* the */, 2}};
+    BOOST_REQUIRE_EQUAL(
+        ml::core::CContainerPrinter::print(expectedCommonUniqueTokenIds),
+        ml::core::CContainerPrinter::print(category.commonUniqueTokenIds()));
+    BOOST_REQUIRE_EQUAL(expectedCommonUniqueTokenIds.size() * 2,
+                        category.commonUniqueTokenWeight());
+    BOOST_REQUIRE_EQUAL(baseUniqueTokenIds.size() * 2, category.origUniqueTokenWeight());
+    BOOST_REQUIRE_EQUAL(std::max(newString2.length(), newString3.length()),
+                        category.maxStringLen());
+    // The bounds go from {2, 6} to {4, 5} as there's now only one common token
+    // and it's in position 4.
+    expectedOrderedCommonTokenBounds = {4, 5};
     BOOST_REQUIRE_EQUAL(
         ml::core::CContainerPrinter::print(expectedOrderedCommonTokenBounds),
         ml::core::CContainerPrinter::print(category.orderedCommonTokenBounds()));
