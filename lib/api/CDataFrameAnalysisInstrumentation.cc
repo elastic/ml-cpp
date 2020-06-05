e2f7ed948516f839dc15e06afebe5b6e4388aa3d
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2020-06-05 16:44:57 +0200

    remove log info

--- lib/api/CDataFrameAnalysisInstrumentation.cc
+++ lib/api/CDataFrameAnalysisInstrumentation.cc
@@ -30,7 +30,7 @@ namespace {
 using TStrVec = std::vector<std::string>;
 
 // clang-format off
-const double MEMORY_LIMIT_INCREMENT{1.1};
+const double MEMORY_LIMIT_INCREMENT{1.25}; // request 25% more memory
 
 const std::string CLASSIFICATION_STATS_TAG{"classification_stats"};
 const std::string HYPERPARAMETERS_TAG{"hyperparameters"};
