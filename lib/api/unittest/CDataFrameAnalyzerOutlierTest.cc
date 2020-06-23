637afc6fba080db5b0e89f2ecc61f95a7b628369
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2020-06-23 16:29:28 +0200

    Outlier detection unit test and enhancement note

--- lib/api/unittest/CDataFrameAnalyzerOutlierTest.cc
+++ lib/api/unittest/CDataFrameAnalyzerOutlierTest.cc
@@ -514,6 +514,7 @@ BOOST_AUTO_TEST_CASE(testErrors) {
 
     // Memory limit exceeded.
     {
+        output.str("");
         errors.clear();
         api::CDataFrameAnalyzer analyzer{
             test::CDataFrameAnalysisSpecificationFactory{}.memoryLimit(10000).outlierSpec(),
@@ -535,6 +536,31 @@ BOOST_AUTO_TEST_CASE(testErrors) {
             }
         }
         BOOST_TEST_REQUIRE(memoryLimitExceed);
+
+        // verify memory status change
+        rapidjson::Document results;
+        rapidjson::ParseResult ok(results.Parse(output.str()));
+        BOOST_TEST_REQUIRE(static_cast<bool>(ok) == true);
+        bool memoryStatusOk{false};
+        bool memoryStatusHardLimit{false};
+        bool advisedMemoryLimitAvailable{false};
+        for (const auto& result : results.GetArray()) {
+            if (result.HasMember("analytics_memory_usage")) {
+                std::string status{result["analytics_memory_usage"]["status"].GetString()};
+                if (status == "ok") {
+                    memoryStatusOk = true;
+                } else if (status == "hard-limit") {
+                    memoryStatusHardLimit = true;
+                    if (result["analytics_memory_usage"].HasMember("advised_limit_bytes") &&
+                        result["analytics_memory_usage"]["advised_limit_bytes"].GetInt() > 0) {
+                        advisedMemoryLimitAvailable = true;
+                    }
+                }
+            }
+        }
+        BOOST_TEST_REQUIRE(memoryStatusOk);
+        BOOST_TEST_REQUIRE(memoryStatusHardLimit);
+        BOOST_TEST_REQUIRE(advisedMemoryLimitAvailable);
     }
 }
 
