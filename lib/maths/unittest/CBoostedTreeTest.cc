8fde0f55faa73590bc3d3c5cc0480ed2cf604790
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2020-07-15 17:10:47 +0200

    includes clean up

--- lib/maths/unittest/CBoostedTreeTest.cc
+++ lib/maths/unittest/CBoostedTreeTest.cc
@@ -4,8 +4,6 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-#include "core/CContainerPrinter.h"
-#include "maths/CSpline.h"
 #include <core/CDataFrame.h>
 #include <core/CJsonStatePersistInserter.h>
 #include <core/CLogger.h>
