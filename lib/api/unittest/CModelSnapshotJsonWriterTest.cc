ae6dd0645d85b0751c4895f3aabd3c64bc8bb351
David Roberts <dave.roberts@elastic.co>
2020-12-10 13:32:13 +0000

    Removing unnecessary casting/temporaries from test assertions

--- lib/api/unittest/CModelSnapshotJsonWriterTest.cc
+++ lib/api/unittest/CModelSnapshotJsonWriterTest.cc
@@ -15,7 +15,6 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <cstdint>
 #include <string>
 
 BOOST_AUTO_TEST_SUITE(CModelSnapshotJsonWriterTest)
@@ -55,7 +54,7 @@ BOOST_AUTO_TEST_CASE(testWrite) {
             core_t::TTime(1521046309),
             "the snapshot description",
             "test_snapshot_id",
-            size_t(15), // # docs
+            15, // # docs
             modelSizeStats,
             "some normalizer state",
             core_t::TTime(1521046409), // last record time
@@ -71,7 +70,7 @@ BOOST_AUTO_TEST_CASE(testWrite) {
     arrayDoc.Parse<rapidjson::kParseDefaultFlags>(sstream.str().c_str());
 
     BOOST_TEST_REQUIRE(arrayDoc.IsArray());
-    BOOST_REQUIRE_EQUAL(rapidjson::SizeType(1), arrayDoc.Size());
+    BOOST_REQUIRE_EQUAL(1, arrayDoc.Size());
 
     const rapidjson::Value& object = arrayDoc[rapidjson::SizeType(0)];
     BOOST_TEST_REQUIRE(object.IsObject());
@@ -79,92 +78,71 @@ BOOST_AUTO_TEST_CASE(testWrite) {
     BOOST_TEST_REQUIRE(object.HasMember("model_snapshot"));
     const rapidjson::Value& snapshot = object["model_snapshot"];
     BOOST_TEST_REQUIRE(snapshot.HasMember("job_id"));
-    BOOST_REQUIRE_EQUAL(std::string("job"), std::string(snapshot["job_id"].GetString()));
+    BOOST_REQUIRE_EQUAL("job", snapshot["job_id"].GetString());
     BOOST_TEST_REQUIRE(snapshot.HasMember("min_version"));
-    BOOST_REQUIRE_EQUAL(std::string("6.3.0"),
-                        std::string(snapshot["min_version"].GetString()));
+    BOOST_REQUIRE_EQUAL("6.3.0", snapshot["min_version"].GetString());
     BOOST_TEST_REQUIRE(snapshot.HasMember("snapshot_id"));
-    BOOST_REQUIRE_EQUAL(std::string("test_snapshot_id"),
-                        std::string(snapshot["snapshot_id"].GetString()));
+    BOOST_REQUIRE_EQUAL("test_snapshot_id", snapshot["snapshot_id"].GetString());
     BOOST_TEST_REQUIRE(snapshot.HasMember("snapshot_doc_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(15), snapshot["snapshot_doc_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(15, snapshot["snapshot_doc_count"].GetUint64());
     BOOST_TEST_REQUIRE(snapshot.HasMember("timestamp"));
-    BOOST_REQUIRE_EQUAL(std::int64_t(1521046309000), snapshot["timestamp"].GetInt64());
+    BOOST_REQUIRE_EQUAL(1521046309000, snapshot["timestamp"].GetInt64());
     BOOST_TEST_REQUIRE(snapshot.HasMember("description"));
-    BOOST_REQUIRE_EQUAL(std::string("the snapshot description"),
-                        std::string(snapshot["description"].GetString()));
+    BOOST_REQUIRE_EQUAL("the snapshot description", snapshot["description"].GetString());
     BOOST_TEST_REQUIRE(snapshot.HasMember("latest_record_time_stamp"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(1521046409000),
-                        snapshot["latest_record_time_stamp"].GetUint64());
+    BOOST_REQUIRE_EQUAL(1521046409000, snapshot["latest_record_time_stamp"].GetInt64());
     BOOST_TEST_REQUIRE(snapshot.HasMember("latest_result_time_stamp"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(1521040000000),
-                        snapshot["latest_result_time_stamp"].GetUint64());
+    BOOST_REQUIRE_EQUAL(1521040000000, snapshot["latest_result_time_stamp"].GetInt64());
 
     BOOST_TEST_REQUIRE(snapshot.HasMember("model_size_stats"));
     const rapidjson::Value& modelSizeStats = snapshot["model_size_stats"];
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("job_id"));
-    BOOST_REQUIRE_EQUAL(std::string("job"),
-                        std::string(modelSizeStats["job_id"].GetString()));
+    BOOST_REQUIRE_EQUAL("job", modelSizeStats["job_id"].GetString());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("model_bytes"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(20000), modelSizeStats["model_bytes"].GetUint64());
+    BOOST_REQUIRE_EQUAL(20000, modelSizeStats["model_bytes"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("peak_model_bytes"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(60000),
-                        modelSizeStats["peak_model_bytes"].GetUint64());
+    BOOST_REQUIRE_EQUAL(60000, modelSizeStats["peak_model_bytes"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("total_by_field_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(3),
-                        modelSizeStats["total_by_field_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(3, modelSizeStats["total_by_field_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("total_partition_field_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(1),
-                        modelSizeStats["total_partition_field_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(1, modelSizeStats["total_partition_field_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("total_over_field_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(150),
-                        modelSizeStats["total_over_field_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(150, modelSizeStats["total_over_field_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("bucket_allocation_failures_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(4),
-                        modelSizeStats["bucket_allocation_failures_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(4, modelSizeStats["bucket_allocation_failures_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("memory_status"));
-    BOOST_REQUIRE_EQUAL(std::string("ok"),
-                        std::string(modelSizeStats["memory_status"].GetString()));
+    BOOST_REQUIRE_EQUAL("ok", modelSizeStats["memory_status"].GetString());
     BOOST_REQUIRE_EQUAL(false, modelSizeStats.HasMember("assignment_memory_basis"));
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("model_bytes_exceeded"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(0),
-                        modelSizeStats["model_bytes_exceeded"].GetUint64());
+    BOOST_REQUIRE_EQUAL(0, modelSizeStats["model_bytes_exceeded"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("model_bytes_memory_limit"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(50000),
-                        modelSizeStats["model_bytes_memory_limit"].GetUint64());
+    BOOST_REQUIRE_EQUAL(50000, modelSizeStats["model_bytes_memory_limit"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("categorized_doc_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(1000),
-                        modelSizeStats["categorized_doc_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(1000, modelSizeStats["categorized_doc_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("total_category_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(100),
-                        modelSizeStats["total_category_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(100, modelSizeStats["total_category_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("frequent_category_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(7),
-                        modelSizeStats["frequent_category_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(7, modelSizeStats["frequent_category_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("rare_category_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(13),
-                        modelSizeStats["rare_category_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(13, modelSizeStats["rare_category_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("dead_category_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(2), modelSizeStats["dead_category_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(2, modelSizeStats["dead_category_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("failed_category_count"));
-    BOOST_REQUIRE_EQUAL(std::uint64_t(8),
-                        modelSizeStats["failed_category_count"].GetUint64());
+    BOOST_REQUIRE_EQUAL(8, modelSizeStats["failed_category_count"].GetUint64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("memory_status"));
-    BOOST_REQUIRE_EQUAL(std::string("warn"),
-                        std::string(modelSizeStats["categorization_status"].GetString()));
+    BOOST_REQUIRE_EQUAL("warn", modelSizeStats["categorization_status"].GetString());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("timestamp"));
-    BOOST_REQUIRE_EQUAL(std::int64_t(1521046309000), modelSizeStats["timestamp"].GetInt64());
+    BOOST_REQUIRE_EQUAL(1521046309000, modelSizeStats["timestamp"].GetInt64());
     BOOST_TEST_REQUIRE(modelSizeStats.HasMember("log_time"));
 
     BOOST_TEST_REQUIRE(snapshot.HasMember("quantiles"));
     const rapidjson::Value& quantiles = snapshot["quantiles"];
     BOOST_TEST_REQUIRE(quantiles.HasMember("job_id"));
-    BOOST_REQUIRE_EQUAL(std::string("job"), std::string(quantiles["job_id"].GetString()));
+    BOOST_REQUIRE_EQUAL("job", quantiles["job_id"].GetString());
     BOOST_TEST_REQUIRE(quantiles.HasMember("quantile_state"));
-    BOOST_REQUIRE_EQUAL(std::string("some normalizer state"),
-                        std::string(quantiles["quantile_state"].GetString()));
+    BOOST_REQUIRE_EQUAL("some normalizer state", quantiles["quantile_state"].GetString());
     BOOST_TEST_REQUIRE(quantiles.HasMember("timestamp"));
-    BOOST_REQUIRE_EQUAL(std::int64_t(1521040000000), quantiles["timestamp"].GetInt64());
+    BOOST_REQUIRE_EQUAL(1521040000000, quantiles["timestamp"].GetInt64());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
