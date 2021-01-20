5ed55472c4db1155341d536e09d3b6daa4cd8686
Tom Veasey <tveasey@users.noreply.github.com>
2021-01-20 18:55:35 +0000

    [ML] Correct a bug in upgrade from pre 6.3 state for the lat_long function (#1681) (#1684)

--- lib/maths/CTimeSeriesModel.cc
+++ lib/maths/CTimeSeriesModel.cc
@@ -1451,6 +1451,11 @@ const CPrior& CUnivariateTimeSeriesModel::residualModel() const {
     return *m_ResidualModel;
 }
 
+const CUnivariateTimeSeriesModel::TDecayRateController2Ary*
+CUnivariateTimeSeriesModel::decayRateControllers() const {
+    return m_Controllers.get();
+}
+
 CUnivariateTimeSeriesModel::CUnivariateTimeSeriesModel(const CUnivariateTimeSeriesModel& other,
                                                        std::size_t id,
                                                        bool isForForecast)
@@ -2751,7 +2756,7 @@ bool CMultivariateTimeSeriesModel::acceptRestoreTraverser(const SModelRestorePar
             RESTORE_SETUP_TEARDOWN(
                 CONTROLLER_OLD_TAG,
                 m_Controllers = std::make_unique<TDecayRateController2Ary>(),
-                core::CPersistUtils::restore(CONTROLLER_6_3_TAG, *m_Controllers, traverser),
+                core::CPersistUtils::restore(CONTROLLER_OLD_TAG, *m_Controllers, traverser),
                 /**/)
             RESTORE_SETUP_TEARDOWN(
                 TREND_OLD_TAG, m_TrendModel.push_back(TDecompositionPtr()),
@@ -2847,6 +2852,11 @@ CMultivariateTimeSeriesModel::trendModel() const {
 
 const CMultivariatePrior& CMultivariateTimeSeriesModel::residualModel() const {
     return *m_ResidualModel;
+}
+
+const CMultivariateTimeSeriesModel::TDecayRateController2Ary*
+CMultivariateTimeSeriesModel::decayRateControllers() const {
+    return m_Controllers.get();
 }
 
 CMultivariateTimeSeriesModel::EUpdateResult
