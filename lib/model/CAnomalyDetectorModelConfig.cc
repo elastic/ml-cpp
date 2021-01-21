37b22b19693c162ceaec30b871bfad12ae167a3c
Ed Savage <ed.savage@elastic.co>
2021-01-21 09:14:29 +0000

    [ML] Parse JSON format config updates (#1682)
    * Expect configuration update requests to be received in JSON formatted
    strings. 
    * Remove support for old, ini file parsing.
    
    Relates elastic/elasticsearch#67721, #1253

--- lib/model/CAnomalyDetectorModelConfig.cc
+++ lib/model/CAnomalyDetectorModelConfig.cc
@@ -375,7 +375,9 @@ bool CAnomalyDetectorModelConfig::init(const boost::property_tree::ptree& propTr
 void CAnomalyDetectorModelConfig::configureModelPlot(bool modelPlotEnabled,
                                                      bool annotationsEnabled,
                                                      const std::string& terms) {
-    if (modelPlotEnabled) {
+    m_ModelPlotEnabled = modelPlotEnabled;
+
+    if (m_ModelPlotEnabled) {
         m_ModelPlotBoundsPercentile = maths::CModel::DEFAULT_BOUNDS_PERCENTILE;
     }
 
@@ -390,6 +392,8 @@ void CAnomalyDetectorModelConfig::configureModelPlot(bool modelPlotEnabled,
     if (remainder.empty() == false) {
         tokens.push_back(remainder);
     }
+
+    m_ModelPlotTerms.clear();
     for (const auto& token : tokens) {
         m_ModelPlotTerms.insert(token);
     }
@@ -479,6 +483,10 @@ bool CAnomalyDetectorModelConfig::configureModelPlot(const boost::property_tree:
     }
 
     return true;
+}
+
+bool CAnomalyDetectorModelConfig::modelPlotEnabled() const {
+    return m_ModelPlotEnabled;
 }
 
 bool CAnomalyDetectorModelConfig::modelPlotAnnotationsEnabled() const {
