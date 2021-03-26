7ac0d7111a4ac21c62575f994ceaa21827bf870c
David Roberts <dave.roberts@elastic.co>
2021-03-26 13:48:43 +0000

    [ML] Restructure Jenkins scripts
    This is the first phase of a refactor that will eventually enable
    the same Jenkins setup in elasticsearch-ci to run:
    
    1. PR builds
    2. Branch builds that cache artifacts for developers
    3. Daily debug builds with assertions enabled
    4. Builds for release to end users
    
    A secondary goal is to speed up CI by spreading the build over
    more servers.
    
    In this first stage of the changes:
    
    1. The extra options needed by debug builds and release builds are
       passed through to the low level build commands
    2. The cross compile steps are split into a separate build script,
       which, eventually, will be run on a different server
    
    Completing this project without lengthy ML CI downtime will
    require a series of changes alternating between the ml-cpp repo
    and the Jenkins configuration.  This is just the first step of
    many.

--- dev-tools/jenkins_ci.ps1
+++ dev-tools/jenkins_ci.ps1
@@ -6,14 +6,15 @@
 
 # The Windows part of ML C++ CI does the following:
 #
-# 1. If this is not a PR build, obtain credentials from Vault for the accessing
-#    S3
+# 1. If this is not a PR build nor a debug build, obtain credentials from Vault
+#    for the accessing S3
 # 2. Build and unit test the Windows version of the C++
-# 3. If this is not a PR build, upload the build to the artifacts directory on
-#    S3 that subsequent Java builds will download the C++ components from
+# 3. If this is not a PR build nor a debug build, upload the builds to the
+#    artifacts directory on S3 that subsequent Java builds will download the C++
+#    components from
 
-# If this isn't a PR build then obtain credentials from Vault
-if (!(Test-Path Env:PR_AUTHOR)) {
+# If this isn't a PR build or a debug build then obtain credentials from Vault
+if (!(Test-Path Env:PR_AUTHOR) -And !(Test-Path Env:ML_DEBUG)) {
     # Generate a Vault token
     $Env:VAULT_TOKEN=& vault write -field=token auth/approle/login "role_id=$Env:VAULT_ROLE_ID" "secret_id=$Env:VAULT_SECRET_ID"
     if ($LastExitCode -ne 0) {
@@ -56,21 +57,52 @@ if (!(Test-Path Env:BUILD_SNAPSHOT)) {
     $Env:BUILD_SNAPSHOT="true"
 }
 
+# Default to running tests
+if (!(Test-Path Env:RUN_TESTS)) {
+    $Env:RUN_TESTS="true"
+}
+
+# Default to no version qualifier
+if (!(Test-Path Env:VERSION_QUALIFIER)) {
+    $Env:VERSION_QUALIFIER=""
+} elseif (Test-Path Env:PR_AUTHOR) {
+    Write-Output "VERSION_QUALIFIER should not be set in PR builds: was $Env:VERSION_QUALIFIER"
+    Exit 2
+}
+
+if (Test-Path Env:PR_AUTHOR) {
+    if ($Env:RUN_TESTS -eq "false") {
+        Write-Output "RUN_TESTS should not be false PR builds"
+        Exit 3
+    }
+    $Tasks="clean", "buildZip", "check"
+} elseif ($Env:RUN_TESTS -eq "false") {
+    $Tasks="clean", "buildZip", "buildZipSymbols"
+} else {
+    $Tasks="clean", "buildZip", "buildZipSymbols", "check"
+}
+
+if (Test-Path Env:ML_DEBUG) {
+    $DebugOption="-Dbuild.ml_debug=$Env:ML_DEBUG"
+} else {
+    $DebugOption=""
+}
+
 # The exit code of the gradlew commands is checked explicitly, and their
 # stderr is treated as an error by PowerShell without this
 $ErrorActionPreference="Continue"
 
 # Run the build and unit tests
 # The | % { "$_" } at the end converts any error objects on stderr to strings
-& ".\gradlew.bat" --info "-Dbuild.snapshot=$Env:BUILD_SNAPSHOT" clean buildZip buildZipSymbols check 2>&1 | % { "$_" }
+& ".\gradlew.bat" --info "-Dbuild.version_qualifier=$Env:VERSION_QUALIFIER" "-Dbuild.snapshot=$Env:BUILD_SNAPSHOT" $DebugOption $Tasks 2>&1 | % { "$_" }
 if ($LastExitCode -ne 0) {
     Exit $LastExitCode
 }
 
-# If this isn't a PR build then upload the artifacts
-if (!(Test-Path Env:PR_AUTHOR)) {
+# If this isn't a PR build and isn't a debug build then upload the artifacts
+if (!(Test-Path Env:PR_AUTHOR) -And !(Test-Path Env:ML_DEBUG)) {
     # The | % { "$_" } at the end converts any error objects on stderr to strings
-    & ".\gradlew.bat" --info -b "upload.gradle" "-Dbuild.snapshot=$Env:BUILD_SNAPSHOT" upload 2>&1 | % { "$_" }
+    & ".\gradlew.bat" --info -b "upload.gradle" "-Dbuild.version_qualifier=$Env:VERSION_QUALIFIER" "-Dbuild.snapshot=$Env:BUILD_SNAPSHOT" upload 2>&1 | % { "$_" }
     if ($LastExitCode -ne 0) {
         Exit $LastExitCode
     }
