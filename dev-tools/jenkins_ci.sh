a7e892cf80b8f73bc64605f683a5a2e35da580c8
David Roberts <dave.roberts@elastic.co>
2021-03-30 16:46:28 +0100

    [7.12][ML] Restructure Jenkins scripts (#1839)
    This is the first phase of a refactor that will eventually enable
    the same Jenkins setup in elasticsearch-ci to run:
    
    1. PR builds
    2. Branch builds that cache artifacts for developers
    3. Daily debug builds with assertions enabled
    4. Builds for release to end users
    
    A secondary goal is to speed up CI by spreading the build over
    more servers.
    
    In this first stage of the changes:
    
    1. The extra options needed by debug builds and release builds are
       passed through to the low level build commands
    2. The cross compile steps are split into a separate build script,
       which, eventually, will be run on a different server
    
    Completing this project without lengthy ML CI downtime will
    require a series of changes alternating between the ml-cpp repo
    and the Jenkins configuration.  This is just the first step of
    many.
    
    Backport of #1835/#1836

--- dev-tools/jenkins_ci.sh
+++ dev-tools/jenkins_ci.sh
@@ -7,15 +7,16 @@
 
 # The non-Windows part of ML C++ CI does the following:
 #
-# 1. If this is not a PR build, obtain credentials from Vault for the accessing
-#    S3
+# 1. If this is not a PR build nor a debug build, obtain credentials from Vault
+#    for the accessing S3
 # 2. If this is a PR build and running on x86_64, check the code style
 # 3. Build and unit test the Linux version of the C++ on the native architecture
 # 4. If running on x86_64, cross compile the macOS build of the C++
 # 5. If this is not a PR build and running on x86_64, cross compile the aarch
 #    build of the C++
-# 6. If this is not a PR build, upload the builds to the artifacts directory on
-#    S3 that subsequent Java builds will download the C++ components from
+# 6. If this is not a PR build nor a debug build, upload the builds to the
+#    artifacts directory on S3 that subsequent Java builds will download the C++
+#    components from
 #
 # The steps run in Docker containers that ensure OS dependencies
 # are appropriate given the support matrix.
@@ -25,41 +26,26 @@
 : "${HOME:?Need to set HOME to a non-empty value.}"
 : "${WORKSPACE:?Need to set WORKSPACE to a non-empty value.}"
 
-# If this isn't a PR build then obtain credentials from Vault
-if [ -z "$PR_AUTHOR" ] ; then
-    set +x
-    export VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
-
-    unset ML_AWS_ACCESS_KEY ML_AWS_SECRET_KEY
-    FAILURES=0
-    while [ $FAILURES -lt 3 -a -z "$ML_AWS_ACCESS_KEY" ] ; do
-        AWS_CREDS=$(vault read -format=json -field=data aws-dev/creds/prelertartifacts)
-        if [ $? -eq 0 ] ; then
-            export ML_AWS_ACCESS_KEY=$(echo $AWS_CREDS | jq -r '.access_key')
-            export ML_AWS_SECRET_KEY=$(echo $AWS_CREDS | jq -r '.secret_key')
-        else
-            let FAILURES++
-            echo "Attempt $FAILURES to get AWS credentials failed"
-        fi
-    done
-
-    unset VAULT_TOKEN VAULT_ROLE_ID VAULT_SECRET_ID
-
-    if [ -z "$ML_AWS_ACCESS_KEY" -o -z "$ML_AWS_SECRET_KEY" ] ; then
-        echo "Exiting after failing to get AWS credentials $FAILURES times"
-        exit 1
-    fi
-    set -x
-fi
-
-set -e
+set +x
 
 # Change directory to the directory containing this script
 cd "$(dirname $0)"
 
+# If this isn't a PR build or a debug build then obtain credentials from Vault
+if [[ -z "$PR_AUTHOR" && -z "$ML_DEBUG" ]] ; then
+    . ./aws_creds_from_vault.sh
+fi
+
+set -e
+
 # Default to a snapshot build
 if [ -z "$BUILD_SNAPSHOT" ] ; then
     BUILD_SNAPSHOT=true
+fi
+
+# Default to running tests
+if [ -z "$RUN_TESTS" ] ; then
+    RUN_TESTS=true
 fi
 
 VERSION=$(cat ../gradle.properties | grep '^elasticsearchVersion' | awk -F= '{ print $2 }' | xargs echo)
@@ -77,6 +63,18 @@ if [ "$BUILD_SNAPSHOT" = false ] ; then
 else
     export SNAPSHOT=yes
     VERSION=${VERSION}-SNAPSHOT
+fi
+
+# Version qualifier shouldn't be used in PR builds
+if [[ -n "$PR_AUTHOR" && -n "$VERSION_QUALIFIER" ]] ; then
+    echo "VERSION_QUALIFIER should not be set in PR builds: was $VERSION_QUALIFIER"
+    exit 2
+fi
+
+# Tests must be run in PR builds
+if [[ -n "$PR_AUTHOR" && "$RUN_TESTS" = false ]] ; then
+    echo "RUN_TESTS should not be false PR builds"
+    exit 3
 fi
 
 # Remove any old builds
@@ -98,9 +96,18 @@ if [ "$HARDWARE_ARCH" = x86_64 ] ; then
         ./docker_check_style.sh
     fi
 
-    ./docker_test.sh linux
+    if [ "$RUN_TESTS" = false ] ; then
+        ./docker_build.sh linux
+    else
+        ./docker_test.sh linux
+    fi
+
 elif [ "$HARDWARE_ARCH" = aarch64 ] ; then
-    ./docker_test.sh linux_aarch64_native
+    if [ "$RUN_TESTS" = false ] ; then
+        ./docker_build.sh linux_aarch64_native
+    else
+        ./docker_test.sh linux_aarch64_native
+    fi
 fi
 
 # If this is a PR build then run some Java integration tests
@@ -115,17 +122,18 @@ if [ -n "$PR_AUTHOR" ] ; then
     fi
 fi
 
+# TODO - remove the cross compiles once the dedicated script is integrated into Jenkins
 # Cross compile macOS
 if [ "$HARDWARE_ARCH" = x86_64 ] ; then
     ./docker_build.sh macosx
-fi
-
-# If this isn't a PR build then cross compile aarch64 and upload the artifacts
-if [ -z "$PR_AUTHOR" ] ; then
-    if [ "$HARDWARE_ARCH" = x86_64 ] ; then
+    # If this isn't a PR build cross compile aarch64 too
+    if [ -z "$PR_AUTHOR" ] ; then
         ./docker_build.sh linux_aarch64_cross
     fi
-    cd ..
-    ./gradlew --info -b upload.gradle -Dbuild.snapshot=$BUILD_SNAPSHOT upload
+fi
+
+# If this isn't a PR build and isn't a debug build then upload the artifacts
+if [[ -z "$PR_AUTHOR" && -z "$ML_DEBUG" ]] ; then
+    (cd .. && ./gradlew --info -b upload.gradle -Dbuild.version_qualifier=$VERSION_QUALIFIER -Dbuild.snapshot=$BUILD_SNAPSHOT upload)
 fi
 
