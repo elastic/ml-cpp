58f58f06f7be75e7f634cf5d00cfe55e5247333d
David Roberts <dave.roberts@elastic.co>
2021-07-19 17:30:14 +0100

    [ML] Switch ml-cpp repo to Elastic License 2.0
    Starting with version 7.15 all the ML C++ code will be
    licensed under the Elastic License 2.0.
    
    The intention is that modified versions may be used in
    production but only when invoked by the Elasticsearch
    JVM such that an Elastic license key that permits ML
    use has been validated before ML functionality is
    invoked.
    
    (As always, modified code is not _supported_, just
    permitted by the license.)

--- bin/categorize/Main.cc
+++ bin/categorize/Main.cc
@@ -1,7 +1,12 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the following additional limitation. Functionality enabled by the
+ * files subject to the Elastic License 2.0 may only be used in production when
+ * invoked by an Elasticsearch process with a license key installed that permits
+ * use of machine learning features. You may not use this file except in
+ * compliance with the Elastic License 2.0 and the foregoing additional
+ * limitation.
  */
 //! \brief
 //! Group machine generated messages into categories by similarity.
@@ -84,7 +89,7 @@ int main(int argc, char** argv) {
     bool isPersistFileNamedPipe{false};
     bool isPersistInForeground{false};
     std::string categorizationFieldName;
-    bool validElasticLicenseKeyConfirmed{true};
+    bool validElasticLicenseKeyConfirmed{false};
     if (ml::categorize::CCmdLineParser::parse(
             argc, argv, limitConfigFile, jobId, logProperties, logPipe, delimiter,
             lengthEncodedInput, persistInterval, namedPipeConnectTimeout,
