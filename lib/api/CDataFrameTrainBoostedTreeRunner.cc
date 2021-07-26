98e9cd5fac9097a2c91f6f037af43901dee3825a
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2021-07-26 13:19:25 +0200

    comment added

--- lib/api/CDataFrameTrainBoostedTreeRunner.cc
+++ lib/api/CDataFrameTrainBoostedTreeRunner.cc
@@ -410,7 +410,8 @@ void CDataFrameTrainBoostedTreeRunner::runImpl(core::CDataFrame& frame) {
         break;
     case E_Predict:
         m_BoostedTree = m_BoostedTreeFactory->buildForPredict(frame, dependentVariableColumn);
-        m_BoostedTree->predict(true /*new data only*/);
+        // prediction occurs in buildForPredict
+        // m_BoostedTree->predict(true /*new data only*/);
         break;
     }
 
