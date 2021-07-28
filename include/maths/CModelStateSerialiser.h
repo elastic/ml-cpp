4e9cc1fdb6eb2e03e0a801a0e50f9ae1b8a94a61
David Roberts <dave.roberts@elastic.co>
2021-07-28 12:08:22 +0100

    [7.x] [ML] Switch ml-cpp repo to Elastic License 2.0 (#1964)
    Starting with version 7.15 all the ML C++ code will be
    licensed under the Elastic License 2.0.
    
    The intention is that modified versions may be used in
    production but only when invoked by the Elasticsearch
    JVM such that an Elastic license key that permits ML
    use has been validated before ML functionality is
    invoked.
    
    (As always, modified code is not _supported_, just
    permitted by the license.)
    
    Backport of #1952

--- include/maths/CModelStateSerialiser.h
+++ include/maths/CModelStateSerialiser.h
@@ -1,7 +1,12 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License;
- * you may not use this file except in compliance with the Elastic License.
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the following additional limitation. Functionality enabled by the
+ * files subject to the Elastic License 2.0 may only be used in production when
+ * invoked by an Elasticsearch process with a license key installed that permits
+ * use of machine learning features. You may not use this file except in
+ * compliance with the Elastic License 2.0 and the foregoing additional
+ * limitation.
  */
 
 #ifndef INCLUDED_ml_maths_CModelStateSerialiser_h
