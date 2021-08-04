7db25eab284605f453e715bef492db28cd0a91f7
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2021-08-04 13:51:26 +0200

    [ML] Visualize Incremental learning (#1953)
    This PR adds a notebook for the illustration of incremental training using three synthetic two-dimensional datasets. I also took the opportunity to refactor some python code out to misc.py and put it to job.py and add some code for tree comparison.

--- jupyter/src/incremental_learning/trees.py
+++ jupyter/src/incremental_learning/trees.py
@@ -5,6 +5,8 @@
 #
 from graphviz import Digraph
 import numpy as np
+from math import isclose
+
 
 class TreeNode:
     def __init__(self, json_node, feature_names):
@@ -19,8 +21,23 @@ class TreeNode:
 
     def label(self):
         label = "id={}\nsamples={}\n{}<{:.4f}".format(self.id, self.number_samples,
-                                                  self.split_feature, self.threshold)
+                                                      self.split_feature, self.threshold)
         return label
+
+    def __eq__(self, value):
+        return self.id == value.id \
+            and self.is_leaf == value.is_leaf \
+            and self.left_child_id == value.left_child_id \
+            and self.right_child_id == value.right_child_id \
+            and isclose(self.threshold,  value.threshold, abs_tol=1e-6) \
+            and self.split_feature == value.split_feature
+
+    def __ne__(self, value):
+        return not self == value
+
+    def __str__(self):
+        return "Node {}: is_leaf {}, left_child_id {}, right_child_id {}\nthreshold {}, split_feature {}, split_gain {}".format(
+            self.id, self.is_leaf, self.left_child_id, self.right_child_id, self.threshold, self.split_feature, self.split_gain)
 
 
 class Leaf:
@@ -31,28 +48,40 @@ class Leaf:
         self.is_leaf = True
 
     def label(self):
-        label = "id={}\nsamples={}\nvalue={:.4f}".format(self.id, self.number_samples, 
-                                                  self.value)
+        label = "id={}\nsamples={}\nvalue={:.4f}".format(self.id, self.number_samples,
+                                                         self.value)
         return label
+
+    def __eq__(self, value):
+        return self.id == value.id \
+            and isclose(self.value, value.value, abs_tol=1e-6) \
+            and self.is_leaf == value.is_leaf
+
+    def __ne__(self, value):
+        return not self == value
+
+    def __str__(self):
+        return "Leaf {}: number_samples {}, value {}".format(self.id, self.number_samples, self.value)
+
 
 class Tree:
     def __init__(self, json_tree):
-        self.feature_names=json_tree['feature_names']
-        self.dot=Digraph()
+        self.feature_names = json_tree['feature_names']
+        self.dot = Digraph()
         self.dot.attr(size='10,8')
-        self.nodes={}
-        self.number_nodes=0
-        self.number_leaves=0
+        self.nodes = {}
+        self.number_nodes = 0
+        self.number_leaves = 0
         for json_node in json_tree['tree_structure']:
             if 'leaf_value' in json_node:
-                leaf=Leaf(json_node)
-                self.nodes[leaf.id]=leaf
+                leaf = Leaf(json_node)
+                self.nodes[leaf.id] = leaf
                 self.dot.attr('node', shape='oval')
                 self.dot.node(str(leaf.id), label=leaf.label())
                 self.number_leaves += 1
             else:
-                node=TreeNode(json_node, self.feature_names)
-                self.nodes[node.id]=node
+                node = TreeNode(json_node, self.feature_names)
+                self.nodes[node.id] = node
                 self.dot.attr('node', shape='box')
                 self.dot.node(str(node.id), label=node.label())
                 self.number_nodes += 1
@@ -63,14 +92,13 @@ class Tree:
         self.traverse(self.nodes[0], 0)
 
     def traverse(self, node, height=0):
-        node.height=height
+        node.height = height
         if not node.is_leaf:
             self.traverse(self.nodes[node.left_child_id], height+1)
             self.traverse(self.nodes[node.right_child_id], height+1)
 
     def max_index(self):
         return max(map(lambda x: x.id, self.nodes.values()))
-
 
     def min_gain(self):
         if self.number_nodes > 0:
@@ -94,14 +122,14 @@ class Tree:
         return max(map(lambda x: x.height, self.nodes.values()))+1
 
     def get_leaf_path(self, x):
-        path=[]
-        node=self.nodes[0]
+        path = []
+        node = self.nodes[0]
         path.append(node.id)
         while not node.is_leaf:
             if x[node.split_feature] < node.threshold:
-                node=self.nodes[node.left_child_id]
+                node = self.nodes[node.left_child_id]
             else:
-                node=self.nodes[node.right_child_id]
+                node = self.nodes[node.right_child_id]
             path.append(node.id)
         return path
 
@@ -109,22 +137,68 @@ class Tree:
         return self.get_leaf_path(x)[-1]
 
     def get_tree_distance(self, x1, x2):
-        path1=self.get_leaf_path(x1)
-        path2=self.get_leaf_path(x2)
+        path1 = self.get_leaf_path(x1)
+        path2 = self.get_leaf_path(x2)
         return len(np.setdiff1d(path1, path2))+len(np.setdiff1d(path2, path1))
 
-    def get_leaves_num_samples(self)->dict:
-        num_samples = {} #np.zeros(self.number_leaves)
+    def get_leaves_num_samples(self) -> dict:
+        num_samples = {}  # np.zeros(self.number_leaves)
         for idx, node in self.nodes.items():
             if node.is_leaf:
                 num_samples[node.id] = node.number_samples
         return num_samples
 
+    def __eq__(self, other):
+        if self.number_leaves != other.number_leaves \
+                or self.number_nodes != other.number_nodes:
+            return False
+        for i in range(len(self.nodes)):
+            if self.nodes[i] != other.nodes[i]:
+                return False
+        return True
+
+    def show_diff(self, other):
+        diff_dot = Digraph()
+        diff_dot.attr(size='10,8')
+        for i, node in self.nodes.items():
+            if i not in other.nodes.keys() or \
+                    self.nodes[i].is_leaf != other.nodes[i].is_leaf or \
+                    self.nodes[i] != other.nodes[i]:
+                if self.nodes[i].is_leaf:
+                    diff_dot.attr('node', shape='box',
+                                  fillcolor='red', style='filled')
+                else:
+                    diff_dot.attr('node', shape='oval',
+                                  fillcolor='red', style='filled')
+                diff_dot.node(str(self.nodes[i].id),
+                              label=self.nodes[i].label())
+            else:
+                if self.nodes[i].is_leaf:
+                    diff_dot.attr('node', shape='box',
+                                  fillcolor='white', style='')
+                else:
+                    diff_dot.attr('node', shape='oval', style='')
+                diff_dot.node(str(self.nodes[i].id),
+                              label=self.nodes[i].label())
+        for node in self.nodes.values():
+            if not node.is_leaf:
+                diff_dot.edge(str(node.id), str(node.left_child_id))
+                diff_dot.edge(str(node.id), str(node.right_child_id))
+        return diff_dot
+
+    def num_diff_nodes(self, other):
+        diff_nodes = 0.0
+        for i, node in self.nodes.items():
+            if i not in other.nodes.keys() or \
+                    self.nodes[i].is_leaf != other.nodes[i].is_leaf or \
+                    self.nodes[i] != other.nodes[i]:
+                diff_nodes += 1.0
+        return diff_nodes
 
 
 class Forest:
     def __init__(self, trained_models):
-        self.trees=[]
+        self.trees = []
         for trained_model in trained_models:
             self.trees.append(Tree(trained_model['tree']))
 
