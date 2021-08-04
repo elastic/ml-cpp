bca420c2f138cc6140e7384d950e73735f4d2c96
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2021-08-04 11:18:38 +0200

    refactorings

--- jupyter/src/incremental_learning/config.py
+++ jupyter/src/incremental_learning/config.py
@@ -1,7 +1,9 @@
-from pathlib2 import Path
+import configparser
+import logging
 import platform
 from sys import exit
-import logging
+
+from pathlib2 import Path
 
 root_dir = Path(__file__).parent.parent.parent.absolute()
 data_dir = root_dir / 'data'
@@ -16,19 +18,41 @@ if cloud:
     dfa_path = Path('/ml-cpp/bin/data_frame_analyzer')
 else:
     search_location = Path(__file__).parent.parent.parent.parent
-    runners = list(search_location.glob('**/build/distribution/platform/**/data_frame_analyzer'))
+    runners = list(search_location.glob(
+        '**/build/distribution/platform/**/data_frame_analyzer'))
     if len(runners) == 0:
         print("Cannot find data_frame_analyzer binary")
         exit(1)
     dfa_path = runners[0]
 
-# Google cloud storage bucket with datasets
-bucket_name = 'ml-incremental-learning-datasets'
-
-logger = logging.getLogger("incremental_learning")
+logger = logging.getLogger(__package__)
 logger.handlers = []
 ch = logging.StreamHandler()
 formatter = logging.Formatter('[%(levelname).1s] %(name)s >> %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
-logger.setLevel("INFO")
+
+# Read config file
+config = configparser.ConfigParser()
+config_file = root_dir / 'config.ini'
+if not config_file.exists():
+    logger.error("Configuration file {} not found.".format(config_file))
+    exit(1)
+config.read(root_dir / 'config.ini')
+
+# Elasticsearch deployment configuration
+if (not config['cloud']['cloud_id']) or (not config['cloud']['user']) or (not config['cloud']['password']):
+    logger.error("Cloud configuration is missing or incomplete.")
+    exit(1)
+es_cloud_id = config['cloud']['cloud_id']
+es_user = config['cloud']['user']
+es_password = config['cloud']['password']
+
+if config['logging']['level']:
+    logger.setLevel(config['logging']['level'])
+
+# Google cloud storage bucket with datasets
+if not config['storage']['bucket_name']:
+    logger.error("Storage bucket configuration is missing")
+    exit(1)
+bucket_name = config['storage']['bucket_name']
--- jupyter/src/incremental_learning/elasticsearch.py
+++ jupyter/src/incremental_learning/elasticsearch.py
@@ -1,46 +1,44 @@
-import configparser
 import json
-from pathlib2 import Path
 import shutil
+
+from pathlib2 import Path
 
 from elasticsearch import Elasticsearch
 
-from .config import root_dir, logger
+from .config import es_cloud_id, es_password, es_user, logger, root_dir
+
 
 def push2es(data_path, name, prefix='experiment-'):
-
-    config = configparser.ConfigParser()
-
-    config.read(root_dir / 'config.ini')
-    es = Elasticsearch(cloud_id=config['cloud']['cloud_id'],
-                    http_auth=(config['cloud']['user'], config['cloud']['password']))
+    es = Elasticsearch(cloud_id=es_cloud_id,
+                       http_auth=(es_user, es_password))
 
     for data_path in data_path.iterdir():
         if not (data_path/'config.json').is_file():
             print('{} ignored'.format(data_path))
             continue
 
-        doc={}
+        doc = {}
         with open(data_path/'config.json') as fp:
-            config=json.load(fp)
-            doc['config']=config
+            config = json.load(fp)
+            doc['config'] = config
 
         with open(data_path/'metrics.json') as fp:
-            metrics=json.load(fp)
-            doc['metrics']=metrics
+            metrics = json.load(fp)
+            doc['metrics'] = metrics
 
         with open(data_path/'run.json') as fp:
-            run=json.load(fp)
-            doc['run']=run
-            artifacts=run['artifacts']
-            run['artifacts']=[]
+            run = json.load(fp)
+            doc['run'] = run
+            artifacts = run['artifacts']
+            run['artifacts'] = []
             for artifact in artifacts:
                 with open(data_path/artifact) as fp:
-                    run['artifacts'].append({'name': artifact, 'value': fp.read()})
+                    run['artifacts'].append(
+                        {'name': artifact, 'value': fp.read()})
 
         with open(data_path/'cout.txt') as fp:
-            cout=fp.read()
-            doc['cout']=cout
+            cout = fp.read()
+            doc['cout'] = cout
 
-        res=es.index(index=prefix+name, body=doc)
+        res = es.index(index=prefix+name, body=doc)
         logger.info('{} {}'.format(data_path, res['result']))
