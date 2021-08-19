55eb45ed4045b2bfae574e4a77e8b446b0c9a942
Tom Veasey <tveasey@elastic.co>
2021-08-19 10:21:38 +0100

    Test thresholds

--- lib/api/unittest/CDataFrameAnalyzerTrainingTest.cc
+++ lib/api/unittest/CDataFrameAnalyzerTrainingTest.cc
@@ -650,7 +650,7 @@ BOOST_AUTO_TEST_CASE(testRegressionTraining) {
               << "ms");
 
     BOOST_TEST_REQUIRE(core::CProgramCounters::counter(
-                           counter_t::E_DFTPMEstimatedPeakMemoryUsage) < 6300000);
+                           counter_t::E_DFTPMEstimatedPeakMemoryUsage) < 6700000);
     BOOST_TEST_REQUIRE(core::CProgramCounters::counter(counter_t::E_DFTPMPeakMemoryUsage) < 1910000);
     BOOST_TEST_REQUIRE(core::CProgramCounters::counter(counter_t::E_DFTPMTimeToTrain) > 0);
     BOOST_TEST_REQUIRE(core::CProgramCounters::counter(counter_t::E_DFTPMTimeToTrain) <= duration);
@@ -1222,7 +1222,7 @@ BOOST_AUTO_TEST_CASE(testClassificationTraining) {
               << "ms");
 
     BOOST_TEST_REQUIRE(core::CProgramCounters::counter(
-                           counter_t::E_DFTPMEstimatedPeakMemoryUsage) < 6300000);
+                           counter_t::E_DFTPMEstimatedPeakMemoryUsage) < 6700000);
     BOOST_TEST_REQUIRE(core::CProgramCounters::counter(counter_t::E_DFTPMPeakMemoryUsage) < 1910000);
     BOOST_TEST_REQUIRE(core::CProgramCounters::counter(counter_t::E_DFTPMTimeToTrain) > 0);
     BOOST_TEST_REQUIRE(core::CProgramCounters::counter(counter_t::E_DFTPMTimeToTrain) <= duration);
