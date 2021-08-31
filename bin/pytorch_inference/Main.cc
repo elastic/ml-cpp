55058bb50b4b81fcf919d25b22a12c97fa31a3b7
David Kyle <david.kyle@elastic.co>
2021-08-31 14:21:03 +0100

    Ensure initialiser list ctor is used

--- bin/pytorch_inference/Main.cc
+++ bin/pytorch_inference/Main.cc
@@ -51,7 +51,7 @@ torch::Tensor infer(torch::jit::script::Module& module,
     LOG_DEBUG(<< "2ndary args size: " << request.s_SecondaryArguments.size());
     inputs.reserve(1 + request.s_SecondaryArguments.size());
 
-    at::IntArrayRef inputSize{request.s_NumberInferences, request.s_NumberInputTokens};
+    at::IntArrayRef inputSize{{request.s_NumberInferences, request.s_NumberInputTokens}};
     LOG_DEBUG(<< "input size: " << inputSize);
 
     // BERT UInt tokens
