7bb2c7e05ef029d19f43850fbde918acea364199
David Kyle <david.kyle@elastic.co>
2021-09-03 16:26:48 +0100

    [ML][PYTORCH] Add benchmark mode to the evaluate.py script (#1948)
    Inference results contain a time that can be used for benchmarking. 
    Benchmark mode measures average execution time over multiple runs

--- bin/pytorch_inference/evaluate.py
+++ bin/pytorch_inference/evaluate.py
@@ -17,13 +17,35 @@ The test file must have the format:
     ...
 ]
 
+BENCHMARK MODE
+-----------
+Benchmarking reports the average inference time.
+
+The inference result docs contain timing information which can be used 
+for benchmarking. Benchmark mode accepts the same format JSON documents
+used in evaluation but the expected results are not checked only the 
+inference time is used.
+
+When benchmarking a warm up phase is used before any measurements are 
+taken. The size of the warm up phase and the number of request used in
+the actual benchmarking are controlled by the hard coded values
+`NUM_WARM_UP_REQUESTS` and `NUM_BENCHMARK_REQUEST`.
+
+Switch to benchmark mode by passing the `--benchmark` argument.
+
+Setting the number of threads used by inference has the biggest affect
+on performance and is controlled by the `--numThreads` argument. If not
+set LibTorch will choose the defaults.
 
 EXAMPLES
 --------
-Run this script with input from one of the example directories,
-for example:
+Run this script with input from one of the example directories.
 
-python3 evaluate.py /path/to/conll03_traced_ner.pt examples/ner/test_run.json
+For test evaluation:
+    python3 evaluate.py /path/to/conll03_traced_ner.pt examples/ner/test_run.json
+
+For Benchmarking:
+    python3 evaluate.py /path/to/conll03_traced_ner.pt examples/ner/test_run.json --benchmark --numThreads=2
 '''
 
 import argparse
@@ -31,9 +53,11 @@ import json
 import math
 import os
 import platform
-import stat
 import subprocess
 import sys
+
+NUM_WARM_UP_REQUESTS = 100
+NUM_BENCHMARK_REQUEST = 100
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -43,6 +67,8 @@ def parse_arguments():
     parser.add_argument('--restore_file', default='restore_file')
     parser.add_argument('--input_file', default='input_file')
     parser.add_argument('--output_file', default='output_file')
+    parser.add_argument('--numThreads', type=int, help='The number of inference threads. The system default is used if not set')
+    parser.add_argument('--benchmark', action='store_true', help='Benchmark inference time rather than evaluting expected results')
 
     return parser.parse_args()
 
@@ -69,10 +95,12 @@ def launch_pytorch_app(args):
         '--restore=' + args.restore_file,
         '--input=' + args.input_file,
         '--output=' + args.output_file,
-        '--validElasticLicenseKeyConfirmed=true',
-        '--numThreads=4',
-        '--numInterOpThreads=1'
+        '--validElasticLicenseKeyConfirmed=true'
         ]
+
+    if args.numThreads:
+        command.append('--numThreads=' + str(args.numThreads))
+        command.append('--numInterOpThreads=1')
 
     subprocess.Popen(command).communicate()
 
@@ -87,6 +115,22 @@ def stream_file(source, destination) :
 def write_request(request, destination):
     json.dump(request, destination)
 
+
+def restore_model(model, restore):
+    # create the restore file
+    with open(restore, 'wb') as restore_file:
+        file_stats = os.stat(model)
+        file_size = file_stats.st_size
+
+        # 4 byte unsigned int
+        b = (file_size).to_bytes(4, 'big')
+        restore_file.write(b)
+
+        print("streaming model of size", file_size, flush=True)
+
+        with open(model, 'rb') as source_file:
+            stream_file(source_file, restore_file) 
+            
 
 def compare_results(expected, actual, tolerance):
     try:
@@ -131,85 +175,125 @@ def compare_results(expected, actual, tolerance):
     return True
 
 
-def main():
+def run_benchmark(args):
+    # Write the requests
+    with open(args.input_file, 'w') as input_file:
+        with open(args.test_file) as test_file:
+            test_requests = json.load(test_file)
+        
+            print(f"warming up with {NUM_WARM_UP_REQUESTS} docs", flush=True)
+            warmup_count = 0
+            while warmup_count < NUM_WARM_UP_REQUESTS:
+                for doc in test_requests:
+                    write_request(doc['input'], input_file)
+                    warmup_count += 1
+                    if warmup_count == NUM_WARM_UP_REQUESTS:
+                        break
 
-    args = parse_arguments()
 
-    try:
-        # create the restore file
-        with open(args.restore_file, 'wb') as restore_file:
-            file_stats = os.stat(args.model)
-            file_size = file_stats.st_size
+            print(f"benchmarking with {NUM_BENCHMARK_REQUEST} docs", flush=True)
+            benchmark_count = 0
+            while benchmark_count < NUM_BENCHMARK_REQUEST:
+                for doc in test_requests:
+                    write_request(doc['input'], input_file)
+                    benchmark_count += 1
+                    if benchmark_count == NUM_BENCHMARK_REQUEST:
+                        break
+   
+    launch_pytorch_app(args)
 
-            # 4 byte unsigned int
-            b = (file_size).to_bytes(4, 'big')
-            restore_file.write(b)
+    print()
+    print("reading benchmark results...", flush=True)
+    with open(args.output_file) as output_file:
+        result_docs = json.load(output_file)
 
-            print("streaming model of size", file_size, flush=True)
+        total_time_ms = 0
+        doc_count = 0
 
-            with open(args.model, 'rb') as source_file:
-                stream_file(source_file, restore_file)
+        # ignore the warmup results
+        for i in range(NUM_WARM_UP_REQUESTS, len(result_docs)):
+            print(result_docs[i]['time_ms'])
+            total_time_ms += result_docs[i]['time_ms']
+            doc_count += 1
 
-        with open(args.input_file, 'w') as input_file:
-            with open(args.test_file) as test_file:
-                test_evaluation = json.load(test_file)
-            print("writing query", flush=True)
-            for doc in test_evaluation:
-                write_request(doc['input'], input_file)
-
-        launch_pytorch_app(args)
-
+        
         print()
-        print("reading results...", flush=True)
-        with open(args.output_file) as output_file:
+        print(f'{doc_count} requests evaluated in {total_time_ms} ms, avg time {total_time_ms / doc_count} ms')
+        print()
 
-            total_time_ms = 0
-            doc_count = 0
-            results_match = True
-            try:
-                result_docs = json.load(output_file)
-            except:
-                print("Error parsing json: ", sys.exc_info()[0])
-                return
 
-            for result in result_docs:                
-                if 'error' in result: 
-                    print('Inference failed. Request: {}, Msg: {}'.format(result['request_id'], result['error']))
-                    results_match = False
-                    continue;
+def test_evaluation(args):
+    with open(args.input_file, 'w') as input_file:
+        with open(args.test_file) as test_file:          
+            test_evaluation = json.load(test_file)
+        print("writing query", flush=True)
+        for doc in test_evaluation:
+            write_request(doc['input'], input_file)
 
-                expected = test_evaluation[doc_count]['expected_output']
-                 
-                tolerance = 1e-04
-                if 'how_close' in test_evaluation[doc_count]:
-                    tolerance = test_evaluation[doc_count]['how_close']                    
+    launch_pytorch_app(args)
 
-                total_time_ms += result['time_ms']
+    print()
+    print("reading results...", flush=True)
+    print()
+    with open(args.output_file) as output_file:
 
-                # compare to expected
-                if compare_results(expected, result, tolerance) == False:
-                    print()
-                    print('ERROR: inference result [{}] does not match expected results'.format(doc_count))
-                    print()
-                    results_match = False
+        total_time_ms = 0
+        doc_count = 0
+        results_match = True
+        try:
+            result_docs = json.load(output_file)
+        except:
+            print("Error parsing json: ", sys.exc_info()[0])
+            return
 
-                doc_count = doc_count +1
+        for result in result_docs:
+            if 'error' in result: 
+                print(f"Inference failed. Request: {result['request_id']}, Msg: {result['error']}")
+                results_match = False
+                continue
 
-            print()
-            print('{} requests evaluated in {} ms'.format(doc_count, total_time_ms))
-            print()
+            expected = test_evaluation[doc_count]['expected_output']
+                
+            tolerance = 1e-04
+            if 'how_close' in test_evaluation[doc_count]:
+                tolerance = test_evaluation[doc_count]['how_close']                                   
 
-            if doc_count != len(test_evaluation): 
+            total_time_ms += result['time_ms']
+
+            # compare to expected
+            if compare_results(expected, result, tolerance) == False:
                 print()
-                print('ERROR: The number of inference results [{}] does not match expected count [{}]'.format(doc_count, len(test_evaluation)))
+                print(f'ERROR: inference result [{doc_count}] does not match expected results')
                 print()
                 results_match = False
 
-            if results_match:
-                print()
-                print('SUCCESS: inference results match expected', flush=True)
-                print()
+            doc_count = doc_count +1
 
+
+        print()
+        print(f'{doc_count} requests evaluated in {total_time_ms} ms')
+        print()
+
+        if doc_count != len(test_evaluation): 
+            print()
+            print(f'ERROR: The number of inference results [{doc_count}] does not match expected count [{len(test_evaluation)}]')
+            print()
+            results_match = False
+
+        if results_match:
+            print()
+            print('SUCCESS: inference results match expected', flush=True)
+            print()
+
+def main():
+
+    args = parse_arguments()
+    try:
+        restore_model(args.model, args.restore_file)
+        if args.benchmark: 
+            run_benchmark(args)
+        else:
+            test_evaluation(args)
     finally:
         if os.path.isfile(args.restore_file):
             os.remove(args.restore_file)
@@ -217,8 +301,6 @@ def main():
             os.remove(args.input_file)
         if os.path.isfile(args.output_file):
             os.remove(args.output_file)
-
-
 
 if __name__ == "__main__":
     main()
