4d30ff208c7fda58b4a9948ad272a2b7ab86ffe9
Tom Veasey <tveasey@users.noreply.github.com>
2021-09-09 18:48:46 +0100

    [ML] Reinitialise predictions before estimating initial loss (#2021)
    Since we incrementally train to get the range to use for the tree topology change penalty, we need to reinitialise
    the predictions before computing the loss for the original model.

--- lib/maths/CBoostedTreeImpl.cc
+++ lib/maths/CBoostedTreeImpl.cc
@@ -369,6 +369,12 @@ void CBoostedTreeImpl::trainIncremental(core::CDataFrame& frame,
     }
     double numberKeptNodes{numberForestNodes(m_BestForest) - retrainedNumberNodes};
 
+    // Make sure that our predictions are correctly initialised before computing
+    // the initial loss.
+    auto allTrainingRowsMask = this->allTrainingRowsMask();
+    auto noRowsMask = core::CPackedBitVector{allTrainingRowsMask.size(), false};
+    this->initializePredictionsAndLossDerivatives(frame, allTrainingRowsMask, noRowsMask);
+
     // When we decide whether to accept the results of incremental training below
     // we compare the loss calculated for the best candidate forest with the loss
     // calculated with the original model. Since the data summary comprises a subset
@@ -380,7 +386,7 @@ void CBoostedTreeImpl::trainIncremental(core::CDataFrame& frame,
     // on the old training data in train and add it on to the threshold to accept
     // adjusting for the proportion of old training data we have.
     double numberNewTrainingRows{m_NewTrainingRowMask.manhattan()};
-    double numberOldTrainingRows{this->allTrainingRowsMask().manhattan() - numberNewTrainingRows};
+    double numberOldTrainingRows{allTrainingRowsMask.manhattan() - numberNewTrainingRows};
     double initialLoss{
         lossAtNSigma(1.0,
                      [&] {
@@ -465,8 +471,6 @@ void CBoostedTreeImpl::trainIncremental(core::CDataFrame& frame,
 
     if (m_ForceAcceptIncrementalTraining || m_BestForestTestLoss < initialLoss) {
         this->restoreBestHyperparameters();
-        core::CPackedBitVector allTrainingRowsMask{this->allTrainingRowsMask()};
-
         if (m_PreviousTrainNumberRows > 0) {
             this->scaleRegularizers(allTrainingRowsMask.manhattan() /
                                         this->meanNumberTrainingRowsPerFold(),
@@ -1757,7 +1761,8 @@ double CBoostedTreeImpl::meanAdjustedLoss(const core::CDataFrame& frame,
         loss += result.s_FunctionState;
     }
 
-    return this->meanLoss(frame, rowMask) + CBasicStatistics::mean(loss);
+    return this->meanLoss(frame, rowMask) +
+           oldRowMask.manhattan() / rowMask.manhattan() * CBasicStatistics::mean(loss);
 }
 
 double CBoostedTreeImpl::betweenFoldTestLossVariance() const {
