09d83055f6b014e572fd91e431e1d6c95b2d045e
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2021-10-29 14:52:42 +0200

    store comments with metrics

--- jupyter/src/incremental_learning/elasticsearch.py
+++ jupyter/src/incremental_learning/elasticsearch.py
@@ -49,6 +49,7 @@ def push2es(data_path, name, prefix='experiment-'):
         doc['cout'] = cout
 
     experiment_uid = doc['run']['experiment']['name']
+    comment = doc['run']['meta']['comment']
     doc['experiment_uid'] = experiment_uid
     res = es.index(index=prefix+name, body=doc)
     logger.info('Indexing experiment info {} {}'.format(
@@ -60,7 +61,7 @@ def push2es(data_path, name, prefix='experiment-'):
         if keys:
             steps = metrics[keys[0]]['steps']
             for step in steps:
-                document = {'experiment_uid': experiment_uid, 'step': step, 'dataset_name': doc['config']['dataset_name']}
+                document = {'experiment_uid': experiment_uid, 'step': step, 'dataset_name': doc['config']['dataset_name'], 'comment': comment}
                 for k in keys:
                     document[k] = metrics[k]['values'][step]
                 res = es.index(index=prefix+name+'-metrics', body=document)
