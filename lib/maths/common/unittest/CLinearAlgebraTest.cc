9b723fd29ba5993e1f09537385efe0fba3724bb1
Ed Savage <ed.savage@elastic.co>
2021-11-24 10:39:35 +0000

    [ML] Upgrade to Eigen 3.4.0
    Eigen 3.4.0 has better support for more modern C++ language features and
    as such quietens a few C++17 deprecation warnings encountered with
    version 3.3.7.
    
    Relates to #2120

--- lib/maths/common/unittest/CLinearAlgebraTest.cc
+++ lib/maths/common/unittest/CLinearAlgebraTest.cc
@@ -1,3 +1,4 @@
+
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
@@ -1159,12 +1160,12 @@ BOOST_AUTO_TEST_CASE(testShims) {
     {
         BOOST_TEST_REQUIRE(TMatrix4(0.0) ==
                            maths::common::las::conformableZeroMatrix(vector1));
-        BOOST_TEST_REQUIRE(TMatrix(4, 0.0) ==
-                           maths::common::las::conformableZeroMatrix(vector2));
-        BOOST_TEST_REQUIRE(TDenseMatrix::Zero(4, 4) ==
-                           maths::common::las::conformableZeroMatrix(vector3));
-        BOOST_TEST_REQUIRE(TDenseMatrix::Zero(4, 4) ==
-                           maths::common::las::conformableZeroMatrix(vector4));
+        BOOST_TEST_REQUIRE((TMatrix(4, 0.0) ==
+                           maths::common::las::conformableZeroMatrix(vector2)));
+        BOOST_TEST_REQUIRE((TDenseMatrix::Zero(4, 4) ==
+                           maths::common::las::conformableZeroMatrix(vector3)));
+        BOOST_TEST_REQUIRE((TDenseMatrix::Zero(4, 4) ==
+                           maths::common::las::conformableZeroMatrix(vector4)));
     }
     LOG_DEBUG(<< "Test isZero");
     {
