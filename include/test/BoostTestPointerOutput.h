bbbd36e48a76f1ed44fb36d56b3320caf2aaf66b
David Roberts <dave.roberts@elastic.co>
2021-11-25 09:35:29 +0000

    [ML] Removing null pointer output workaround for Boost.Test (#2138)
    Removing Boost.Test output function for nullptr now
    this is provided by a standard operator<< overload.

--- include/test/BoostTestPointerOutput.h
+++ include/test/BoostTestPointerOutput.h
@@ -15,38 +15,32 @@
 #include <ostream>
 
 //! \brief
-//! Functions to output unique_ptr and nullptr_t.
+//! Function to output unique_ptr.
 //!
 //! DESCRIPTION:\n
 //! Boost.Test likes to print values that fail assertions.  It
 //! can do this when operator<< is defined for the type being
 //! asserted on.  It is common that we assert on pointer values,
-//! yet there is no operator<< for unique_ptr until C++20 and
-//! no operator<< for nullptr until C++17.  This file adds
-//! replacements for these until such time as we are using a
-//! sufficiently modern standard.
+//! yet there is no operator<< for unique_ptr until C++20.  This
+//! file adds a replacement for this until such time as we are
+//! using a sufficiently modern standard.
 //!
 //! IMPLEMENTATION DECISIONS:\n
 //! Memory addresses are printed, not the dereferenced object.
 //! The intermediate boost_test_print_type function is
 //! overloaded rather than operator<< itself, as it will not
 //! cause an overloading ambiguity as newer compiler versions
-//! add the operators as extensions to the standard (for example
+//! add the operator as extensions to the standard (for example
 //! Xcode 10.3 has added operator<< for unique_ptr even when the
-//! selected standard is C++14).  Functions need to be in the
-//! std namespace so that they can be found via argument-dependent
-//! lookup (since unique_ptr and nullptr_t are in the std
-//! namespace).  This is not ideal but the intention is that this
-//! file is a temporary measure.
+//! selected standard is C++14).  The function needs to be in the
+//! std namespace so that it can be found via argument-dependent
+//! lookup (since unique_ptr is in the std namespace).  This is
+//! not ideal but the intention is that this file is a temporary
+//! measure.
 //!
 
 // TODO boost test: delete this source file once all compilers are using C++20
 namespace std {
-
-// TODO boost test: remove once all compilers are using C++17
-inline std::ostream& boost_test_print_type(std::ostream& os, std::nullptr_t p) {
-    return os << static_cast<void*>(p);
-}
 
 // TODO boost test: remove once all compilers are using C++20
 template<class T, class D>
