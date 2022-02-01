f261c3c09ab26898f02734ca78abd8029de972a1
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2022-02-01 15:07:25 +0100

    fix unit test evaluation

--- jupyter/tests/incremental_learning/test_multistep_learning.py
+++ jupyter/tests/incremental_learning/test_multistep_learning.py
@@ -31,35 +31,21 @@ def remove_subset(dataset: pd.DataFrame, subset: pd.DataFrame):
     return dataset
 
 
-def compute_error(job: Job, dataset: pd.DataFrame, dataset_name: str, _run):
-    job_eval = evaluate(dataset_name=dataset_name, dataset=dataset,
-                        original_job=job, run=_run, verbose=False)
-    job_eval.wait_to_complete()
-    dependent_variable = job.dependent_variable
-    if job.is_regression():
-        y_true = np.array([y for y in dataset[dependent_variable]])
-        return metrics.mean_squared_error(y_true, job_eval.get_predictions())
-    elif job.is_classification():
-        y_true = np.array([str(y) for y in dataset[dependent_variable]])
-        return metrics.accuracy_score(y_true,
-                                      job_eval.get_predictions())
-    else:
-        _run.run_logger.warning(
-            "Job is neither regression nor classification. No metric scores are available.")
-    return -1
-
-
 class TestIncrementalLearning(unittest.TestCase):
     def setUp(self) -> None:
         self.seed = 1000
         test_fraction = 0.2
-        training_fraction = 0.25
+        training_fraction = 0.2
         self.update_fraction = 0.1
         self.verbose = False
         self.dataset_name = 'house'
+        self.num_holdout_rows = 100
 
         self.run = FakeRun(
-            config={'analysis': {'parameters': {'seed': self.seed}}},
+            config={'threads': 8, 'analysis': {
+                    'parameters': {'seed': self.seed,
+                                   'num_holdout_rows': self.num_holdout_rows,
+                                   'data_summarization_fraction': 1.0}}},
             run_logger=logger)
 
         dataset = read_dataset(self.dataset_name)
@@ -79,7 +65,7 @@ class TestIncrementalLearning(unittest.TestCase):
             self.baseline_model = Job.from_file(path)
         else:
             self.baseline_model = train(self.dataset_name, baseline_dataset,
-                                        verbose=False, run=self.run)
+                                        verbose=self.verbose, run=self.run)
             self.baseline_model.wait_to_complete()
             path = jobs_dir / self.baseline_model_name
             self.baseline_model.store(path)
@@ -101,34 +87,49 @@ class TestIncrementalLearning(unittest.TestCase):
                 k += 1
                 continue
             if D_update.shape[0] >= self.update_num_samples:
-                D_update = D_update.sample(n=self.update_num_samples)
+                D_update = D_update.sample(
+                    n=self.update_num_samples, random_state=random_state)
                 success = True
         logger.info("Sampling completed after with k={}.".format(k))
         return D_update
+
+    def compute_error(self, job: Job) -> float:
+        job_eval = evaluate(dataset_name=self.dataset_name, dataset=self.used_dataset[:self.num_holdout_rows],
+                            original_job=job, run=self.run, verbose=self.verbose)
+        job_eval.wait_to_complete()
+        dependent_variable = job.dependent_variable
+        y_true = self.used_dataset[dependent_variable][:self.num_holdout_rows].to_numpy(
+        )
+        if job.is_regression():
+            return metrics.mean_squared_error(y_true, job_eval.get_predictions())
+        elif job.is_classification():
+            return metrics.accuracy_score(y_true,
+                                          job_eval.get_predictions())
+        else:
+            self.run.run_logger.warning(
+                "Job is neither regression nor classification. No metric scores are available.")
+        return -1
 
     def test_overfitting(self) -> None:
         """Make sure the updated model does not overfit on the test error.
         """
         hyperparameters = self.baseline_model.get_hyperparameters()
-        hyperparameters['retrained_tree_eta'] = [0.1, 0.4]
         random_state = np.random.RandomState(seed=self.seed)
         previous_hyperparameters = copy(hyperparameters)
         previous_job = self.baseline_model
-        previous_test_error = compute_error(job=previous_job, dataset=self.test_dataset,
-                                            dataset_name=self.dataset_name, _run=self.run)
-        for update_step in range(10):
-            update_dataset =  self.train_dataset.sample(frac=self.update_fraction) #self.sample_points(random_state)
+        previous_test_error = float('inf')
+        for update_step in range(15):
+            del previous_hyperparameters['retrained_tree_eta']
+            update_dataset = self.sample_points(random_state)
             update_job = update(dataset=update_dataset, dataset_name=self.dataset_name,
-                                original_job=previous_job, force=True, verbose=False,
+                                original_job=previous_job, verbose=self.verbose,
                                 hyperparameter_overrides=previous_hyperparameters, run=self.run)
             update_job.wait_to_complete()
-            new_hyperparameters = update_job.get_hyperparameters()
-            new_test_error = compute_error(job=update_job, dataset=self.test_dataset,
-                                           dataset_name=self.dataset_name, _run=self.run)
 
-            self.assertTrue(previous_test_error > new_test_error,
+            new_test_error = self.compute_error(job=update_job)
+            self.assertTrue(previous_test_error >= new_test_error,
                             msg='Test errors comparison failed at step {}: {} < {}'
                             .format(update_step, previous_test_error, new_test_error))
             previous_job = update_job
-            previous_hyperparameters = new_hyperparameters
+            previous_hyperparameters = update_job.get_hyperparameters()
             previous_test_error = new_test_error
