c56b8837748a7610cfba1f5e8317d1a4212e0477
Tom Veasey <tveasey@elastic.co>
2022-02-08 14:02:44 +0000

    Add a comment to data summarisation

--- lib/maths/analytics/CBoostedTreeImpl.cc
+++ lib/maths/analytics/CBoostedTreeImpl.cc
@@ -2399,11 +2399,27 @@ CTreeShapFeatureImportance* CBoostedTreeImpl::shap() {
 
 core::CPackedBitVector CBoostedTreeImpl::dataSummarization(const core::CDataFrame& frame) const {
 
-    std::size_t sampleSize{std::max(
-        static_cast<std::size_t>(static_cast<double>(frame.numberRows()) * m_DataSummarizationFraction),
-        static_cast<std::size_t>(2))};
-    // get row mask for sampling
+    // Note that if we are training on using a holdout set we include the holdout
+    // set in the data we consider for summarisation. Typical usage in this case
+    // is we're planning to train by query and so the summarisation fraction would
+    // be one.
+    //
+    // (I considered ensuring that the holdout set is always included in the data
+    // summary in entirety, which is consistent with this usage, but in practice
+    // it may be useful to be able to incrementally train a model with a cutdown
+    // summary after having trained by query. When we come to implement this it
+    // will be behind a new API which can ensure the data summarization fraction
+    // is set appropriately.)
+
     core::CPackedBitVector allTrainingRowsMask{this->allTrainingRowsMask()};
+
+    if (m_DataSummarizationFraction >= 1.0) {
+        return allTrainingRowsMask;
+    }
+
+    std::size_t sampleSize{std::max(
+        static_cast<std::size_t>(allTrainingRowsMask.manhattan() * m_DataSummarizationFraction),
+        static_cast<std::size_t>(2))};
     core::CPackedBitVector rowMask{CDataFrameUtils::stratifiedSamplingRowMasks(
         m_NumberThreads, frame, m_DependentVariable, m_Rng, sampleSize, 10, allTrainingRowsMask)};
 
