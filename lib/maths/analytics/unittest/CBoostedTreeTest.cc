7554b238b0b5378f65476525168ae95abb815a55
Tom Veasey <tveasey@elastic.co>
2022-03-29 16:08:13 +0100

    Logging

--- lib/maths/analytics/unittest/CBoostedTreeTest.cc
+++ lib/maths/analytics/unittest/CBoostedTreeTest.cc
@@ -1310,6 +1310,8 @@ BOOST_AUTO_TEST_CASE(testFeatureBags) {
                static_cast<double>(std::accumulate(selected.begin(), selected.end(), 0));
     };
 
+    LOG_DEBUG(<< "distanceToSorted(selectedForTree) = " << distanceToSorted(selectedForTree)
+              << ", distanceToSorted(selectedForNode) = " << distanceToSorted(selectedForNode));
     BOOST_TEST_REQUIRE(distanceToSorted(selectedForTree) < 0.014);
     BOOST_TEST_REQUIRE(distanceToSorted(selectedForNode) < 0.014);
 }
