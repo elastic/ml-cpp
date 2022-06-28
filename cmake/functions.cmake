bb3e3149c97a7124f49b1a6c4144917425ff52eb
David Roberts <dave.roberts@elastic.co>
2022-06-28 08:57:17 +0100

    Revert "[ML] Fix cross compilation for Linux aarch64 using CMake (#2327)"
    This reverts commit dafa08d16604931f694d57c2bcda1b4d3a73a56e.

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -35,7 +35,7 @@ function(ml_generate_resources _target)
     set(ML_VERSION_STR "${ML_VERSION_STR}-$ENV{VERSION_QUALIFIER}")
   endif()
 
-  if(NOT $ENV{SNAPSHOT} STREQUAL no)
+  if(NOT ENV{SNAPSHOT} STREQUAL no)
     set(ML_VERSION_STR "${ML_VERSION_STR}-SNAPSHOT")
   endif()
 
