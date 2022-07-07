9b8b900f98d711d74e813ffc10b64adcc8720167
David Roberts <dave.roberts@elastic.co>
2022-07-07 14:53:00 +0100

    [ML] More CMake tidyups (#2347)
    1. We no longer need references to make as we always use cmake
    2. Use camelCase for variables in build.gradle
    3. Don't require that BOOST_ROOT be an environment variable when
       building natively on Windows
    4. Remove the last occurrence of $ML_KEEP_GOING
    5. Include all variations of CMake build directory in Git/Docker ignores
    6. Docker builds should not hardcode the "Release" config

--- set_env.bat
+++ set_env.bat
@@ -1,31 +1,30 @@
 @echo off
-REM
-REM Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
-REM or more contributor license agreements. Licensed under the Elastic License
-REM 2.0 and the following additional limitation. Functionality enabled by the
-REM files subject to the Elastic License 2.0 may only be used in production when
-REM invoked by an Elasticsearch process with a license key installed that permits
-REM use of machine learning features. You may not use this file except in
-REM compliance with the Elastic License 2.0 and the foregoing additional
-REM limitation.
-REM
+rem
+rem Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+rem or more contributor license agreements. Licensed under the Elastic License
+rem 2.0 and the following additional limitation. Functionality enabled by the
+rem files subject to the Elastic License 2.0 may only be used in production when
+rem invoked by an Elasticsearch process with a license key installed that permits
+rem use of machine learning features. You may not use this file except in
+rem compliance with the Elastic License 2.0 and the foregoing additional
+rem limitation.
+rem
 
-REM Set up a build environment, to ensure repeatable builds
+rem Set up a build environment, to ensure repeatable builds
 
-REM Initialize the Visual Studio command prompt environment variables
+rem Initialize the Visual Studio command prompt environment variables
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-REM Set %CPP_SRC_HOME% to be an absolute path to this script's location, as
-REM different builds will come from different repositories and go to different
-REM staging areas
-SET CPP_SRC_HOME=%~dp0
+rem Set %CPP_SRC_HOME% to be an absolute path to this script's location, as
+rem different builds will come from different repositories and go to different
+rem staging areas
+set CPP_SRC_HOME=%~dp0
 
-REM Logical filesystem root
-SET ROOT=%CD:~0,2%
+rem Assume the drive letter where our 3rd party dependencies are installed under
+rem \usr\local is the current drive at the time this script is run
+set ROOT=%CD:~0,2%
 
-SET PATH=%ROOT%/PROGRA~1/CMake/bin;%CPP_SRC_HOME%/build/distribution/platform/windows-x86_64/bin;%PATH%
-		
-SET BOOST_ROOT=%ROOT%/usr/local
+set PATH=C:\Program Files\CMake\bin;%CPP_SRC_HOME%\build\distribution\platform\windows-x86_64\bin;%PATH%
 
-SET INCLUDE=
-SET LIBPATH=
+set INCLUDE=
+set LIBPATH=
