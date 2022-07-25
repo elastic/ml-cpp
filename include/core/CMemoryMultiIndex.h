135d4d98c4461c13fa0160c1be66164f5aaf67dc
Tom Veasey <tveasey@elastic.co>
2022-07-25 12:15:35 +0100

    Fix bad merge

--- include/core/CMemoryMultiIndex.h
+++ include/core/CMemoryMultiIndex.h
@@ -42,25 +42,13 @@ CMemory::storageNodeOverhead(const boost::multi_index::multi_index_container<T, 
 
 template<typename T, typename I, typename A>
 void CMemoryDebug::dynamicSize(const char* name,
-                        const boost::multi_index::multi_index_container<T, I, A>& t,
-                        const CMemoryUsage::TMemoryUsagePtr& mem) {
-    // It's tricky to determine the container overhead of a multi-index
-    // container.  It can have an arbitrary number of indices, each of which
-    // can be of a different type.  To accurately determine the overhead
-    // would require some serious template metaprogramming to interpret the
-    // "typename I" template argument, and it's just not worth it given the
-    // infrequent and relatively simple usage (generally just two indices
-    // in our current codebase).  Therefore there's an approximation here
-    // that the overhead is 2 pointers per entry per index.
-    using TMultiIndex = boost::multi_index::multi_index_container<T, I, A>;
-    constexpr std::size_t indexCount{
-        boost::mpl::size<typename TMultiIndex::index_type_list>::value};
+                               const boost::multi_index::multi_index_container<T, I, A>& t,
+                               const CMemoryUsage::TMemoryUsagePtr& mem) {
     std::string componentName(name);
 
     std::size_t items = t.size();
-    CMemoryUsage::SMemoryUsage usage(
-        componentName + "::" + typeid(T).name(),
-        items * (sizeof(T) + 2 * indexCount * sizeof(std::size_t)));
+    CMemoryUsage::SMemoryUsage usage(componentName + "::" + typeid(T).name(),
+                                     items * (sizeof(T) + CMemory::storageNodeOverhead(t)));
     CMemoryUsage::TMemoryUsagePtr ptr = mem->addChild();
     ptr->setName(usage);
 
