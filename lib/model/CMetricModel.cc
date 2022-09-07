0b8ecfbcd830c2f68124e85f205384d9bed91720
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2022-09-07 12:23:07 +0200

    explanation for metric anomalies fixed

--- lib/model/CMetricModel.cc
+++ lib/model/CMetricModel.cc
@@ -428,7 +428,7 @@ bool CMetricModel::computeProbability(const std::size_t pid,
     LOG_TRACE(<< "probability(" << this->personName(pid) << ") = " << p);
 
     resultBuilder.probability(p);
-    resultBuilder.probabilityExplanations(result.s_ProbabilityExplanations);
+    resultBuilder.probabilityExplanations(pJoint.probabilityExplanations());
 
     double multiBucketImpact{-1.0 * CAnomalyDetectorModelConfig::MAXIMUM_MULTI_BUCKET_IMPACT_MAGNITUDE};
     if (pJoint.calculateMultiBucketImpact(multiBucketImpact)) {
