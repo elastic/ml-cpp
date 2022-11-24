1303e693d76b61aebd7f7d8ec33e15a369156748
Ed Savage <ed.savage@elastic.co>
2022-11-24 09:57:53 +0000

    Testing check-style script
    Slightly reworked script to be more in line with Buildkite principles

---
+++ .buildkite/scripts/steps/check-style.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License
+# 2.0 and the following additional limitation. Functionality enabled by the
+# files subject to the Elastic License 2.0 may only be used in production when
+# invoked by an Elasticsearch process with a license key installed that permits
+# use of machine learning features. You may not use this file except in
+# compliance with the Elastic License 2.0 and the foregoing additional
+# limitation.
+#
+
+set -euo pipefail
+
+source ${REPO_ROOT}/.buildkite/scripts/common/base.sh
+source ${REPO_ROOT}/.buildkite/scripts/common/activate_service_account.sh
+
+echo "Running $0 from $(pwd)"
+
+# Ensure clang-format is available
+which clang-format > /dev/null 2>&1
+if [ $? != 0 ] ; then
+    echo "ERROR: The clang-format code formatter is not available. Exiting."
+    exit 1
+fi
+
+REQUIRED_CLANG_FORMAT_VERSION=5.0.1
+FOUND_CLANG_FORMAT_VERSION=$(expr "`clang-format --version`" : ".* \([0-9].[0-9].[0-9]\)")
+
+if [ -z "${FOUND_CLANG_FORMAT_VERSION}" ] ; then
+    echo "ERROR: Required clang-format major version ${REQUIRED_CLANG_FORMAT_VERSION} not found."
+    echo "       Could not determine clang-format version."
+    exit 2
+fi
+
+if [ "${REQUIRED_CLANG_FORMAT_VERSION}" != "${FOUND_CLANG_FORMAT_VERSION}" ] ; then
+    echo "ERROR: Required clang-format major version ${REQUIRED_CLANG_FORMAT_VERSION} not found."
+    echo "       Detected clang-format version ${FOUND_CLANG_FORMAT_VERSION}"
+    exit 3
+fi
+
+WRONG_FORMAT_FILES=()
+WRONG_COPYRIGHT_HEADER_FILES=()
+
+COPYRIGHT_HEADER_FILE="copyright_code_header.txt"
+LINES_IN_COPYRIGHT_HEADER=`wc -l "$COPYRIGHT_HEADER_FILE" | awk '{ print $1 }'`
+
+check_file() {
+    local FQFILE="$1"
+    local FILE="$2"
+    if ! cmp -s "$FQFILE" <(clang-format "$FQFILE"); then
+        WRONG_FORMAT_FILES+=("$FILE")
+    fi
+    if ! cmp -s "$COPYRIGHT_HEADER_FILE" <(head -$LINES_IN_COPYRIGHT_HEADER "$FQFILE"); then
+        WRONG_COPYRIGHT_HEADER_FILES+=("$FILE")
+    fi
+}
+
+if [ "x$1" = "x--all" ] ; then
+    # Batch mode - check everything and only display errors
+    INFILES=`find . \( -name 3rd_party -o -name build-setup \) -prune -o \( -name \*.cc -o -name \*.h \) -print`
+    for DOTFILE in ${INFILES}; do
+        FILE="${DOTFILE#./}"
+        check_file "${FILE}" "$FILE"
+    done
+else
+    # Local mode - check changed files only and report which files are checked
+    INFILES=`git diff --name-only --diff-filter=ACMRT | grep -v 3rd_party | grep -E "\.(cc|h)$"`
+    for FILE in ${INFILES}; do
+        echo "Checking: ${FILE}"
+        check_file "${FILE}" "$FILE"
+    done
+fi
+
+RC=0
+
+if [ -n "${WRONG_FORMAT_FILES}" ] ; then
+    echo -e "${RED}A format error has been detected within the following files${NOCOLOR}:"
+    printf "%s\n" "${WRONG_FORMAT_FILES[@]}"
+    RC=4
+else
+    echo "No format errors detected"
+fi
+
+if [ -n "${WRONG_COPYRIGHT_HEADER_FILES}" ] ; then
+    echo "${RED}The following files do not contain the correct copyright header${NOCOLOR}:"
+    printf "%s\n" "${WRONG_COPYRIGHT_HEADER_FILES[@]}"
+    RC=5
+else
+    echo "No copyright header errors detected"
+fi
+
+exit $RC
