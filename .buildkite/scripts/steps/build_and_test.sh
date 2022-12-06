797a74b140391a5a81f697d4768444b5ee1079e1
Ed Savage <ed.savage@elastic.co>
2022-12-06 13:21:00 +0000

    Experimenting with test analytics

--- .buildkite/scripts/steps/build_and_test.sh
+++ .buildkite/scripts/steps/build_and_test.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License
+# 2.0 and the following additional limitation. Functionality enabled by the
+# files subject to the Elastic License 2.0 may only be used in production when
+# invoked by an Elasticsearch process with a license key installed that permits
+# use of machine learning features. You may not use this file except in
+# compliance with the Elastic License 2.0 and the foregoing additional
+# limitation.
 
 # For now, re-use our existing CI scripts based on Docker
-${REPO_ROOT}/dev-tools/docker/docker_entrypoint.sh --test
+if [ "$RUN_TESTS" = "true" ]; then
+    ${REPO_ROOT}/dev-tools/docker/docker_entrypoint.sh --test
+else
+    ${REPO_ROOT}/dev-tools/docker/docker_entrypoint.sh
+fi
