c2e5455b34fc761e808c421cd0c574b8263f5618
Ed Savage <ed.savage@elastic.co>
2022-12-08 16:36:28 +0000

    Build and test in a single step

--- .buildkite/scripts/steps/build_and_test.sh
+++ .buildkite/scripts/steps/build_and_test.sh
@@ -12,17 +12,9 @@ echo "environment variables:"
 env
 # For now, re-use our existing CI scripts based on Docker
 if [ "$RUN_TESTS" = "true" ]; then
-    buildkite-agent artifact download "build/*" .
-    buildkite-agent artifact download "cmake-build-docker/*" .
-    #${REPO_ROOT}/dev-tools/docker/docker_entrypoint.sh --test
-    # Convert any failure of this make command into the word passed or failed in
-    # a status file - this allows the Docker image build to succeed if the only
-    # failure is the unit tests, and then the detailed test results can be
-    # copied from the image
-    echo passed > build/test_status.txt
-    cmake --build cmake-build-docker -v -j`nproc` -t test || echo failed > build/test_status.txt
+    ${REPO_ROOT}/dev-tools/docker/docker_entrypoint.sh --test
 else
     ${REPO_ROOT}/dev-tools/docker/docker_entrypoint.sh
-    buildkite-agent artifact upload "build/distributions/*"
-    buildkite-agent artifact upload "cmake-build-docker/**/*"
 fi
+
+buildkite-agent artifact upload "build/distributions/*"
