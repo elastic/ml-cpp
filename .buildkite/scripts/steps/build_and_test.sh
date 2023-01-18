78b9e4bbc9a7cea6f71c048bb882e353dfe816cf
Ed Savage <ed.savage@elastic.co>
2023-01-18 14:19:31 +0000

    Attend to review comments

--- .buildkite/scripts/steps/build_and_test.sh
+++ .buildkite/scripts/steps/build_and_test.sh
@@ -29,13 +29,19 @@ else
 fi
 
 # Version qualifier shouldn't be used in PR builds
-if [[ -n "$BUILDKITE_PULL_REQUEST" && -n "$VERSION_QUALIFIER" ]] ; then
+if [[ x"$BUILDKITE_PULL_REQUEST" != xfalse && -n "$VERSION_QUALIFIER" ]] ; then
     echo "VERSION_QUALIFIER should not be set in PR builds: was $VERSION_QUALIFIER"
     exit 2
 fi
 
-echo "environment variables:"
-env
+# If this is a PR build then it's redundant to cross compile aarch64 (as
+# we build and test aarch64 natively for PR builds) but there's a benefit
+# to building one platform with debug enabled to detect code that only
+# compiles with optimisation
+if [[ x"$BUILDKITE_PULL_REQUEST" != xfalse && "$CPP_CROSS_COMPILE" = "aarch64" ]] ; then
+    export ML_DEBUG=1
+fi
+
 # For now, re-use our existing CI scripts based on Docker
 if [ "$RUN_TESTS" = "true" ]; then
     ${REPO_ROOT}/dev-tools/docker/docker_entrypoint.sh --test
