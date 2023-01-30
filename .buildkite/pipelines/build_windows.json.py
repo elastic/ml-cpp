b0ea5af167d9235ddc4485b1ec7aca8b16916617
Ed Savage <ed.savage@elastic.co>
2023-01-30 13:48:22 +0000

    Another attempt to fix windows pipeline

--- .buildkite/pipelines/build_windows.json.py
+++ .buildkite/pipelines/build_windows.json.py
@@ -49,8 +49,7 @@ def main(args):
               "image": "family/ml-cpp-1-windows-2016",
             },
             "commands": [
-              'if ("\$Env:GITHUB_PR_COMMENT_VAR_ACTION" -eq "debug" ) {\$Env:ML_DEBUG=1}',
-              "./.buildkite/scripts/steps/build_and_test.ps1"
+              "bash .buildkite/scripts/steps/build_and_test_windows.sh"
             ],
             "depends_on": "check_style",
             "key": f"build_test_Windows-{arch}-{build_type}",
---
+++ .buildkite/scripts/steps/build_and_test_windows.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+env
+if [[ "$GITHUB_PR_COMMENT_VAR_ACTION" == "debug" ]]; then export ML_DEBUG=1; fi
+powershell -File .buildkite/scripts/steps/build_and_test.ps1"
