0a9a9c3943da28ba974edf85cb92c20db4e48e2f
Ed Savage <ed.savage@elastic.co>
2023-02-10 11:20:51 +0000

    Another typo :-/

--- .buildkite/scripts/steps/upload_dra_to_gcs.sh
+++ .buildkite/scripts/steps/upload_dra_to_gcs.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/bin/bash
 # Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
 # or more contributor license agreements. Licensed under the Elastic License
 # 2.0 and the following additional limitation. Functionality enabled by the
