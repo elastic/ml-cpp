727928a8043554eb45c674a3aa8e81eca3d1da1c
Ed Savage <ed.savage@elastic.co>
2023-02-13 16:31:17 +0000

    Temporary changes to test notifications

--- .buildkite/pipelines/send_email_notification.sh
+++ .buildkite/pipelines/send_email_notification.sh
@@ -14,6 +14,7 @@ steps:
   - label: "Schedule :email: notification"
     command: "echo schedule :email: notification"
 notify:
-  - email: "build-machine-learning@elastic.co"
-    if: build.state == "failed" && build.pull_request.id == null
+#  - email: "build-machine-learning@elastic.co"
+  - email: "ed.savage@elastic.co"
+#    if: build.state == "failed" && build.pull_request.id == null
 EOL
