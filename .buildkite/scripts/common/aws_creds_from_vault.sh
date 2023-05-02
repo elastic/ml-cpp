d072170a4cbd1543f3475064fb8aa4a08a0c0f8a
Ed Savage <ed.savage@elastic.co>
2023-05-02 17:11:35 +0100

    Remove debugging

--- .buildkite/scripts/common/aws_creds_from_vault.sh
+++ .buildkite/scripts/common/aws_creds_from_vault.sh
@@ -37,38 +37,20 @@ case $- in
         ;;
 esac
 
-# Temporary installation of aws cli to test the credentials obtained from vault
-mkdir ~/bin
-mkdir ~/aws-cli
-wget "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -O "awscliv2.zip"
-jar -xvf awscliv2.zip
-chmod u+x aws/install
-chmod u+x aws/dist/aws
-./aws/install -i ~/aws-cli -b ~/bin
-export PATH=~/bin:$PATH
-
 unset ML_AWS_ACCESS_KEY ML_AWS_SECRET_KEY ML_AWS_SECURITY_TOKEN
 FAILURES=0
 while [[ $FAILURES -lt 3 && -z "$ML_AWS_ACCESS_KEY" ]] ; do
     echo "vault read -format=json -field=data aws-elastic-ci-prod/creds/prelert-artifacts"
     AWS_CREDS=$(vault read -format=json -field=data aws-elastic-ci-prod/creds/prelert-artifacts)
     if [ $? -eq 0 ] ; then
-        echo "Successfully read creds from vault"
-        echo "================================="
-        echo $AWS_CREDS
-        echo "================================="
-
-        echo "Parsing credentials"
         export AWS_ACCESS_KEY_ID=$(echo $AWS_CREDS | jq -r '.access_key')
         export AWS_SECRET_ACCESS_KEY=$(echo $AWS_CREDS | jq -r '.secret_key')
         export AWS_SESSION_TOKEN=$(echo $AWS_CREDS | jq -r '.security_token')
 
         export ML_AWS_ACCESS_KEY=$AWS_ACCESS_KEY_ID
         export ML_AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY
-
-        env
-
     fi
+
     if [ -z "$ML_AWS_ACCESS_KEY" ] ; then
         let FAILURES++
         echo "Attempt $FAILURES to get AWS credentials failed"
@@ -79,23 +61,6 @@ if [ -z "$ML_AWS_ACCESS_KEY" -o -z "$ML_AWS_SECRET_KEY" ] ; then
     echo "Exiting after failing to get AWS credentials $FAILURES times"
     exit 1
 fi
-
-mkdir ~/.aws
-cat <<EOL > ~/.aws/credentials
-[default]
-aws_access_key_id = $AWS_ACCESS_KEY_ID
-aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
-aws_session_token = $AWS_SESSION_TOKEN
-EOL
-
-cat <<EOL > ~/.aws/config
-[default]
-region = us-east-1
-EOL
-
-echo "listing aws s3 bucket"
-aws s3 ls prelert-artifacts/maven/org/elasticsearch/ml/ml-cpp/ || echo "aws ls command failed.. continuing anyway"
-
 
 if [ "$REENABLE_X_OPTION" = true ] ; then
     set -x
