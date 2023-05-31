5a2bc2638ee1f3e841818874f4792efcf9ace7e0
Ed Savage <ed.savage@elastic.co>
2023-05-31 10:49:47 +0100

    Always upload test artifacts
    Ensure test artifacts are always uploaded for all build platforms

--- .buildkite/scripts/steps/build_and_test.ps1
+++ .buildkite/scripts/steps/build_and_test.ps1
@@ -59,10 +59,13 @@ $ErrorActionPreference="Continue"
 # The | % { "$_" } at the end converts any error objects on stderr to strings
 & ".\gradlew.bat" --info "-Dbuild.version_qualifier=$Env:VERSION_QUALIFIER" "-Dbuild.snapshot=$Env:BUILD_SNAPSHOT" $DebugOption $Tasks 2>&1 | % { "$_" }
 if ($LastExitCode -ne 0) {
-    Get-ChildItem -Path . -Include *.out -File -Recurse -ErrorAction SilentlyContinue | Compress-Archive -DestinationPath windows-x86_64-unit_test_results.zip
+    $ExitCode=$LastExitCode
+}
 
-    buildkite-agent artifact upload "windows-x86_64-unit_test_results.zip"
-    Exit 1
+Get-ChildItem -Path . -Include *.out -File -Recurse -ErrorAction SilentlyContinue | Compress-Archive -DestinationPath windows-x86_64-unit_test_results.zip
+buildkite-agent artifact upload "windows-x86_64-unit_test_results.zip"
+if ($ExitCode -ne 0) {
+    Exit $ExitCode
 }
 
 buildkite-agent artifact upload "build/distributions/*"
