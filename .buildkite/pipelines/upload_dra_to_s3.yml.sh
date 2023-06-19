ad2e787b42af125717b990ee0a69343abd57a84c
Ed Savage <ed.savage@elastic.co>
2023-06-19 15:07:34 +0100

    [ML] DRA upload: Only download artifacts once (#2534)
    The DRA upload pipelines are currently downloading required artifacts
    from multiple pipeline steps. While this is not an error or causing any
    harm it is slightly more efficient to only download the artifacts once,
    from the `create_dra_artifacts` step.

--- .buildkite/pipelines/upload_dra_to_s3.yml.sh
+++ .buildkite/pipelines/upload_dra_to_s3.yml.sh
@@ -16,7 +16,7 @@ steps:
     key: "upload_dra_artifacts"
     depends_on: create_dra_artifacts
     command:
-      - 'buildkite-agent artifact download "build/distributions/*" .'
+      - 'buildkite-agent artifact download "build/distributions/*" --step create_dra_artifacts .'
       - "./.buildkite/scripts/steps/upload_dra_to_s3.sh"
     agents:
       cpu: "2"
