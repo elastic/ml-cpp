bc84ccf6b80902deeac853d006c34090d952d101
Ed Savage <ed.savage@elastic.co>
2023-08-29 16:32:32 +0100

    Update the conditional governing downloading the latest windows dependencies

--- dev-tools/download_windows_deps.ps1
+++ dev-tools/download_windows_deps.ps1
@@ -11,9 +11,7 @@
 $ErrorActionPreference="Stop"
 $Archive="usr-x86_64-windows-2016-10.zip"
 $Destination="C:\"
-# If PyTorch is not version 1.13.1 then we need the latest download
-if (!(Test-Path "$Destination\usr\local\include\pytorch\torch\csrc\api\include\torch\version.h") -Or
-    !(Select-String -Path "$Destination\usr\local\include\pytorch\torch\csrc\api\include\torch\version.h" -Pattern "1.13.1" -Quiet)) {
+if (!(Test-Path "$Destination\usr\local\include\boost-1_83\boost\unordered\detail\prime_fmod.hpp")) {
     Remove-Item "$Destination\usr" -Recurse -Force -ErrorAction Ignore
     $ZipSource="https://storage.googleapis.com/elastic-ml-public/dependencies/$Archive"
     $ZipDestination="$Env:TEMP\$Archive"
