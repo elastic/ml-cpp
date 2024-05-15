2b35172903030b5fa060d8c01b8bdc56e62f582d
Ed Savage <ed.savage@elastic.co>
2024-05-16 09:21:13 +1200

    [ML] Use Orka for macOS ARM builds. (#2662)
    At the moment macOS ARM builds are performed on bare metal Mac Minis
    provided by MacStadium. For consistency with the rest of engineering,
    ease of maintenance, reproducibility of builds, automation of provisioning
    builders etc. it is preferred that we migrate to using VMs provided by
    MacStadium Orka.
    
    This PR makes use of an image built using `packer`. See #2663 for the associated `packer` config and scripts.

--- .buildkite/scripts/steps/build_and_test.sh
+++ .buildkite/scripts/steps/build_and_test.sh
@@ -80,7 +80,7 @@ if ! [[ "$HARDWARE_ARCH" = aarch64 && -z "$CPP_CROSS_COMPILE" ]] ; then
 else
   if [[ `uname` = "Darwin" && "$HARDWARE_ARCH" = "aarch64" ]]; then
      # For macOS, build directly on the machine
-     ${REPO_ROOT}/dev-tools/download_macos_deps.sh
+     sudo -E ${REPO_ROOT}/dev-tools/download_macos_deps.sh
      if [ "$RUN_TESTS" = false ] ; then
          TASKS="clean buildZip buildZipSymbols"
      else
