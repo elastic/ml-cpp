bbfe9116981bdab183da30073e9442ebc2d6bade
Ed Savage <ed.savage@elastic.co>
2024-06-12 16:32:46 +1200

    Adjust format of test code

--- lib/api/unittest/CModelSnapshotJsonWriterTest.cc
+++ lib/api/unittest/CModelSnapshotJsonWriterTest.cc
@@ -44,13 +44,13 @@ BOOST_AUTO_TEST_CASE(testWrite) {
             core_t::TTime(1521046309),         // bucket start time
             0,                                 // model bytes exceeded
             50000,                             // model bytes memory limit
-            60000, // JSON memory allocator bytes used
-            {1000, // categorized messages
-             100,  // total categories
-             7,    // frequent categories
-             13,   // rare categories
-             2,    // dead categories
-             8,    // failed categories
+            60000,                             // JSON memory allocator bytes used
+            {1000,                             // categorized messages
+             100,                              // total categories
+             7,                                // frequent categories
+             13,                               // rare categories
+             2,                                // dead categories
+             8,                                // failed categories
              model_t::E_CategorizationStatusWarn}};
 
         CModelSnapshotJsonWriter::SModelSnapshotReport report{
