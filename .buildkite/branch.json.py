0aad4ae3b9a74be64827412cda87f0e1e1f03df8
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2024-07-09 16:46:48 +0200

    typo

--- .buildkite/branch.json.py
+++ .buildkite/branch.json.py
@@ -28,7 +28,7 @@ def main():
                                                        ".buildkite/pipelines/send_email_notification.sh"))
     pipeline_steps.append(pipeline_steps.generate_step("Upload clang-format validation",
                                                        ".buildkite/pipelines/format_and_validation.yml.sh"))
-    pipeline_steps.append(pipeline_steps.generate_step("Scana and upload SonarQube report", 
+    pipeline_steps.append(pipeline_steps.generate_step("Scan and upload SonarQube report", 
                                                        ".buildkite/pipelines/sonarqube.yml.sh"))
     config = buildConfig.Config()
     config.parse()
