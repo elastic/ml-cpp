d995d162c66033fbd6705b5d0ed7fc7e6dfe8a21
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2024-07-10 10:55:36 +0200

    add env variables

--- .buildkite/pipelines/sonarqube.yml.sh
+++ .buildkite/pipelines/sonarqube.yml.sh
@@ -15,6 +15,9 @@ steps:
     depends_on: "check_style"
     agents:
       image: "docker.elastic.co/ml-dev/ml-linux-build:29"
+    env:
+      - PATH: /usr/local/gcc103/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+      - CPP_SRC_HOME: /ml-cpp
     command:
       - "cmake CMAKE_EXPORT_COMPILE_COMMANDS=ON -B cmake-build-docker"
     artifact_paths:
