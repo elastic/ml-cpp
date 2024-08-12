fe188a0af7a73259c9bd8a73d5b5d6db3828ae95
Ed Savage <ed.savage@elastic.co>
2024-08-12 15:11:13 +1200

    [ML] Trial s3 bucket in elastic-dev AWS account
    Experiment using the newer elastic-dev account for the s3 bucket
    containing the dependencies for the macOS x86 build.

--- dev-tools/download_macos_deps.sh
+++ dev-tools/download_macos_deps.sh
@@ -26,9 +26,11 @@ case `uname -m` in
 
     x86_64)
         ARCHIVE=local-x86_64-apple-macosx12.0-1.tar.bz2
+        URL="https://ml-cpp-artifacts.s3.eu-west-2.amazonaws.com/dependencies/$ARCHIVE"
         ;;
     arm64)
         ARCHIVE=local-arm64-apple-macosx11.1-10.tar.bz2
+        URL="https://s3-eu-west-1.amazonaws.com/prelert-artifacts/dependencies/$ARCHIVE"
         ;;
 
     *)
@@ -37,8 +39,6 @@ case `uname -m` in
         ;;
 
 esac
-
-URL="https://s3-eu-west-1.amazonaws.com/prelert-artifacts/dependencies/$ARCHIVE"
 
 echo "Downloading dependencies from $URL"
 cd "$TMPDIR" && curl -s -S --retry 5 -O "$URL"
