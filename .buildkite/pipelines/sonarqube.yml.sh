cbbbbef83857be9f436cf7aec0d27ed8c263ca9c
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2024-09-06 14:50:15 +0200

    increate resources

--- .buildkite/pipelines/sonarqube.yml.sh
+++ .buildkite/pipelines/sonarqube.yml.sh
@@ -15,6 +15,8 @@ steps:
     # depends_on: "build_test_linux-x86_64-RelWithDebInfo"
     soft_fail: true
     agents:
+      cpu: 6
+      memory: "16GB"
       image: "docker.elastic.co/ml-dev/ml-linux-build:30"
     env:
       PATH: "/usr/local/gcc103/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
