14f2a5a65ecbd518ce456a701dfe805d12d7e29b
Max Hniebergall <max.hniebergall@elastic.co>
2024-09-12 14:04:59 -0400

    Remove debug line that was printing out huge tensors causing timeouts

--- bin/pytorch_inference/Main.cc
+++ bin/pytorch_inference/Main.cc
@@ -69,7 +69,7 @@ torch::Tensor infer(torch::jit::script::Module& module_,
         }
 
         auto output = module_.forward(inputs);
-        LOG_DEBUG(<< "output_" << i << ": " << output);
+
         if (output.isTuple()) {
             // For transformers the result tensor is the first element in a tuple.
             all.push_back(output.toTuple()->elements()[0].toTensor());
