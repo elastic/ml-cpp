118ed171174a54c82607cf31cff871b50ce5c2df
Ed Savage <ed.savage@elastic.co>
2024-09-16 11:41:11 +1200

    [ML] Use java 21 for ES tests.
    Elasticsearch main branch now requires Java 21 to build. Bump the
    version so that the ES integration tests can build and run.

--- .buildkite/pipelines/run_es_tests_aarch64.yml.sh
+++ .buildkite/pipelines/run_es_tests_aarch64.yml.sh
@@ -13,7 +13,7 @@ steps:
   - label: "Java :java: Integration Tests for aarch64 :hammer:"
     key: "java_integration_tests_aarch64"
     command:
-      - "sudo yum -y install java-17-amazon-corretto-devel"
+      - "sudo wget -O - https://corretto.aws/downloads/latest/amazon-corretto-21-aarch64-linux-jdk.tar.gz | tar xvzf - -C /usr/lib/jvm"
       - 'buildkite-agent artifact download "build/*" . --step build_test_linux-aarch64-RelWithDebInfo'
       - '.buildkite/scripts/steps/run_es_tests.sh || (cd ../elasticsearch && find x-pack -name logs | xargs tar cvzf logs.tgz && buildkite-agent artifact upload logs.tgz && false)'
     depends_on: "build_test_linux-aarch64-RelWithDebInfo"
@@ -24,6 +24,8 @@ steps:
       diskSizeGb: 100
       diskName: '/dev/xvda'
     env:
+      JAVA_HOME: "/usr/lib/jvm/amazon-corretto-21-aarch64-linux-jdk"
+      PATH: "$JAVA_HOME/bin:$PATH"
       IVY_REPO: "../ivy"
       GRADLE_JVM_OPTS: "-Dorg.gradle.jvmargs=-Xmx16g"
     notify:
