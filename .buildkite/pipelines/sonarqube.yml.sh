845d7da65d54ad1f4f3af0427d8654921977f93b
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2024-10-28 11:13:53 +0000

    update memory

--- .buildkite/pipelines/sonarqube.yml.sh
+++ .buildkite/pipelines/sonarqube.yml.sh
@@ -15,9 +15,9 @@ steps:
     # depends_on: "build_test_linux-x86_64-RelWithDebInfo"
     soft_fail: true
     agents:
-      cpu: 4
+      cpu: 6
       ephemeralStorage: "20G"
-      memory: "64G"
+      memory: "8G"
       image: "docker.elastic.co/ml-dev/ml-linux-build:30"
     env:
       PATH: "/usr/local/gcc103/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
