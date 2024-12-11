91370827cb0e9b459d275a8e45feaa876247302d
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2024-12-11 09:36:35 +0100

    Update lib/maths/time_series/CTimeSeriesDecompositionDetail.cc
    Co-authored-by: Tom Veasey <tveasey@users.noreply.github.com>

--- lib/maths/time_series/CTimeSeriesDecompositionDetail.cc
+++ lib/maths/time_series/CTimeSeriesDecompositionDetail.cc
@@ -2918,9 +2918,9 @@ bool CTimeSeriesDecompositionDetail::CComponents::CCalendar::acceptRestoreTraver
                     core::CPersistUtils::restore(ERRORS_6_4_TAG, m_PredictionErrors, traverser))
         }
     } else {
-        LOG_ERROR(<< "Input error: unsupported state serialization version'"
+        LOG_ERROR(<< "Input error: unsupported state serialization version '"
                   << traverser.name()
-                  << "'. Currently supported version: " << VERSION_6_4_TAG);
+                  << "'. Currently supported minimum version: " << VERSION_6_4_TAG);
         return false;
     }
     return true;
