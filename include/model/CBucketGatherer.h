d0dfcb974fbc3a319e3308920bd24e11a9859d3c
Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>
2025-03-27 16:44:41 +0100

    Extend influencer counts if the resource monitor allows

--- include/model/CBucketGatherer.h
+++ include/model/CBucketGatherer.h
@@ -168,7 +168,7 @@ public:
     //! to gather data.
     //! \param[in] numberInfluencers The number of result influencers
     //! for which to gather data.
-    CBucketGatherer(CDataGatherer& dataGatherer, core_t::TTime startTime, std::size_t numberInfluencers);
+    CBucketGatherer(CDataGatherer& dataGatherer, const SBucketGathererInitData& bucketGathererInitData);
 
     //! Create a copy that will result in the same persisted state as the
     //! original.  This is effectively a copy constructor that creates a
@@ -474,6 +474,8 @@ private:
 
     //! The influencing field value counts per person and/or attribute.
     TSizeSizePrOptionalStrPrUInt64UMapVecQueue m_InfluencerCounts;
+
+    TOptionalResourceMonitorCRef m_ResourceMonitor;
 };
 }
 }
