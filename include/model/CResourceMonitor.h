fe6f1fac7df9daf4b6a7a8f42c0ea3ce0f668a32
Ed Savage <ed.savage@elastic.co>
2025-04-11 11:08:49 +1200

    Update include/model/CResourceMonitor.h
    Co-authored-by: Valeriy Khakhutskyy <1292899+valeriy42@users.noreply.github.com>

--- include/model/CResourceMonitor.h
+++ include/model/CResourceMonitor.h
@@ -182,7 +182,7 @@ public:
     //! Returns the sum of used memory plus any extra memory
     std::size_t totalMemory() const;
 
-    //! Returns the current physical memory of the process as reported by the system
+    //! Returns the current physical memory of the process (rss) as reported by the system
     std::size_t systemMemory() const;
 
     //! Returns the maximum physical memory of the processs as reported by the system
