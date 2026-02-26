b6ed84c84f0cf176f6af42afd596b0ecdb929950
Nina Lee <nina.lee@elastic.co>
2026-02-26 11:19:11 -0600

    add license header to version bump pipeline

--- .buildkite/job-version-bump.json.py
+++ .buildkite/job-version-bump.json.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License
+# 2.0 and the following additional limitation. Functionality enabled by the
+# files subject to the Elastic License 2.0 may only be used in production when
+# invoked by an Elasticsearch process with a license key installed that permits
+# use of machine learning features. You may not use this file except in
+# compliance with the Elastic License 2.0 and the foregoing additional
+# limitation.
+#
+# This script generates a pipeline JSON for the ml-cpp-debug-build pipeline
+# which is triggered nightly on a schedule. The build itself runs with
+# full debug and assertions enabled.
+#
+# The basic logic of this script is very simple.
+# It either parses the github label or the triggering PR comment.
+# If a PR comment is present, the script ignores the github label.
+#
 
 import contextlib
 import json
