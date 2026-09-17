7818f32312cfdadaf9e4645e71e9b0e8ffab3000
Ed Savage <ed.savage@elastic.co>
2026-09-17 15:34:34 +1200

    Revert "[ML] Temporary: deliberate warning to test -Werror enforcement (revert me)"
    This reverts commit 1eab514c6f61c0a2975045883ad305825cad519e.

--- lib/ver/CBuildInfo.cc
+++ lib/ver/CBuildInfo.cc
@@ -13,8 +13,6 @@
 #include <core/CProgName.h>
 #include <core/CStringUtils.h>
 
-#warning "deliberate test warning to verify -Werror enforcement — remove before merging"
-
 namespace ml {
 namespace ver {
 
